df77e18dfece4130e9d06d41e0a9da759b807475
kelly <kelfro@fastmail.com>
2018-04-17 13:28:36 -0700

    array-methods - done challenge 4

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,7 +57,6 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
 runners.forEach((runner) => {
-  console.log(runner.first_name);
   fullName.push(`${runner.first_name} ${runner.last_name}`);
 });
 console.log(fullName);
@@ -78,9 +77,17 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
 
-// ==== Challenge 5: Be Creative ====
+runners.forEach((runner) => {
+  ticketPriceTotal.push(runner.donation);
+});
+
+let total = ticketPriceTotal.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(total);
+
+//
+
+//==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
