ef9ac90611808927743f606cef64826f5214a201
JSNavey <jsnavey@gmail.com>
2018-04-17 17:32:46 -0400

    Ja Navey - JSII - finished closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function multiply() {
+  let num = 5;
+  function timesTen() {
+    return num * 10;
+  }
+  return timesTen();
+}
+console.log(multiply());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  return () => {
+    num++;
+    return num;
+  };
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -16,4 +28,20 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+    let x = 0;
+  function newVal(num) {
+    x += num;
+  }
+  return {
+    increment: function() {
+      newVal(1);
+    },
+    decrement: function() {
+      newVal(-1);
+    },
+    value: function() {
+      return x;
+    }
+  };
 };
+
