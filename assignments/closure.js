8971769dd99c1b90fe7fa1321af70ca0a01c04b8
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-04-17 23:11:14 +0100

    closure challange completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -10,14 +10,13 @@ wait('Hay! this is Closure');
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  let count = 0;
-  counter();
-  count++;
-  return count;
+ let count = 0;
+ return  () => (count++)
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+console.log(newCounter()); 
+console.log(newCounter()); 
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
@@ -25,4 +24,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    "increment" : function(){
+      count++;
+      return count;
+    },
+    "decrement" : function(){
+      count--;
+      return count;git
+    }
+  }
 };
