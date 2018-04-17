038c5831189e4c2fadeb39d3ec243df57897cc4d
David Loveday <davemech@yahoo.com>
2018-04-17 15:21:42 -0700

    added return statement to counterFactory function of closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -27,8 +27,16 @@ console.log(newCounter());
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   let counter = 0;
-  const obj = {"increment": () =>{ return ++counter;  }, "decrement": ()=> { return --counter; }};
+ return obj = {
+   "increment": () =>{ 
+     return ++counter;  
+    }, 
+    "decrement": ()=> { 
+      return --counter; 
+    }
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
