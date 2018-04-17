3d61198b9a893c8d69ba3db3c3f5a03dd9af5bea
Steve <shalomlandsman@gmail.com>
2018-04-17 16:15:07 -0400

    array-methods complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -64,23 +64,56 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+allCaps = runners.map(element => element.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(element => element.shirt_size === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+ticketPriceTotal = runners.reduce((theReducer, data) =>{
+    return theReducer += data.donation;
+  }, 0)
+
+  console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
 
+// Total donation amount of people with shirt size small:
+
+let smallShirts = [];
+smallShirts = runners.filter(element => element.shirt_size === "S");
+smallShirtsTotal = smallShirts.reduce((theReducer, data) =>{
+    return theReducer += data.donation;
+  }, 0)
+
+console.log(smallShirtsTotal);
+
 // Problem 2
+// Create array of email addresses of all donors who donated above average amounts:
+
+let avg = ticketPriceTotal / runners.length;
+
+let aboveAverage = [];
+aboveAverage = runners.filter(element => element.donation > avg);
+
+aboveAvgEmails = aboveAverage.map(element => element.email);
+
+console.log(aboveAvgEmails);
+ 
 
 // Problem 3
+// How many runners have a shirt size of XL?:
+
+let xlShirts = [];
+xlShirts = runners.filter(element => element.shirt_size === "XL");
+let sum = xlShirts.length;
+console.log(sum);
