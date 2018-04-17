4680ac7a91033c8be9335c2093ef116ea8f1ee1a
William VanDolah <william.vandolah@gmail.com>
2018-04-17 14:49:25 -0700

    completed closure Challenge 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -7,13 +7,14 @@ console.log(greeting());
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = function() {
   // Return a function that when invoked increments and returns a counter variable.
   let counting = 1;
   return function(){
     return counting++
   }
 };
+console.log(counter())
 
 const newCounter = counter();
 console.log(newCounter())
@@ -28,8 +29,24 @@ console.log(newCounter())
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// let counting = 1
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let counting = 1;
+  return {
+  	"increment": () => counting++,
+  	'decrement': () => counting--,
+  }
+}
+
+factoryCounter = counterFactory();
+console.log(factoryCounter.increment());
+console.log(factoryCounter.increment());
+console.log(factoryCounter.increment());
+console.log(factoryCounter.increment());
+console.log(factoryCounter.decrement());
+console.log(factoryCounter.decrement());
+console.log(factoryCounter.decrement());
+console.log(factoryCounter.decrement());
