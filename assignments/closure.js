519aa5bc5cd011331c633440f80d2fdbb8f72071
Sam Park <sam.park.email@gmail.com>
2018-04-17 17:59:16 -0400

    finished

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,16 +4,65 @@
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (function() {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let countNum = 0;
+  function newNum(num) {
+    countNum += num;
+  }
+  return {
+    increment: function() {
+      newNum(1);
+    },
+    decrement: function() {
+      newNum(-1);
+    },
+    number: function() {
+      return countNum;
+    }
+  };
+})();
+  
+  // return {
+  //   increment: function(){
+  //     countNum++;
+  //     return countNum;
+  //   },
+  //   decrement: function(){
+  //     countNum--;
+  //     return countNum;
+  //   }
+  // }
+ 
+ counterFactory.increment();
+ counterFactory.increment();
+ counterFactory.increment();
+ console.log(counterFactory.number());
+ counterFactory.decrement();
+ counterFactory.decrement();
+ console.log(counterFactory.number());
+ counterFactory.increment();
+ counterFactory.increment();
+ counterFactory.increment();
+ console.log(counterFactory.number());
