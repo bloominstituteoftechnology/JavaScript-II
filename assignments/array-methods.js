620cb5888deca9c417f90b5bff8213776a7c1fc3
Steve <shalomlandsman@gmail.com>
2018-04-17 15:07:03 -0400

    function conversion complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,6 +56,9 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(element) {
+    fullName.push(`${element.first_name} ${element.last_name}`);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
