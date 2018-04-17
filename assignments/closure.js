a53bb253b7c897860eac1acb1dbccc9c2ca8c12e
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-17 17:45:37 -0400

    Completed closure.js Challenge 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,12 +5,14 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  let count = 1;
-  return () => count++;
+  let count = 0;
+  return () => ++count;
 };
+
 const newCounter = counter();
 console.log(newCounter()); // 1
 console.log(newCounter()); // 2
+console.log(newCounter());
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
@@ -18,4 +20,16 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    'increment': () => ++count,
+    'decrement': () => --count
+  }
 };
+
+const newCounterFactory = counterFactory();
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
