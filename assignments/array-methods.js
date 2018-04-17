3d93f415a57b325f255e5f1f21ebf7efff89463f
jesuarva <jean.ariza@jesuarva.com>
2018-04-17 22:12:30 +0200

    Array -> END Challenge 3: Use .filter()

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -80,8 +80,11 @@ allCaps = runners.map( value => value.first_name.toUpperCase() );
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+/* The large shirts won't be available for the event due to an ordering issue.
+    Get a list of runners with large sized shirts so they can choose a different size.
+    Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result */
 let largeShirts = [];
+largeShirts = runners.filter( (value, index, array) => value.shirt_size === "L" ? value : null );
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
