8b94e4bf11fa91b16ca45f0c8e74d9624d250cfd
jggarza5 <jggarza5@gmail.com>
2018-04-17 14:37:40 -0500

    completed array-methods.js and function-conversion.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,9 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const challenge1 = () => {
+    
+}
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
