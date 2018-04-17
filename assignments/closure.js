1b42754be4d1862870f59f376c297e4773110e2f
dlinz7 <linzdiegoa@gmail.com>
2018-04-17 17:33:24 -0500

    JS2 Incomplete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,21 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const favColor = 'blue';
+function returnFaveColor(){
+  return favColor;
+}
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let newCount = 0
+    return () => {
+      newCount++;
+        return newCount;
+    }
 };
+
+const newCounter= counter;
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -16,4 +26,6 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  
 };
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -11,10 +11,10 @@
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+//let subtract = function (param1, param2) {
+ // return param1 - param2;
+//};
+//subtract(1,2); //?
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
