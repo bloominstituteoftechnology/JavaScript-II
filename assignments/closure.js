5e78fd43e6565475dd72aadab9127043e5a8046f
jesuarva <jean.ariza@jesuarva.com>
2018-04-17 23:22:26 +0200

    Closure -> END Challenge 3: Counter factory.

--- assignments/closure.js
+++ assignments/closure.js
@@ -42,4 +42,18 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let counter = 0;
+  function increment () {
+    return ++counter;;
+  }
+  function decrement () {
+    return --counter;
+  }
+  return {
+    increment,
+    decrement
+  }
+}
+console.log(counterFactory().increment());
+console.log(counterFactory().decrement());
+
