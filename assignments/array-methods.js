32338caa18ac6959d90ee221efd3c26d252e8203
jesuarva <jean.ariza@jesuarva.com>
2018-04-17 22:06:57 +0200

    Arrays -> END Challenge 2: Use .map()

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,14 +67,16 @@ for ( let value of runners ) {
     fullName.push(`${value.first_name} ${value.last_name}`);
 }
 /** Option 2 */
-runners.forEach( value => fullName.push(`${value.first_name} ${value.last_name}`) )
+runners.forEach( value => fullName.push(`${value.first_name} ${value.last_name}`) );
 console.log(fullName);
 
 
 
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+/* The event director needs to have all the runner's first names converted
+    to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result */
 let allCaps = [];
+allCaps = runners.map( value => value.first_name.toUpperCase() );
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
