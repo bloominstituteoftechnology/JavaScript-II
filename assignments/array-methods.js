2e357b9715348e07b63177febe976ff874bab2b7
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-18 05:53:23 +0800

    Updated array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -78,14 +78,25 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 
-let largeShirts = [];
-console.log(largeShirts);
+// let largeShirts = [];
 
+// const runnerLarge = runners.filter((index) => {
+//   if (index.shirt_size === "L")
+//   return largeShirts.push(.shirt_size);
+// });
+
+// console.log(largeShirts);
 
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+
 let ticketPriceTotal = [];
+
+const whatever = runners.reduce((item, index, array) => {
+  ticketPriceTotal.push(item.donation);
+}, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
