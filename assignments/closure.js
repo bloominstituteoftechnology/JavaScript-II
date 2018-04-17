ef55c4fc0f2c7d06e1343d8b2e3015f59eeeedc9
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-04-17 14:52:19 -0700

    closures done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const greet = () => {
+  let person = {
+    firstName: 'Erinc',
+    lastName: 'Emer'
+  };
+  const hello = () => {
+    return `Hello ${person.firstName} ${person.lastName}`
+  }
+  return hello()
+}
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  }
 };
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -16,4 +32,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: function () {
+      count++;
+      return count;
+    },
+    decrement: function () {
+      count--;
+      return count;
+    }
+  }
 };
