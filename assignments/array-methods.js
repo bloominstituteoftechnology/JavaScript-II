3f3c75db2de5b02797bbc2a481545cc7dd6b74ec
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-18 06:50:13 +0800

    Answered Challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -78,15 +78,17 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 
-// let largeShirts = [];
+console.log(largeShirts);
 
-// const runnerLarge = runners.filter((index) => {
-//   if (index.shirt_size === "L")
-//   return largeShirts.push(.shirt_size);
-// });
+let largeShirts = [];
 
-// console.log(largeShirts);
+const result = runners.filter((item) => {
+ if (item.shirt_size === "L") {
+   largeShirts.push(item);
+ }
+});
 
+console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
