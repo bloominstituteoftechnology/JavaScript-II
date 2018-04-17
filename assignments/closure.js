c9d2eb0fadeefdac13a63083d33ce5b6ddaa843e
mr-burton <taylormade800@gmail.com>
2018-04-17 15:53:35 -0700

    solving closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -28,5 +28,14 @@ console.log(newCounter());
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  // `decrement` should decrement the counter variable and return it.'
+  let count = 0;
+  return function increase() {
+    ++count;
+    return count;
+  }
+  return function decrease() {
+    --count;
+    return count;
+  };
 };
