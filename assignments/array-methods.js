9c6679f89b79a2d577941c90778a3bf552260373
William VanDolah <william.vandolah@gmail.com>
2018-04-17 13:17:03 -0700

    completed arry-methods Challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -72,7 +72,9 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter((cRunnerInfo) =>{ //filter takes 3 params, ist is the current item, second is the index of that item, third is the entire Aaray
+    return cRunnerInfo.shirt_size === "L"
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
