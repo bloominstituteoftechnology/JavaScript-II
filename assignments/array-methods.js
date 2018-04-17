a4e579ea6dd6d952c6b3f7d43a813164de2a4ee1
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-17 15:12:31 -0400

    Completed array-methods.js Challenge 2

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -61,7 +61,7 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(x => x.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
