708d101e4426ce333e2a36406d58cd8e49d8214c
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-04-17 16:26:06 -0400

    function-conversion done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -11,13 +11,36 @@
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+
+const myFunction = () => {};
+
+const anotherFunction =(param) => {
+  return param;
+}
+
+const add = (param1, param2) => {
+  return param1 + param2;
+}
+console.log(add(1,2));
+
+
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+
+const subtract = (param1, param2) => {
+  return param1 -param2;
+}
+console.log(subtract(1,2));
+
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => {
+  return num * 3;
+});
+console.log(triple);
