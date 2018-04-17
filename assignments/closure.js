ee0465a13337907fc68e415274899e4f4e7e7e86
Derrick Mei <derrick.mei.168@gmail.com>
2018-04-17 18:05:22 -0500

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+let gender = 'I am male'
+let self = () => {
+  let gender = 'I am female'
+  console.log( gender)
+}
+console.log(gender)
+self();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return function (){
+    count ++
+    return count
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
+
+let newCounter = counter()
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -16,4 +34,21 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  let math = {
+    increment: function(){
+      count ++;
+      return count;
+    },
+    decrement: function(){
+      count--;
+      return count;
+    }
+  }
+  return math;
 };
+
+let mathematics = counterFactory()
+console.log(mathematics.increment());
+console.log(mathematics.increment());
+console.log(mathematics.decrement());
