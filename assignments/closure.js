affa525e0ef251af41d637b1d1591c80f1d7d6c6
jaklundun <govi.escudero@gmail.com>
2018-04-17 16:07:49 -0700

    Completed challenge 1 and 2 for Closures.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let logger = function (x) {
+  console.log(x);
+};
+
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+counter = () => {
+  let count = 0;
+  
+  return function () {
+    count++;
+    return count;
+  }
+}
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
