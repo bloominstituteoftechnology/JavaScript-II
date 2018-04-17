f97b8dbc9487d71d1e66904f6b2b05953eacfd28
NandoTheessen <nando.theessen@gmail.com>
2018-04-17 22:31:08 +0200

    minor fixes around array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,26 +56,26 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-runners.forEach((i) => fullName.push(`${i.first_name} ${i.last_name}`));
+runners.forEach((runner) => fullName.push(`${runner.first_name} ${runner.last_name}`));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
 //runners.forEach((i) => allCaps.push(`${i.first_name.toUpperCase()}`));
-allCaps = runners.map((item, index, array) => item.first_name.toUpperCase());
+allCaps = runners.map((runner) => runner.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-largeShirts = runners.filter((data, index, array) => data.shirt_size = "L")
+largeShirts = runners.filter((runner) => runner.shirt_size = "L")
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce((reducer, elem) =>  reducer += elem.donation, 0);
+ticketPriceTotal = runners.reduce((reducer, runner) =>  reducer += runner.donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -89,17 +89,17 @@ console.log(mailList);
 // Problem 2 VIP and lowBaller List
 const VIP = [];
 const lowBaller = [];
-runners.map((item, index, array) => {
-    if (item.donation >= 200) {
-        VIP.push(`${item.first_name} ${item.last_name}`);
-    } else if(item.donation <= 100){
-        lowBaller.push(`${item.first_name} ${item.last_name}`);
+runners.map((runner) => {
+    if (runner.donation >= 200) {
+        VIP.push(`${runner.first_name} ${runner.last_name}`);
+    } else if(runner.donation <= 100){
+        lowBaller.push(`${runner.first_name} ${runner.last_name}`);
     }
 });
 console.log(VIP, lowBaller);
 
 // Problem 3 adults only! 
 let adults = [];
-adults = runners.filter((data, index, array) => data.shirt_size != "XS" && data.shirt_size != "S"); 
+adults = runners.filter((runner) => runner.shirt_size != "XS" && runner.shirt_size != "S"); 
 
 console.log(adults);
