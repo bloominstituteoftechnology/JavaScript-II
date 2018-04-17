60ff7a90d0a16dcd0169cb4257e2ba17267e99cf
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-17 16:58:52 -0700

    completed function-conversion.js and array-methods.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -6,20 +6,40 @@
 //   return param;
 // };
 
-
+let anotherFunction = (param) => {
+    return param;
+}
+anotherFunction();
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+}
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 + param2;
 // };
 // subtract(1,2);
+
+let subtract = (param1, param2) => {
+    return param1 - param2;
+}
+subtract(48, 37);
+
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => {
+    return num*3;
+});
+
+console.log(triple);
