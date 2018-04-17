72b6fcfe3560b64bc5e0c4d67d360870f8b701ef
jaklundun <govi.escudero@gmail.com>
2018-04-17 14:52:13 -0700

    Completed Arrays Challenge 1-4.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,56 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+// forEach logic
+// runners.forEach(function(element){
+//     console.log(element.id);
+// })
+
+
+// ES5
+//runners.forEach(function(element){
+//     fullName.push(element.first_name + ' ' + element.last_name);
+// })
+
+// code above refactored 
+
+runners.forEach((element) => fullName.push(`${element.first_name}  ${element.last_name}`));
+
 console.log(fullName);
+
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+
+//ES5
+// let firstNamesCap = runners.map(function(element){
+//   return element.first_name.toUpperCase();
+// })
+
+//ES6 refactored code
+let firstNamesCap = runners.map((element) => element.first_name.toUpperCase());
+console.log(firstNamesCap);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+
+let largeShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'L') {
+        return element.shirt_size;
+    }
+});
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(accumlator, element){
+    accumlator += element.donation;
+    return accumlator;
+},0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -23,13 +23,26 @@ console.log(anotherFunction);
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+let add = (param1, param2) => param1 + param2;
+
+console.log(add);
+
+
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+
+let subtract = (param1, param2) => param1 - param2;
+
+console.log(subtract(1,2)); 
+
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => num * 3); //example of fat arrow function notation on an anonymous function 
+
+console.log(triple);
