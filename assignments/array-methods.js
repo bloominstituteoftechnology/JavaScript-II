b449654d54ed3c4161ce5321963b75a30f431487
jesuarva <jean.ariza@jesuarva.com>
2018-04-17 22:20:30 +0200

    Array -> END Challenge 4: Use .reduce()

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -89,7 +89,8 @@ console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal;
+ticketPriceTotal = runners.reduce( ( (total, value)  => total += value.donation), 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
