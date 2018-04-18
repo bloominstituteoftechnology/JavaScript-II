f4b596fef3191fab5cc59c6c73e30f67749c3778
dlinz7 <linzdiegoa@gmail.com>
2018-04-18 11:11:11 -0500

    JS2 function-conv finished

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -96,7 +96,7 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1 tally up all the dontations from skinix
+// Problem 1 tally up all the dontations from the Skinix company employees
 let skinixEmployees = runners.filter((runners, index, array) =>{
     return runners.company_name === "Skinix"; 
   })
--- assignments/closure.js
+++ assignments/closure.js
@@ -26,6 +26,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-
+  let count = 0
   
+    const increment = () => {
+      count++;
+        return count;
+    }
+    const decrement = () => {
+      count--;
+        return count;
+    }
+
 };
