d4a3f8e214effcd9f01f13ca1ffcb30ad3317536
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-18 21:19:26 +0800

    Added answer to Challenge 4

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -91,10 +91,8 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 
-let ticketPriceTotal = [];
-
-const whatever = runners.reduce((item, index, array) => {
-  ticketPriceTotal.push(item.donation);
+let ticketPriceTotal = runners.reduce((donation, runner, index, runners) => {
+  return donation += runner.donation;
 }, 0);
 
 console.log(ticketPriceTotal);
