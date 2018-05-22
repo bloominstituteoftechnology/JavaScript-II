411b1eb6a7837e28e5d9eae1d87177e1ea7e24bc
Mark Hermansen <markwilliams.aus@gmail.com>
2018-05-22 14:35:47 -0500

    already completed callsbacks in JavaScript-I on 05/21/2018

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,34 +1,37 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-}
+// ALREADY COMPLETED CALLBACKS IN JS-I ON 05/21/2018
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+// function firstItem(arr, cb) {
+//   // firstItem passes the first item of the given array to the callback function.
+// }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+// }
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+// }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+// function sumNums(x, y, cb) {
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
 
-/* STRETCH PROBLEM */
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+// }
+
+// /* STRETCH PROBLEM */
+
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
