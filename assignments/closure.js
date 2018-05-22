5e26227bd951f481b2619d5a55ca616208c8f0bc
Ciara Burkett <ciaraburkett@gmail.com>
2018-05-22 18:41:14 -0400

    Finished Function Conversion

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,13 +3,24 @@
 
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let i = 0;
+
+  function increment(n) {
+    n++;
+    return n;
+  }
+
+  return i = increment(i);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+const newCounter = counter();
+console.log(newCounter);
+console.log(newCounter);
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
