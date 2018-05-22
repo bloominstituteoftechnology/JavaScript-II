3e020ca796b0580ceffb5110bcfba5d2f07b8067
Randy Calderon <randycweb@gmail.com>
2018-05-22 15:50:23 -0700

    Finished closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -33,4 +33,13 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let counter = 0;
+  return {
+    'increment': function() {
+      return counter++;
+    },
+    'decrement': function() {
+      return counter--;
+    }
+  }
+}
