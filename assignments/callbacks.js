43f7c2a5c22bf772f49a0cd11eda1085e4495441
Katie Gorbell <katiegorbell@gmail.com>
2018-05-22 16:13:51 -0500

    worked on callbacks and function-conversion

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,28 +2,73 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
+
+firstItem(items, function(first) {
+  console.log(first);
+});
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(3, 4, function(sum) {
+  console.log(sum);
+});
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 5, function(multiply) {
+  console.log(multiply);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let containsItem = '';
+  if (list.indexOf(item) >= 0) {
+    containsItem = 'true';
+  } else {
+    containsItem = 'false';
+  }
+  return cb(containsItem);
 }
+
+contains('Notebook', items, function(truthiness) {
+  console.log(truthiness);
+});
+
+contains('Desk', items, function(containsItem) {
+  console.log(containsItem);
+});
+
 
 /* STRETCH PROBLEM */
 
