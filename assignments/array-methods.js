28b793ac5e8ed2f54bee16aa48113a9640eb3ac9
Ciara Burkett <ciaraburkett@gmail.com>
2018-05-22 17:17:58 -0400

    Finished Array Methods, Challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,15 +67,22 @@ console.log(fullName);
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
 
-fullName.forEach((name) => {
+fullName.map((name) => {
   allCaps.push(name.toUpperCase().split(" ")[0]);
-});
+})
 
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+
+runners.filter((runner) => {
+  if (runner.shirt_size === "L") {
+    largeShirts.push(runner);
+  }
+});
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
