3fd76d4754a42920676909ab61d34f63f2a03fe1
Kairi <kairijb@gmail.com>
2018-05-22 15:54:35 -0500

    function conversion done

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,6 +5,11 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  const newCounter = (count) => {
+    count++;
+    return count;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
