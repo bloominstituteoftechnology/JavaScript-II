292bab3526d06cbd47c90cff2497c17af58f6d10
M.Real <mreal126@protonmail.com>
2018-05-22 18:11:31 -0400

    all but array-methods challenge 5 done

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,21 +5,37 @@ function truth() {
 return (Me + ' has much to learn about Javascript!');
 }
 console.log(truth()); //?
-let Me = 'Schlomo'
-console.log(truth()); //?
+
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
-
+let count = 0;
+const counter = () => {
+  count++;
+  return count;
+};
+console.log(counter());
+console.log(counter());
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let num = 0;
+  const counterFunction = () => {
+    function increment(){
+      num++;
+      return num;
+    }
+    return increment();
+
+    function decrement(){
+      num--;
+      return num;
+  }
+    return decrement();
+}
+
+console.log(counterFunction('increment')); //? 
