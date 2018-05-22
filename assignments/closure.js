1ce55ca824c80f1228089abe9e981033556e28d9
Joe Stanfield <jlstan06@hotmail.com>
2018-05-22 19:03:28 -0400

    closure is finished but still having a tough time understanding it

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function parentFunction(param1, param2) {
+  let myValue = "Joe";
+  console.log("parent: " + myValue);
 
-// ==== Challenge 2: Create a counter function ====
+  function childFunction() {
+    console.log("Child: " + myValue + " " + param1);
+   
+  }
+  return childFunction();
+}
+parentFunction();
+
+// ==== Challenge 2: Create a counter function called 
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  return () => (num++);
 };
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+
+
+
+
+
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -16,4 +42,18 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let num = 0;
+  return () => (newObj = {
+    "increment": () => (++num),
+    "decrement": () => (--num),
+
+  });
+
 };
+let newObj = counterFactory();
+newObj();
+
+console.log(newObj.increment());
+console.log(newObj.increment());
+console.log(newObj.decrement());
+
