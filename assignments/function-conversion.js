49991702fc511b209b00b70eb6190584af9db187
chainchompa <chainchompa@gmail.com>
2018-05-22 15:58:38 -0400

    initial commit

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -2,21 +2,32 @@
 
 // let myFunction = function () {};
 
+let myFunction = () => {};
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = (param) => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+
+let add = (param1, param2) => param1 + param2;
+add(3,4);
+
+
+let subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
 
 exampleArray = [1,2,3,4];
+let triple = exampleArray.map((num) => {return num * 3});
+console.log(triple);
+
+
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
