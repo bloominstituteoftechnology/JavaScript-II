858cee2c3ff2c5751da5378f424a05da5e8e443a
CLN <cln@Sags-MacBook-Pro.local>
2018-05-22 19:04:25 -0400

    started array, .

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,8 +54,10 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  
+//Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach((element) => fullName.push)
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
