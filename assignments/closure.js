6f2553ae7c3e168db24df334e2b8b823c852e4fe
Daniel Chamorro <dan@dcitllc.com>
2018-05-22 18:55:57 -0400

    Final Push

--- assignments/closure.js
+++ assignments/closure.js
@@ -40,4 +40,16 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let num = 0;
+  function increment() {
+    return num++; //?
+
+    function decrement() {
+      return num--; //?
+    }
+    return decrement; //?
+  }
+  return increment; //?
 };
+
+counterFactory(); //?
