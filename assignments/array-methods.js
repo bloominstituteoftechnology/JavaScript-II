c90abd621efa18919c7f62d4ff8cbd9767ce2b2a
Jameson <jamesonbrown@neolivity.com>
2018-05-22 16:52:29 -0600

    Fixed syntax, array-methods.js Challenge : Problem .

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -77,9 +77,9 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1: The event director needs a list of companies participating to send everyone a thank you.
+// Problem 1: The event director needs a list of companies participating and their emails to send reminder/update emails.
 let companyName = [];
-runners.forEach((element) => companyName.push(element.company_name));
+runners.forEach((element) => companyName.push(element.company_name+' '+element.email));
 console.log(companyName)
 
 // Problem 2
