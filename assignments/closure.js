b7d51029bad52f73b7d2c5faf26e6dcd047724a6
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-05-22 16:51:02 -0400

    completed closures.js ... this one gave me a headache :(

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function dayorNight(time) {
+  let dayorNight = time;
 
+  function dayOrNight() {
+    if (dayorNight <= 18) {
+   return 'Day';
+    
+    } else {
+      return 'Night'
+    }
+    
+  }
+return dayorNight();
+}
 // ==== Challenge 2: Create a counter function ====
+// Return a function that when invoked increments and returns a counter variable.
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let value = 0;
+  return () => {
+    return ++value;
 };
+}
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -13,7 +30,16 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let value = 0;
+  return {
+    increment: () => {
+      return ++value;
+    },
+     decrement: () => {
+      return --value;
+    }
+  };
+};
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
