967be8d343cb1b667661cc0d8ad791835744a4a5
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-05-22 15:36:29 -0700

    Completes all requirements and stretch goals.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -11,13 +11,19 @@
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+// subtract(1,2);
 
-exampleArray = [1,2,3,4];
+const subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1, 2));
+
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+// const triple = () => exampleArray.map(num => num * 3);
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
