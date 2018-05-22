b1ab0f5521bc5dfb068ac8b26e058e0b8bd9dc5a
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-22 14:31:41 -0700

    Completed all array-methods exercises, as well as one self structured stretch problem

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,30 +54,56 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
+fullName = runners.map(runner => `${runner.first_name} ${runner.last_name}`)
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+allCaps = runners.map(runner => runner.first_name.toUpperCase())
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(runner => runner.shirt_size === "L")
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((total, runner) => {return total += runner.donation}, 0)
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// We wanna know where the BIG MONEY is coming from!!! Find the company whose employees paid the highest in donations
+//First get a list of companies
+let companies = []
+companies = Array.from(new Set(runners.map(runner => runner.company_name)))
+console.log(companies);
+//Then create objects for Each
+companies = companies.map((company) => ( {"name": company,
+                                          "donation": 0})  )
+console.log(companies);
+runners.forEach(runner => {
+  let myCompany = companies.find(company => company.name === runner.company_name)
+  myCompany.donation += runner.donation;
+})
 
+console.log(companies);
+
+//And find the company that paid the highest in donations
+let greatest = companies[0]
+for (let i = 0; i < companies.length; i++) {
+  if(companies[i].donation > greatest.donation)
+  { greatest = companies[i];  }
+}
+console.log(greatest);
 // Problem 2
 
 // Problem 3
