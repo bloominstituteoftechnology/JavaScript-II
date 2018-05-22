444d9cc4c1c29fa1382aea183fa5ae70d118a73b
Luis <luis@Luiss-MacBook-Air.local>
2018-05-22 11:52:31 -0700

    created a function that increments everytime it is invoked

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,10 +21,15 @@ math(100, 3)
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0
+  return () => {
+    count++
+    return count
+  }
 }
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter()
+console.log(newCounter()) // 1
+console.log(newCounter()) // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
