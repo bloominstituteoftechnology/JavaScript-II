a4737d78b0287f305920e8e3724e936d4c6c13ee
snowcoding <vimalshah77@gmail.com>
2018-05-22 11:26:40 -0700

    converted all functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,23 +1,47 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {};
+// let myFunctionES5 = function () {};
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
+let myFunctionES6 = () => {};
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
 
-let subtract = function (param1, param2) {
+
+let anotherFunctionES5 = function (param) {
+  return param;
+};
+
+let anotherFunctionES6 = param => param;
+console.log(anotherFunctionES5('test'));
+console.log(anotherFunctionES6('test'));
+
+
+
+let addES5 = function (param1, param2) {
+  return param1 + param2;
+};
+
+let addES6 = (param1,param2) => param1 + param2;
+
+console.log(addES5(1,3))
+console.log(addES6(1,3))
+
+
+
+let subtractES5 = function (param1, param2) {
   return param1 - param2;
 };
-subtract(1,2); //?
+let subtractES6 = (param1,param2) => param1 - param2;
+console.log(subtractES5(1,2))
+console.log(subtractES6(1,2))
+
+
+
 
 exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+const tripleES5 = exampleArray.map(function (num) {
+  return num * 3;
+});
+
+const tripleES6 = exampleArray.map(num => num*3)
+console.log(tripleES5);
+console.log(tripleES6);
