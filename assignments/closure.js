191ac1badee8c2228f59fc70f42509bc03195168
Alex Ager <alexager5028@Alexs-MacBook-Air.local>
2018-05-22 14:50:01 -0600

    Refactored arrow functions. Almost finished with closures,

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,26 +18,55 @@ console.log(addHundred(200))
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
- // Return a function that when invoked increments and returns a counter variable.
-};
+// const counter = () => { return
+
+// };
+
+//  // Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
+function counter(a){
+
+  return function(){
+    //  return a += b;
+    return a++;
+
+};
+
+}
 
 
+let newCounter = counter(46);
 
-
-
-
-
-
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+// const counterFactory = () => {
+// };
  // Return an object that has two methods called `increment` and `decrement`.
  // `increment` should increment a counter variable in closure scope and return it.
  // `decrement` should decrement the counter variable and return it.
-};
+
+ function countUpOrDown(a){
+
+//     function theCounter
+//   let incOrDec = {
+//       increment = function(){
+//           return a++;
+//       },
+
+//       decrement = function(){
+//         return a--;
+
+//       },
+    
+//     };
+
+ }  
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -23,19 +23,26 @@ const anotherFunction = (param) => { return param };
 //   return param1 + param2;
 // };
 // add(1,2);
-const add = (param1, param2) => { return param1 + param2 };
-console.log(add(1,2));
+const add = (param1, param2) =>  param1 + param2 ;
+console.log(add(16,20));
 
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2); 
-const subtract = (param1, param2) => { return param1 - param2 };
-console.log(subtract(1,2));
+const subtract = (param1, param2) => param1 - param2 ;
+console.log(subtract(120,10));
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
+
+
+// const prices = smartPhones.map(smartPhone => smartPhone.price);
+// console.log(prices); // [649, 576, 489]
