2d38d1f3e5466ee2dfdfb708cf3f0e9c65547ec7
Sam Khaled <samkhaled@outlook.com>
2018-05-22 17:34:32 -0400

    Array Methods are great

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,78 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach((element) => fullName.push(`${element.first_name}  ${element.last_name}`));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+//let allCaps = [];
+let allCaps = runners.map((element) => element.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'S') {
+        return element.shirt_size;
+    }
+ });
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+let ticketPriceTotal = runners.reduce((total, runner) => {return total + runner.donation}, 0);
+console.log(`ticket Price Total: $ ${ticketPriceTotal}`);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
 
+
+ let printingXSmallShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'XS') {
+        return element.shirt_size;
+    }
+ });
+ let printingSmallShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'S') {
+        return element.shirt_size;
+    }
+ });
+  let printingMediumShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'M') {
+        return element.shirt_size;
+    }
+ });
+ let printingLargeShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'L') {
+        return element.shirt_size;
+    }
+ });
+ let printingXLargeShirts = runners.filter(function(element) {
+    if(element.shirt_size === 'XL') {
+        return element.shirt_size;
+    }
+ });
+  let printing2XShirts = runners.filter(function(element) {
+    if(element.shirt_size === '2XL') {
+        return element.shirt_size;
+    }
+ });
+   let printing3XShirts = runners.filter(function(element) {
+    if(element.shirt_size === '3XL') {
+        return element.shirt_size;
+    }
+ });
+ console.log(`\n \n We need to print these quantity T-Shirts:\n \n XS = ${printingXSmallShirts.length} 
+                                                    \n S =  ${printingSmallShirts.length}
+                                                    \n M =  ${printingMediumShirts.length}
+                                                    \n L =  ${printingLargeShirts.length} 
+                                                    \n Xl =  ${printingXLargeShirts.length}
+                                                    \n 2XL =  ${printing2XShirts.length}
+                                                    \n 3XL =  ${printing3XShirts.length}`);
 // Problem 2
 
 // Problem 3
