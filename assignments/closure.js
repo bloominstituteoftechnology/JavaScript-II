b6dd8aab10e14982edb89ba2014140446434b45c
Randy Calderon <randycweb@gmail.com>
2018-05-22 15:43:49 -0700

    Almost done with closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function closure(array) {
+  let newArray = [];
+
+  function addArray(){
+    let info = 'owmdpoampodmcopamsopcmaopdmpalzmck';
+  }
+}
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function () {
+   return count += 1;
+  }
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
