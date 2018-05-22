a4547f3d18c9f3f858cc7b53c6743d704c848a8d
Erin Meredith <ekmeredith@att.net>
2018-05-22 17:59:08 -0400

    1-4 finished

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,33 +57,31 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
 runners.forEach(function(i) {
-    fullName.push(runners[i].first_name + runners[i].last_name);
-    return fullName;
+    return fullName.push(i.first_name + i.last_name);
 });
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-runners.map(function(i) {
-    allCaps.toUpperCase(runners[i].first_name).push(runners[i].first_name);
-    return allCaps;
+let allCaps = runners.map(function(i) {
+    return i.first_name.toUpperCase();
     });
 
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-runners.filter(function() {
-    largeShirts.push(runners.shirt_size === 'L');
-    return largeShirts;
+let largeShirts = runners.filter((data, i, runners) => {
+    return data.shirt_size === 'L';
+
     });
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((theReducer, item) => {
+  return theReducer += item.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -94,3 +92,5 @@ console.log(ticketPriceTotal);
 // Problem 2
 
 // Problem 3
+
+
