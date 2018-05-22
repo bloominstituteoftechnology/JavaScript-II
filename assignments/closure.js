aa33aaa413e8918557dac43ffb1fb5de4ecf24a7
Mark Hermansen <markwilliams.aus@gmail.com>
2018-05-22 17:07:53 -0500

    completed challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const closure = () => {
+  let sushi = 0;
+    return function makeSushi() {
+      return sushi += 1;
+  }  
+};
+let sush = closure();
+console.log(sush());
+console.log(sush());
+console.log(sush());
+console.log(sush());
+console.log(sush());
+
+
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+    return function () {return count += 1;}
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+console.log(newCounter());
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
@@ -16,4 +34,18 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let count = 0;
+  return {
+    increment() {
+      console.log(++count);
+    },
+    decrement() {
+      console.log(--count);
+    },
+  };
+}
+
+let count = counterFactory();
+
+console.log(count);
+count.increment();
