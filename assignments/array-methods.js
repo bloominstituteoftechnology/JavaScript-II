aa4031b349f1df5264e931f8592504e3f40a92e2
Austin Merando <ajmerando1@yahoo.com>
2018-05-22 15:42:38 -0700

    almost complete, unsure about last part of closure

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -63,34 +63,39 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = []
-allCaps = runners.map(item => {
-   let upper = '';
-   upper = item.first_name.toUpperCase();
-   return upper;
-});
+allCaps = runners.map(item => {return item.first_name.toUpperCase();});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-largeShirts = runners.filter((items) => {
-    return items.shirt_size === 'L';
-});
+largeShirts = runners.filter((items) => {return items.shirt_size === 'L';});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce((total,item) => {
-    return total += item.donation;
-}, 0);
+ticketPriceTotal = runners.reduce((total,item) => { return total += item.donation;}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
+//Find people whos companies start with the letter S
 // Problem 1
+let companyLetter = [];
+    companyLetter = runners.filter((company) => {
+        return company.company_name.charAt(0) === 'S';
+    });
+    console.log(companyLetter);
 
+//Find all donations over 200
 // Problem 2
+let bigSpenders = [];
+bigSpenders = runners.filter((donors) => {
+    return donors.donation > 200;
+});
+console.log(bigSpenders);
 
 // Problem 3
+
