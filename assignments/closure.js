b35263d4b085382f41e91f7acb385ce4e0f3687a
Luis <luis@Luiss-MacBook-Air.local>
2018-05-22 11:55:41 -0700

    created counterFactory function

--- assignments/closure.js
+++ assignments/closure.js
@@ -36,4 +36,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let factory = 0
+  return {
+    increment: () => ++factory,
+    decrement: () => --factory,
+    value: () => factory
+  }
 }
+
+let newFac = counterFactory()
+console.log(newFac.decrement())
+console.log(newFac.decrement())
+console.log(newFac.increment())
