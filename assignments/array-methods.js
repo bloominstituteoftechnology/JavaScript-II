2177dcb007ac296197889c8e40a3dd751b04437a
Allen Hai <allenhai03@gmail.com>
2018-05-22 11:00:01 -0500

    done with all array-method challenges

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,39 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(({first_name, last_name}) => {
+    fullName.push({
+        first_name,
+        last_name 
+    })
+})
+
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = fullName.map(({first_name, last_name}) => (
+    {
+        first_name: first_name.toUpperCase(),
+        last_name
+    }
+))
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(({shirt_size}) => {
+    if (shirt_size === "L") return true
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((accum, {donation} ) => (
+    accum + donation
+), 0)
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +96,25 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+let emails = []
+
+runners.forEach(({email}) => {
+    emails.push(email)
+})
+
+console.log("Emails:", emails)
+
 // Problem 2
 
+let govEmails = emails.filter(email => email.includes(".gov"))
+
+console.log("Government Emails:", govEmails)
+
 // Problem 3
+
+let emailDomains = emails.map(email => {
+    let splitEmail = email.split("@")
+    return splitEmail[splitEmail.length - 1]
+})
+
+console.log("Email Domains:", emailDomains)
