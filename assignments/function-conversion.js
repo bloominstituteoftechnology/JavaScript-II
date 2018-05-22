32c8fd8b2390156ed8049f20a51c8259296f3577
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-22 14:37:37 -0700

    re-factored functions in function-conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -2,22 +2,33 @@
 
 // let myFunction = function () {};
 
+// let muFunction = () =>
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 
+let anotherFunction = (param) => param
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2
+
 let subtract = function (param1, param2) {
   return param1 - param2;
 };
-subtract(1,2); //?
+
+let subtractArrow = (param1, param2) => param1 - param2
+
+console.log(subtract(1,2)); //?
+console.log(subtractArrow(1,2));
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = (num) => num * 3
