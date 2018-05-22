3c1de4874bfaf521f5442fb93f9423a851e744de
chainchompa <chainchompa@gmail.com>
2018-05-22 17:32:13 -0400

    completed callbacks and closure

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,26 +1,33 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
+  return cb(items[0]);
   // firstItem passes the first item of the given array to the callback function.
 }
 
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
 
 function last(arr, cb) {
+  return cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 }
 
 function sumNums(x, y, cb) {
+  return cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
 function multiplyNums(x, y, cb) {
+  return cb(x + y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
 function contains(item, list, cb) {
+  return cb(list.includes(item));
+
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,19 +1,41 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function tellstheProduct(a, b) {
+  function multiply(a, b) {
+    return a * b;
+  }
+  return 'The product of these numbers is ' + multiply(a, b);
+}
+
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const counter = () => {
+  let count = 1;
+  return function() {
+    return count++
+  }
+   }
+let newCounter = counter();
+newCounter();
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let count = {
+  increment: function() {
+    count++;
+  },
+  decrement: function() {
+    count--;
+  }
+}
+  return count;
 };
