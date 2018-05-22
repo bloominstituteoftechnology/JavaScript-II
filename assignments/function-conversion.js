180a6df0446bff7945a461c98158ba12478dabb4
Daniel Chamorro <dan@dcitllc.com>
2018-05-22 14:21:10 -0400

    Initial Pull Request

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -14,10 +14,12 @@
 let subtract = function (param1, param2) {
   return param1 - param2;
 };
-subtract(1,2); //?
+subtract(1, 2); //?
 
-exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+// Initial Pull Request 
