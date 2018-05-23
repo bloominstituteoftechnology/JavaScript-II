9a7fc3d42e556c28657a51214749965603243bd3
Nicky Chen <nicky.chen0@gmail.com>
2018-05-22 19:45:43 -0700

    added arrow functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -5,29 +5,54 @@ function firstItem(arr, cb) {
   cb(arr[0]);
 }
 
+//Refactoring
+const firstItem = (arr, cb) => cb(arr[0]);
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   cb(arr.length);
 }
 
+//Refactoring
+const getLength = (arr, cb) => cb(arr.length);
+
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  cb(arr[arr.length-1])
+  cb(arr[arr.length - 1])
 }
+
+//Refactoring
+const last = (arr, cb) => cb(arr[arr.length - 1]);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  cb(x+y);
+  cb(x + y);
 }
+
+//Refactoring
+const sumNums => (x, y, cb) => cb(x + y);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   cb(x * Y);
 }
 
+//Refactoring
+const multiplyNums(x, y, cb) => cb(x * y);
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.'
+  if (list.includes(item)) {
+    cb(true);
+  } else {
+    cb(false);
+  }
+}
+
+//Refactoring
+const contains = (item, list, cb) => {
   if (list.includes(item)) {
     cb(true);
   } else {
@@ -42,10 +67,21 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
   const newArr = [];
-  for (let i =0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (!newArr.includes(arr[i])) {
       newArr.push(arr[i]);
     }
   }
+  cb(newArr);
+}
+
+//Refactoring
+const removeDuplicates => (array, cb) => {
+  const newArr = [];
+  array.forEach(num => {
+    if (!newArr.includes(num)) {
+      newArr.push(num);
+    }
+  })
   cb(newArr);
 }
