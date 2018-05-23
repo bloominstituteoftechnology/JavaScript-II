9a63dad4367209a04954b6dac2419c7a48b2e571
MICHAEL ZOITAS <michaelzoitas@MICHAELs-MacBook-Air.local>
2018-05-23 11:09:20 -0400

    updated

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -64,7 +64,10 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = runners.map(first_name => toUpper());
+let allCaps = runners.map((person) => {
+	return person.first_name.toUpperCase();
+});
+
 
 console.log(allCaps); 
 
@@ -72,18 +75,35 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter((runner) =>{
+		return runner.shirt_size === "L"
+
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((donation, total, index, runners) => {
+	return donation += total.donation
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// The Skinix has undergone a merger, the Bounty corp. The new name is "Skinix + Bounty"; filter find Skinix participants & make the appropriate name changes.
+
+
+
+
+let findSkinix = runners.filter((skinixCo) =>{
+	return skinixCo.company_name === "Skinix"
+});
+
+findSkinix.company_name = "Skinix + Bounty"
+console.log(findSkinix)
+
 
 // Problem 2
 
