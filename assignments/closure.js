266b47cd11cd8513d957f3c6bed0fb4d267b3021
Erin Meredith <ekmeredith@att.net>
2018-05-22 22:53:18 -0400

    completed closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,16 +32,15 @@ newCounter();
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-    let counterVar = 0;
-    const increment = () => {
-        counterVar += 1;
-        increment();
-        return counterVar;
-    };
-    const decrement
-    return increment();
-    
+   let countVar = 0;
+   return {
+    increment: () => (countVar++),
+    decrement: () => (countVar--)
+   };
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+const newCounterFactory = counterFactory();
