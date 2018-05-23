c52267130e85b3047df46faaacba218fe3a93d53
Jameson <jamesonbrown@neolivity.com>
2018-05-22 20:56:25 -0600

    Started on closure.js but didn't get far at all.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,19 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function topFunction() {
+  let x=0;
+  return () => {
+    x++
+    return x;
+  }
+}
+let something = topFunction();
+console.log(something());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -13,7 +22,17 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+let counter = 0;
+  // let car = {
+  //   "make" : "subaru",
+  //   "model" : "impreza",
+  //   "color" : "silver",
+  //   "honk" : console.log('Car honks'),
+  // };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let obj = {
+    
+  }
 };
