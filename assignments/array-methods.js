7daa6436c24d401c766f643c2ce47a2321e1570c
david <davidlayman85@gmail.com>
2018-05-22 22:07:19 -0400

    almost done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,8 +57,8 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
 
-function namer (value, index, array){
-	fullName.push(runners[index].last_name+" "+runners[index].first_name);
+function namer (value){
+	fullName.push(value.last_name+" "+value.first_name);
 }
 runners.forEach(namer);
 console.log(fullName);
@@ -66,23 +66,28 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+
+const map1 = runners.map((runner)=>{
+	allCaps.push(runner.first_name.toUpperCase());
+})
+
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter((runner)=>{
+	return runner.shirt_size === "L"
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = 0;
+let total = runners.reduce((donation, runner, index, runners) => {
+    return ticketPriceTotal += runner.donation
+}, 0)
 console.log(ticketPriceTotal);
-
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
-
-// Problem 2
-
-// Problem 3
+// Problem 1: I'm not creative, and anything I could think up wouldn't really be a challenge to me...
