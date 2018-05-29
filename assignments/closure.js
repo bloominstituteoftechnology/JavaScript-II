ab1af147a09ab79108de72aa9de00f93bf3e668b
meganfontenot <33911508+meganfontenot@users.noreply.github.com>
2018-05-28 21:37:53 -0500

    Completion of JSII

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,44 +1,43 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-const simple = () => {
-	console.log('Hello world');
+function parent() {
+  let parentage = 50
+  return "I am a parent."
+    function child() {
+      let childage = 30
+      return "I am a child."
+        function grandchild() {
+          let grandChildAge = 10
+          return "I am a grandchild."
+        }
+    }
 }
 
 // ==== Challenge 2: Create a counter function ====
-	
-const counter = (function() {
-	var n=0;
-		return function(){
-      console.log(n+=5);
-		};
-})();
-var counter1=counter;
-counter1();
-counter1();
-counter1();
-
+const counter = () => {
+  // Return a function that when invoked increments and returns a counter variable.
+    var count = 0;
+    return function () {return count += 1;}
+};
+// Example usage: const newCounter = counter();
+// newCounter(); // 1
+// newCounter(); // 2
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = function(){
-	var n=0;
-		return {
-    increment: function() {
-      console.log(n+=1)
-    },
-    decrement: function() {
-      console.log(n-=1)
-    }
-  }
-}();
-
-let count=counterFactory;
-count.increment();
-count.increment();
-count.decrement();
+const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-
-
+  let counterObject = {
+  "increment": function() {
+    var newCount = 0;
+    return function () {return newCount += 1;}
+  },
+  "decrement": function() {
+    var newCount = 0;
+    return function () {return newCount += -1;}
+  },
+}
+return counterObject
+};
