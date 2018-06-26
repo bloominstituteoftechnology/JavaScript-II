16015f53c49231a5640732b122e0791c6a3d0da3
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-06-26 16:13:39 -0600

    More progress

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -71,15 +71,17 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-
+let largeShirts = runners.filter((runner) => {
+    return runner.shirt_size === 'L';
+});
 
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-
+let ticketPriceTotal = runners.reduce(function(donation, runners) {
+    return donation = runners.donation + runners.donation;
+});
 
 console.log(ticketPriceTotal);
 
@@ -87,7 +89,36 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Every runner who donated $200 or more gets a special prize. A list of everyone who donated $200 or greater needs to be made.
+
+let largeDonors = runners.filter((runner) => {
+    if (runner.donation >= 200) {
+        return runner.first_name;
+    }
+});
+
+console.log(largeDonors);
 
 // Problem 2
+// A list of all emails and names needs to be made so all participants may be emaild. Make a list of first names and email.
+
+let emailListWithNames = [];
+runners.forEach(function(runners) {
+    emailListWithNames.push(runners.first_name + ", " + runners.email);
+});
+
+console.log(emailListWithNames);
 
 // Problem 3
+// Wordtune is sponsering each of it's racers and is matching donations. Make a list of all donors who work at Wordtune as well as the amount of their donation.
+
+let matchedDonations = [];
+
+runners.filter((runners) => {
+    if (runners.company_name === 'Wordtune') {
+        return matchedDonations.push(runners.first_name + " " + runners.last_name + ", " + runners.company_name + ", $" + runners.donation);
+    }
+});
+
+console.log(matchedDonations);
+
