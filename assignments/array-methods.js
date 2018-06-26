222a192c0617e968cfa6f9f98eea956ed26b0668
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-26 17:12:43 -0500

    completed array methods challenge five problem 1

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -88,7 +88,13 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1: Use .filter() to see if first_name starts with 'M' then create an array of just those first names.  They ran out of M's for the shirts I guess. :P
+let firstNames = [];
+
+runners.filter(runner => {if (runner.first_name.startsWith('M')){firstNames.push(runner.first_name)}})
+
+console.log(firstNames);
+
 
 // Problem 2
 
