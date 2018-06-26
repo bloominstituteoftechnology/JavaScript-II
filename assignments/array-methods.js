e1c64818ee37c13aecf77bebff45732caccf0f5e
mkerbleski <mkerbles@gmail.com>
2018-06-26 17:01:50 -0600

    Made improvments in each of the challenge but did not complete closures or arrays. Feel better about arrays tho.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,30 +54,57 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
+// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
+
+  let fullName = [];
+  runners.forEach(function(x){fullName.push(x.first_name + " " + x.last_name)})
+// fullName.push(function (runner.first_name + runner.last_name))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+
+let allCaps = runners.map(function(x) {return x['first_name'].toUpperCase()})
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+// let largeShirts = [];
+
+let largeShirts = runners.filter(function(x) {return x.shirt_size === 'L'});
+
+let largeNames = [];
+
+largeShirts.forEach(function(x) {largeNames.push(x.first_name)});
+
+console.log(largeNames);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+runners.forEach(function(x){ticketPriceTotal.push(x.donation)})
+
+let finalNum = ticketPriceTotal.reduce(function (x, y) {return x + y})
+
+console.log(finalNum);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// companies with more than 1 person
+
+
+//find the company and if they are f
+
+runners.forEach(function(x){})
+
+
 
 // Problem 2
+
+//contact info
 
 // Problem 3
--- assignments/closure.js
+++ assignments/closure.js
@@ -2,18 +2,43 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 
+
+
+
+
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
-
+};
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
+  let count = 0;
+
+  let return () {
+      let  plus = () => {
+        count++;
+      return count;   }
+
+      let minus = () => {
+         count--
+      return count;
+      }
+    }
+};
+
+let x = counterFactory();
+
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+console.log(x(return))
+
+
+
+
+
+console.log("test")
