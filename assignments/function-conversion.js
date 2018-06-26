972e8dc63fc6c0d44ca075e5f04623861769891c
keegn <keegan.t.burkett@gmail.com>
2018-06-26 15:33:35 -0500

    added console log test for anotherFunction

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,6 +4,7 @@ let myFunction = () => {};
 
 // we could also include curly braces and the return keyword, but here i omitted it 
 let anotherFunction = (param) => param;
+console.log(anotherFunction('hey'))
 
 let add = (param1, param2) => param1 + param2;
 console.log(add(1,2)); // 3
@@ -12,5 +13,5 @@ let subtract = (param1, param2) => param1 - param2;
 console.log(subtract(1,2)); // -1
 
 exampleArray = [1,2,3,4];
-const triple = exampleArray.map((num) => num * 3;
+const triple = exampleArray.map((num) => num * 3);
 console.log(triple); // [3, 6, 9, 12]
