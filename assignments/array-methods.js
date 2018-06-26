0d9a9bc32e287c73a90369852117f5d09f85c24e
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-06-26 12:31:09 -0700

    Luis F. Salazar

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,6 +56,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+a
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
