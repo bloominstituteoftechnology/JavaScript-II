7c58b44392c51bc5cb1cbcdd4868c03d779e4970
Fred <sohnf90@yahoo.com>
2018-06-26 13:33:09 -0700

    Completed the function conversion task

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -2,22 +2,42 @@
 
 // let myFunction = function () {};
 
+let myFunction = () => {};
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = param => param;
+console.log(anotherFunction(1));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
+let add = (param1, param2) => param1 + param2;
+console.log(add(1, 2));
+
+// New Problem
+
+let subtract = function(param1, param2) {
   return param1 - param2;
 };
-subtract(1,2); //?
+subtract(1, 2); //?
 
-exampleArray = [1,2,3,4];
+//Answer
+
+subtract = (param1, param2) => param1 - param2;
+console.log(subtract(5, 1));
+
+// New Problem
+
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
