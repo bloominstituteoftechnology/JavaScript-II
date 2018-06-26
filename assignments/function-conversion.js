e46515aeec926dad717c52829754e84c802ca796
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-06-26 15:11:59 -0600

    Bryce Evans

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,23 +1,43 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+let myFunction = () => {return};
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = (param) => {return param};
+
+console.log(anotherFunction('my String'));
+console.log(anotherFunction(5));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {return param1 + param2};
+
+console.log(add(4,5));
+
 let subtract = function (param1, param2) {
   return param1 - param2;
 };
-subtract(1,2); //?
+console.log(subtract(1,2)); //?
+
+let subract = (param1, param2) => {return param1 - param2};
+console.log(subtract(2,1));
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+for (let x of exampleArray) {
+  console.log(x);
+}
+
+let triple = exampleArray.map((num) => {return num * 3});
+console.log(triple);
