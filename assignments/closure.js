4022ad30e5b1472276d4e74e4ad4f452dbcab25f
Alberto-Funk <laptopreneur@gmail.com>
2018-06-26 13:55:52 -0700

    WIP
    - Finished function conversion, closure
    - Need to finish array-methods

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,38 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function add (x) {
+  let addMe = x;
+
+  return function (y) {
+    return x + y;
+  }
+}
+
+let adderFour = add(4);
+let adderFive = add(5);
+let adderten = add(10);
+adderten(4);
+
+
+
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return function () {
+    return ++counter;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+
+
+
+
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
@@ -16,4 +40,14 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let increment = 0;
+  let decrement = 0;
+  return {
+    more: function () {
+      return ++increment;
+    },
+    less: function () {
+      return --decrement;
+    }
+  }
 };
