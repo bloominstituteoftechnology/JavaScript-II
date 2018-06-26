528b971884712a1d75f7aa9074a9f6e7bd60aeb0
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-26 16:22:52 -0500

    first closure challenge complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let myName = 'Kyle';
 
+function saysHi() {
+console.log(`Hi, my name is ${myName}!`)
+};
+
+myName = 'Jimmy'
+
+saysHi();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
