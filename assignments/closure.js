3aec71edadb532e048c1c9fe5aa58c5b2f85759b
Trevor <trevorfehrman@gmail.com>
2018-06-26 13:47:15 -0700

    Trying to understand closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function testing (param) {
+  let result;
+  function newFunc(param) {
+    let a = 3;
+    result = a * param;
+  }
+  newFunc(param);
+  console.log(result);
+}
+testing(3);
+testing(5);
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return function() {
+    return counter += 1;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
