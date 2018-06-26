86ede43cd01cbfc72fafdc3338b2a544843facf5
bri79319 <bri79319@gmail.com>
2018-06-26 18:57:09 -0400

    JS
    Array 1-4 challenges done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,11 +67,18 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+
+largeShirts = runners.filter(function(element){
+  return element.shirt_size === 'L';
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+
+ticketPriceTotal = runners.reduce((acc, next) => acc + next.donation, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
