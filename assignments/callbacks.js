c63c2512f17da94ef9dd6b6740e3e22bc2379a55
Stephanie Banks <thisgirlcancode@gmail.com>
2018-06-26 17:22:16 -0500

    finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,28 +2,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+cb(arr[0])
 }
+firstItem(items,console.log)
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+cb(arr.length)
+
 }
+getLength(items,console.log)
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+cb(arr[arr.length-1])
 }
-
+last(items,console.log)
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+cb(x+y)
 }
+sumNums(5,2,console.log)
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+cb(x*y)
 }
+multiplyNums(2,7,console.log)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+
+let hasItem = true;
+for (let i=0; i<list.length; i++) {
+  if (list[i] === item) {
+     hasItem = true; 
+
+    }
+      else hasItem = false;
+    }
+    cb(hasItem)
+  }
+  
+  
+contains('Gum', items, console.log)
 
 /* STRETCH PROBLEM */
 
