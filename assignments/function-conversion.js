1cc0c57d2a45fa566eeef02414a09e97148ddda7
mkerbleski <mkerbles@gmail.com>
2018-06-26 14:31:13 -0600

    Completed function-converstion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -2,22 +2,23 @@
 
 // let myFunction = function () {};
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
+let anotherFunction = param => param;
 
-// let add = function (param1, param2) {
+console.log(anotherFunction('test'));
+
+
+let add = (param1, param2) => (param1 + param2);
+console.log(add(2 , 8));
 //   return param1 + param2;
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
-  return param1 - param2;
-};
-subtract(1,2); //?
+let subtract = (param1, param2) => (param1 - param2);
+console.log(subtract(17, 2));
+//   return param1 - param2;
+// };
+// subtract(1,2); //?
 
 exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+const triple = exampleArray.map((num) => (num*3));
+console.log(triple);
