c71b74c60af79f7e0fa679020f23f86475b84d77
jaymzsocool <james.mcraejr@gmail.com>
2018-06-26 19:01:58 -0400

    Still needs closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,7 +5,14 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  function increase() {     
+    console.log(num++);
+    return num++;
+  }  
+  return increase;  
 };
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -16,4 +23,17 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+  let increment = () => {
+    count++;
+    return count;
+  }
+  let decrement = () =>{
+    count--;
+    return count;
+  }
+  }
+
+
 };
