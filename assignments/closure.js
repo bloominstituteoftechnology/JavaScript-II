d56ed72d2d1964f0d6fdc5f615911feaaa766192
jordan-massingill <jordan_massingill@hotmail.com>
2018-06-26 17:21:28 -0500

    Closures
    Completed 2 out of 3 closure challenges
    still working on closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let position = "Seated";
+let state = "in anticipation.";
 
+let status = () => {return `${position} ${state} Hooray!`;};
+
+console.log(status());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
