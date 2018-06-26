7aa68a44e1e0e18947294b6c9dc0783a877443b6
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-26 16:48:12 -0500

    completed challenge 3 in closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,8 +15,8 @@ const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   let total = 0;
   return function () {
-    total++
-    return total
+    total++;
+    return total;
   }
 };
 // Example usage: const newCounter = counter();
@@ -27,6 +27,19 @@ const counter = () => {
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
+  let total = 0;
+  return {
   // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+    increment: function (){
+      total++;
+      return total;
+    },
+// `decrement` should decrement the counter variable and return it.
+    decrement: function () {
+      total--;
+      return total;
+    }
+  }
 };
+
+const counter = counterFactory();
