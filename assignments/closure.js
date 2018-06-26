a5316669a15374e6e9fb5af8effc304783650e51
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-06-26 15:52:06 -0700

    Started the closures.js objectives.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+let a = 5;
+function myClosure() {
+  console.log(a+a);
+}
+myClosure();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
