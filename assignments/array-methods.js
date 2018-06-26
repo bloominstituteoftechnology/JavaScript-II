0db2e4728fb0b89283b178e004908f27862050bb
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-06-26 13:11:39 -0600

    Finished array-methods and wrote up creative problems

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,98 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(runner => {
+    const name = `${runner['first_name']} ${runner['last_name']}`
+    fullName.push(name);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(runner => {
+    return runner['first_name'].toUpperCase();  
+})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(runner =>{
+    return runner['shirt_size'] === "L";
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+
+let donationsArr = runners.map(runner => {
+    return runner['donation']
+});
+let ticketPriceTotal = donationsArr.reduce((total,current) =>{
+    return total + current;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Some of the racers have been very kind to donate more than $150, which is so cool! 
+//The race officials want to send the SuperDonors an extra gift to thank them for their large donation. 
+//Filter all racers that contributed $150 or more to the race.
+
+let superDonors = runners.filter(runner => {
+    return runner['donation'] >= "150";
+})
+console.log(superDonors);
 
 // Problem 2
+//The racing organization wants to know how many shirts of each size they need to order before the race. 
+//Use filter to output each size and how many of each are needed. 
 
+let xs = runners.filter(runner =>{
+    return runner['shirt_size'] === "XS";
+})
+
+let small = runners.filter(runner => {
+    return runner['shirt_size'] === "S";
+})
+
+let medium = runners.filter(runner =>{
+    return runner['shirt_size'] === "M";
+})
+
+let large = runners.filter(runner =>{
+    return runner['shirt-size'] === "L"
+})
+
+let xl = runners.filter(runner => {
+    return runner['shirt_size'] === "XL"
+})
+
+let twoXL = runners.filter(runner => {
+    return runner['shirt_size'] === "2XL"
+})
+
+let threeXL = runners.filter(runner => {
+    return runner['shirt_size'] === "3XL"
+})
+
+let sizes = [
+    {"XS": xs.length - 1 },
+    {"S": small.length - 1},
+    {"M": medium.length - 1},
+    {"L": large.length - 1},
+    {"XL": xl.length - 1},
+    {"2XL": twoXL.length - 1},
+    {"3XL": threeXL.length - 1}
+]
+
+console.log(sizes);
 // Problem 3
+//The organization now wants an array of each racer and their contact info for permanent database storage. Use forEach
+let contactInfo = [];
+runners.forEach(runner => {
+    const contact = `Name: ${runner['first_name']} ${runner['last_name']}, Email: ${runner['email']}`
+    contactInfo.push(contact);
+});
+console.log(contactInfo);
