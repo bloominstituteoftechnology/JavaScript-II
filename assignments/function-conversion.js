8ded45b4767c0a4b343d3193f52788d38e440394
bri79319 <bri79319@gmail.com>
2018-06-26 16:15:53 -0400

    JS
    function conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,10 +1,15 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+let myFunction = () => {};
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = (param) => {
+  return `${param}`;
+}
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
