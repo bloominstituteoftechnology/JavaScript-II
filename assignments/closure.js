08c224588b42a52ac3150d032f0935a184626bfb
Ashwin S <aswinjeganath@gmail.com>
2018-06-27 01:28:20 +0530

    Complete all challenges in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,19 +1,48 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function person(firstName, lastName) {
+  return {
+    getFirstName: function getFirstName() {
+      return firstName;
+    },
+    getLastName: function getLastName() {
+      return lastName;
+    },
+    getFullName: function getFullName() {
+      return firstName + " " + lastName;
+    }
+  };
+}
+console.log(person("Bob", "Jr").getFirstName());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => ++count;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => ++count,
+    decrement: () => --count
+  };
 };
+
+const sophiticatedCounter = counterFactory();
+console.log(sophiticatedCounter.increment());
+console.log(sophiticatedCounter.increment());
+console.log(sophiticatedCounter.increment());
+console.log(sophiticatedCounter.decrement());
+console.log(sophiticatedCounter.decrement());
