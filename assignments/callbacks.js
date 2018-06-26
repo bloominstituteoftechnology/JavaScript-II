3dc7ef1bd4a0903e1e9614b05d0367b49645184a
johnoro <jjcummins102@gmail.com>
2018-06-26 15:37:35 -0400

    Converted callbacks.js functions into ES6 syntax.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,31 +1,16 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-  cb(arr[0]);
-}
+const firstItem = (arr, cb) => cb(arr[0]);
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-  cb(arr.length);
-}
+const getLength = (arr, cb) => cb(arr.length);
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  cb(arr.length - 1);
-}
+const last = (arr, cb) => cb(arr.length - 1);
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  cb(x + y);
-}
+const sumNums = (x, y, cb) => cb(x + y);
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  cb(x * y);
-}
+const multiplyNums = (x, y, cb) => cb(x * y);
 
-function contains(item, list, cb) {
+const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   cb(list.includes(item));
@@ -33,7 +18,7 @@ function contains(item, list, cb) {
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
