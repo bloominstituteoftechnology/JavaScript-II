5a86cffd3b0dbe2532e4dec8a086ab3a628e927f
keegn <keegan.t.burkett@gmail.com>
2018-06-26 16:51:17 -0500

    completed closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -11,6 +11,12 @@ const counter = () => {
     return count;
   }
 };
+// creating a variable that is set to the return value (function) of counter () which is newCounter()
+let incremetor = counter();
+// if we console.log(incrementor) js would return function newCounter () {count += 1; return count}; 
+// if we call incrementor() then the count variable will look outward and find out what value count is currently set to, then it will update that value by 1 then return the updated number
+console.log(incremetor())
+console.log(incremetor())
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -21,4 +27,23 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 0; 
+  return {
+    increment: function() {
+      counter += 1;
+      return counter;
+    },
+    decrement: function() {
+      counter -= 1;
+      return counter;
+    }
+  }
 };
+// assiging the closure to a new function that will have access to the object methods and the counter variable;
+let cFunc = counterFactory()
+// incrementing the counter three times 
+console.log(cFunc.increment()) // 1 
+console.log(cFunc.increment()) // 2
+console.log(cFunc.increment()) // 3
+// decremething the counter
+console.log(cFunc.decrement()) // 2
