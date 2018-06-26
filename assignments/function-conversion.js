ed3f3fe537f1a638359e04894e68035f3370e7ef
Grant Reighard <grantreighard@Grants-iMac.fios-router.home>
2018-06-26 15:41:14 -0400

    Finished function-conversion.js.
    - Now on to array-methods.js.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -10,18 +10,35 @@ const myFunction = () => {
 //   return param;
 // };
 
+const anotherFunction = param => {
+  return param;
+}
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-let subtract = function (param1, param2) {
+const add = (param1, param2) => {
+  return param1 + param2;
+}
+console.log(add(1, 2));
+
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+// subtract(1,2); //?
+
+const subtract = (param1, param2) => {
   return param1 - param2;
-};
-subtract(1,2); //?
+}
+console.log(subtract(1, 2));
 
 exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
