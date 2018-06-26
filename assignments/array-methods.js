2753d8261980a3070ecc331bcf2fb1c01cc67b36
Brandon White <bwhite06@stac.edu>
2018-06-26 17:19:59 -0400

    learning foreach

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,14 +54,23 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
+findrunner();
 console.log(fullName);
+function findrunner(){
+  runners.forEach(function(i) {
+    fullName.push(i.first_name);
+    fullName.push(i.last_name);
+
+  });
+  JSON.stringify(fullName);
+}
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
