60e5e2f0503887bd0d96cfd1589efd70f224662e
Fred <sohnf90@yahoo.com>
2018-06-27 00:15:01 -0700

    Only challenge 5 for array-methods and challenge 3 for closure left

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let dog = 'English Bulldog'
+function house (where) {
+  let cat = 'Maine';
+  dog = 'Corgi'; 
+  return dog + where
+};
 
+  dog = 'Labrador';
+//console.log(cat);
+//console.log(house(' in the house'));
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let add= 0;
+  return () => (++add);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+newCounter();
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
