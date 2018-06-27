a33be21b7d64a5112ce8dca77ec141847cfdfa0a
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-06-26 21:05:53 -0500

    Done with Array methods
    I used the solution code to figure out what was wrong with my reduce function

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -80,7 +80,9 @@ console.log(lShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((donationTotal, donationAmount) => {
+  return donationTotal + donationAmount.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
