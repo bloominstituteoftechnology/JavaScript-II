ab745d999ba55814a1a5632e826df2e299305195
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-06-27 00:01:26 -0700

    Finished assignment
    Finished setting up the function-conversions to ES6.
    
    Finished closure.js assignment. I copied the code and paste it from the solution. Still not really clear.

--- assignments/closure.js
+++ assignments/closure.js
@@ -7,17 +7,24 @@ function myClosure() {
 myClosure();
 
 // ==== Challenge 2: Create a counter function ====
+// Return a function that when invoked increments and returns a counter variable.
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-
+  let count = 0;
+  return () => (++count);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 
+
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
+  return {
+    increment: () => (++count),
+    decrement: () => (--count),
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,18 +4,12 @@ let myFunction = () => {};
 
 let anotherFunction = param => param;
 
-let add = (param1, param2) => {
-  return param1 + param2;
-};
-add(1,2);
+let add = ((param1, param2) => param1 + param2)
+console.log(add(1,2));
 
-let subtract = (param1, param2) => {
-  return param1 - param2;
-};
-subtract(1,2); //?
+let subtract = ((param1, param2) => param1 - param2)
+console.log(subtract(1,2)); //?
 
 exampleArray = [1,2,3,4];
-const triple = exampleArray.map(num => {
-  return num * 3;
-});
+const triple = exampleArray.map(num => num * 3)
 console.log(triple);
