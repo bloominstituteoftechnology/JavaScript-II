7632b7adf5291918b34ee3aefbd939867d4c8383
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-06-26 21:09:33 -0500

    Closures and Gunction Method are in
    I pasted the solution code into both of the assignments just so that I can see how they work, I plan on revisiting and studying these

--- assignments/closure.js
+++ assignments/closure.js
@@ -6,18 +6,36 @@ function myFunction(){
 }
 console.log(myFunction())
 
-// ==== Challenge 2: Create a counter function ====
+/// ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => (++count);
   // Return a function that when invoked increments and returns a counter variable.
 };
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+const newCounter = counter();
+newCounter() // 1
+newCounter() // 2
+newCounter() // 3
+newCounter() // 4
+newCounter() // 5
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
+  return {
+    increment: () => (++count),
+    decrement: () => (--count),
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+const newCounterFactory = counterFactory();
+newCounterFactory.increment(); // 1
+newCounterFactory.decrement(); // 0
+newCounterFactory.decrement(); // -1
