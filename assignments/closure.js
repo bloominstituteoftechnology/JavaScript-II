4d449255aff7c7f55bc4f3d21a4a9f01bf66e8ee
Charles Boegner <cmikeb@gmail.com>
2018-06-28 19:50:53 -0400

    Charles Boegner: JavaScript-II
    Completed closure section

--- assignments/closure.js
+++ assignments/closure.js
@@ -14,18 +14,24 @@ console.log(closeit10());
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+function counter() {
   // Return a function that when invoked increments and returns a counter variable.
    counterUp = 0;
 
   return function () {
-    counterUp += 1;
+
+    counterUp = counterUp + 1;
+    
     return counterUp;
   };
 };
-counter();
-counter();
-counter();
+let bob = counter();
+
+console.log(bob());
+console.log(bob());
+console.log(bob());
+console.log(bob());
+console.log(bob());
 
 
 // Example usage: const newCounter = counter();
@@ -35,7 +41,26 @@ counter();
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+
+let count = 0;
+
+return{
+  increment: () => (++count),
+  decrement: () => (--count)
+};
+};
+
+let counterific = counterFactory();
+
+console.log(counterific.increment());
+console.log(counterific.increment());
+console.log(counterific.increment());
+console.log(counterific.increment());
+
+console.log(counterific.decrement());
+console.log(counterific.decrement());
+console.log(counterific.decrement());
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+
