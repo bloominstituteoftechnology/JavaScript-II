545ed764dc2ff410438074ccc35918ae8f7734c1
SurrealityDD <aurel.surrealitydesign@gmail.com>
2018-06-29 15:54:29 -0700

    062918

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -72,9 +72,13 @@ return (runners.first_name.toUpperCase());
 console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
-
+let largeshirtRunners = runners.filter (function (runners) {
+    return runners.shirt_size == "L"; 
+  
+  
+  });
+  console.log(largeshirtRunners);
+  
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
