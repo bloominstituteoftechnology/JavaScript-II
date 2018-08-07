0dcfa11e00660e0e51205e4d02873e89785740f8
lndubose <lndubose13@gmail.com>
2018-08-07 16:35:47 -0500

    Closure.js done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function spiderman() {
+  let name = "Spiderman";
+  let hasWeb = "does";
+  function web() {
+    let secretIdentity = "Yes"
+    console.log(`${name} ${hasWeb} have web.`);
+    function identity() {
+      console.log(`${secretIdentity} ${name} has a secret identity.`)
+    }
+    identity();
+  }
+  web();
+}
+
+spiderman();
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+  let variable = 0;
+  return function plus() {
+    variable++;
+    return variable; 
+  }
+  plus();
+}
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
