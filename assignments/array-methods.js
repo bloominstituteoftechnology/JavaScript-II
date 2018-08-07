b1ac837a8a2292afdd8df2a18818a12bfa35602f
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-08-07 15:22:38 -0700

    Finished all normal tasks - Jonathan Laluces

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,91 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(element) {
+  fullName.push(element["first_name"] + "," + element["last_name"]);
+})
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+const newCapNames = runners.map(function(item) {
+  allCaps.push(item["first_name"].toUpperCase());
+})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+const shirtSizeFilter = runners.filter((data) => {
+  if (data["shirt_size"] === "L") {
+    return data["shirt_size"] = "L";
+  }
+})
+console.log(shirtSizeFilter);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+
+const ticketPriceTotal = runners.reduce((sum, order) =>{
+  return sum += order.donation;
+}, 0)
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
+
+
+
+
+
 // Problem 1
+
+// The event director needs all participants' full names and donation so he can personally send them thank-you emails. Combine the email and donation amount into a new array called thankYouInformation
+let thankYouInformation = [];
+runners.forEach(function(element) {
+  thankYouInformation.push(element["first_name"] + "," + element["last_name"] + ", donated " + "$" + element["donation"] );
+})
+
+console.log(thankYouInformation);
+
+
+
+
+
+
+
+
+
 
 // Problem 2
 
+//The event director, needs the company names of each contestant capitalized to look "professional", as he calls it. Make sure the company names are in alphabetical order, and next to each company name, the corresponding person's name.
+
+let companyTeeShirts = [];
+runners.map(function(element) {
+  companyTeeShirts.push(element["company_name"].toUpperCase() + "->" + element["first_name"] + "-" + element["last_name"]);
+})
+
+console.log(companyTeeShirts.sort());
+
+
+
+
+
+
+
+
+
+
+
 // Problem 3
+// The event director got word that the CEO at Skinix wants to see which of his employees skipped work to do the 5k. You know you shouldn't, but its also your second day in the company.
+
+let workSkippers = runners.filter((data) => {
+  if (data["company_name"] === "Skinix") {
+    return data["company_name"] = "Skinix";
+  }
+})
+
+console.log(workSkippers);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let userName = 'GolemSlayer552';
 
+function welcomeUser() {
+  console.log("Welcome back to the Dark Dungeon " + userName + "!");
+}
+
+welcomeUser(userName);
 
 // ==== Challenge 2: Create a counter function ====
+let countedTimes = 0;
+
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  countedTimes++;
+  console.log(countedTimes);
 };
+
+counter();
+console.log("You've invoked me once. Hurray.");
+counter();
+console.log("You've invoked me twice, I don\'t think you need to do it anymore haha..");
+counter();
+console.log("You've invoked me three times, please stop.");
+counter();
+console.log("You've invoked me one too many times. User, this isn\'t funny anymore.");
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
