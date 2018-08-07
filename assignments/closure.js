cb943a7e9583fb34a28b85b44ef1a9b4239fa745
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-07 22:20:58 +0100

    done the closure stretch counter factory

--- assignments/closure.js
+++ assignments/closure.js
@@ -41,6 +41,9 @@ console.log(newCounter()); // 2
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
+  let count = 0;
   // `increment` should increment a counter variable in closure scope and return it.
+  const increment = function() {count++; return count};
   // `decrement` should decrement the counter variable and return it.
+  const decrement = function() {count; return count};
 };
