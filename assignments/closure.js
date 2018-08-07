c6b34940f6df97d6aa8a6e092423dc917f8fd835
Clint Kunz <clintkunz@gmail.com>
2018-08-07 16:32:50 -0600

    closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,25 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let name = 'SPIDA';
 
+function dunkIt() {
+  console.log(name + ', with another slam!');
+}
+
+dunkIt();
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+let counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+  let value = 0;
+  return () => ++value; 
+} 
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
