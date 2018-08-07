c1456698beee8bb911d224e892d84116ad0fac5c
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-07 15:57:42 -0400

    Completed callbacks.js, still need to do stretch goals

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,30 +2,65 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr);
 }
+
+function first(arr)  {
+  console.log(arr[0]);
+}
+
+firstItem(items, first);
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+function howLong(arr)  {
+  console.log(arr.length);
+}
+
+getLength(items, howLong);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr);
 }
+
+function lastItem(arr)  {
+  console.log(arr[arr.length - 1]);
+}
+
+last(items, lastItem);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+function add(a, b)  {
+  console.log(a + b);
+}
+
+sumNums(3, 5, add);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+function multiply(a, b)  {
+  console.log(a * b);
+}
+
+sumNums(3, 5, multiply);
+
+/* STRETCH PROBLEMS */
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
-/* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
