e4a0999e0c804c98231a90095f987190d626663f
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-08-07 18:28:35 -0400

    Task 4: -Completed the exercises in closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,41 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function whatFruit() {
+  let person = "I";
+
+  function eatTheFruit() {
+    let action = "eat";
+    console.log(person + " like to eat.");
+
+    function fruit() {
+      console.log(person + " like to " + action + " bananas!");
+    }
+
+    fruit();
+  }
+
+  eatTheFruit();
+}
+
+whatFruit();
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// Return a function that when invoked increments and returns a counter variable.
+
+const counter = (() => {
+  let count = 0;
+  return function () {
+    count += 1;
+    return count
+  }
+})();
+
+counter();
+counter();
+counter();
+counter();//returns 4
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
