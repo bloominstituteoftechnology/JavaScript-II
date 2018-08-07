4059e545c27046664755045f4c53550c67cbff4b
Kevin Nguyen <kevinn858@gmail.com>
2018-08-07 14:32:58 -0700

    Finished Task 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,86 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(runner => fullName.push({"first_name":runner.first_name, "last_name":runner.last_name}));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(runner => runner.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(runner => runner.shirt_size=="L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal.push(runners.reduce((ticketSales, runner) => {return ticketSales+=runner.donation}, 0));
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//  Return an array of the unique shirt sizes of the runners
+let findPossibleSizes = function(runners)
+{
+    let copy = runners;
+    let sizes = [];
+    while(copy.length>0)
+    {
+        const currentShirtSize = copy[0].shirt_size;
+        sizes.push(currentShirtSize);
+        let temporary = [];
+        copy.forEach(runner =>
+        {
+            if(runner.shirt_size!=currentShirtSize)
+                     {
+                        temporary.push(runner);
+                     }
+        })
+        copy = temporary;
+    }
+    return sizes;
+}
+console.log(findPossibleSizes(runners));
 
 // Problem 2
+// Return an array showing how many of each shirt size is needed.
+let computeSizesArray = function(runners)
+{
+    let sizesArray = [];
+    sizes = findPossibleSizes(runners);
+    let copy = runners;
+    while(copy.length>0)
+    {
+        const currentShirtSize = copy[0].shirt_size;
+        let temporaryArray = [];
+        let accumulator = 0;
+        copy.forEach(runner =>
+                     {
+                        if(runner.shirt_size!=currentShirtSize)
+                        {
+                            temporaryArray.push(runner);
+                        }
+                         else
+                         {
+                            accumulator++;
+                         }
+                     })
+        sizesArray.push({'shirt_size':currentShirtSize, 'shirts_needed':accumulator});
+        copy = temporaryArray;
+    }
+    return sizesArray;
+}
+console.log(computeSizesArray(runners));
 
 // Problem 3
+//Return an array of just e-mails for a universal notification list
+let createEmailList = function(runners)
+{
+    return runners.map(runner => runner.email);
+}
+console.log(createEmailList(runners));
