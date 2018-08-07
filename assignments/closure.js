f76002f4adacf04df635e3710328e9edb6de7f86
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-08 00:50:43 +0100

    rescoped some variables

--- assignments/closure.js
+++ assignments/closure.js
@@ -37,10 +37,9 @@ console.log(newCounter()); // 1
 console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
-
+let count = 0;
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = (startNumber = 0) => {
-  count =  startNumber;
+const counterFactory = () => {
   const counter = {
     // `increment` should increment a counter variable in closure scope and return it.
     increment: () => {count++; return count},
