936dc118665da2a926c05e303a94294445ce6a79
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-07 17:30:48 -0400

    accidentally did #1 with map instead of forEach, update to fix

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,10 +55,9 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
-const fullName = runners.map(function(item) {
-  return (`${item.first_name} ${item.last_name}`);
+const fullName = runners.forEach(function(element) {
+  console.log(`${element.first_name} ${element.last_name}`);
 });
-console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
