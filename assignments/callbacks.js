79a32cd62b41db79347b2c63c574e6ff0701e125
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-08-07 16:32:09 -0500

    task two completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,31 +1,53 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
+   return cb(arr[0]);
 }
-
+firstItem(items, function(first) {
+  console.log(first);
+});
+//------------------------------------------//
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr[0]);
 }
-
+getLength(items, function() {
+  console.log(items.length);
+});
+//------------------------------------------//
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[3]);
+}
+last (items, function(final) {
+  console.log(final);
+});
+//------------------------------------------//
+function sumNums(x, y, cb) {
+  return cb(x,y);
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+function add (x,y){
+  console.log (x + y)
 }
+sumNums(2,4, add);
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+function multiply (x,y){
+  console.log (x * y);
 }
+sumNums(2,4, multiply);
 
+//------------------------------------------//
 /* STRETCH PROBLEM */
+
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
+function contains(item, list, cb) {
+  
+}
+
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
