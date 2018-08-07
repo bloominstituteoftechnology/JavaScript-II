9120429b40a0ddc99ee47593f1e330222f48f947
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-07 15:53:58 -0700

    finish homework and all stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,34 +1,108 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
+// -----------------------------------
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  console.log(cb(arr));
 }
+
+let first = function(arr){
+  return arr[0];
+}
+
+firstItem(items, first);
+// ----------------------------------- end of section first
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  console.log(cb(arr));
 }
+
+let length = function(arr){
+  return arr.length;
+}
+
+getLength(items, length);
+// ----------------------------------- end of section getLength
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  console.log(cb(arr));
 }
+
+let getLast = function(arr){
+  return arr[arr.length - 1];
+}
+
+last(items, getLast);
+// ----------------------------------- end of section getLast
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  console.log(cb(x, y));
 }
+
+let addNums = function(a, b){
+  return a + b;
+}
+
+sumNums(2, 2, addNums);
+// ----------------------------------- end of section addNums
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  console.log(cb(x, y));
 }
+
+let timesNums = function(x, y){
+  return x * y;
+}
+
+multiplyNums(3, 3, timesNums);
+// ----------------------------------- end of section timesNums
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  console.log(cb(item, list));
 }
 
+let checkItem = function(item, list){
+  if (list.indexOf(item) !== -1){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+contains("Notebook", items, checkItem); //true
+contains("frog", items, checkItem); //false
+// ----------------------------------- end of section checkItem
+
+
 /* STRETCH PROBLEM */
+const items2 = ['wood', 'straw', 'brick', 'wood', 'leaves', 'leaves', 'wood'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(array);
 }
+
+let removeDups = function(arr){
+  arr1 = arr.sort();
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr1[i] !== arr1[i + 1]){
+      arr2.push(arr[i])
+    }
+  }
+  return arr2;
+}
+removeDuplicates(items2,removeDups);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function greeting(){
+  let name = "dude";
 
+  function greetName(){
+    console.log("What up! " + name)
+  }
+  return greetName();
+}
+greeting()
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let n = 0;
+  return function(){
+    n++;
+    return n;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+newCounter(); //1
+newCounter(); //2
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -17,4 +33,19 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let newObj = {};
+  let x = 0;
+  newObj.increment = function(){
+    return function(){
+      x++;
+      return x;
+    }
+  }
+  newObj.decrement = function(){
+    return function(){
+      x--;
+      return x;
+    }
+  }
+  return newObj;
+}
