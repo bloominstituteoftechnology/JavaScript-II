4396dc75d3fb7d404278d9e5afbbd96f632c6dfb
iAmAdamReid <adam@adamreid.me>
2018-08-07 16:03:04 -0700

    Adam Reid - JavaScript II - Callbacks, Array Methods, and Closures

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,28 +2,62 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
+
+firstItem(items, function(first){
+  console.log(first);
+});
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
-function last(arr, cb) {
+getLength(items, function(length){
+  console.log(length);
+});
+
+function lastItem(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+lastItem(items, function(last){
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(4, 8, function(sum){
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x + y);
 }
+
+multiplyNums(4, 8, function(product){
+  console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
+  if(list.indexOf(item) == -1){
+    return cb(false);
+  } else {
+    return cb(true);
+  }
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+contains('Gum', items, function(result){
+  console.log(result);
+});
 
 /* STRETCH PROBLEM */
 
@@ -32,3 +66,4 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function airplane(){
 
+  let model = 'Boeing 747';
+
+  function takeoff(){
+    let departure = 'Los Angeles International';
+    console.log('The ' + model + ' takes off from ' + departure + ' airport.');
+
+  function landing(){
+    let destination = 'New York La Guardia';
+    console.log('The ' + model + ' touches down in ' + destination + ' airport.');
+  }
+  landing();
+  }
+  takeoff();
+}
+
+airplane();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let n = 0;
+  return function(){
+    n += 1;
+    console.log(n);
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+newCounter(); // 3
+newCounter(); // 4
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
