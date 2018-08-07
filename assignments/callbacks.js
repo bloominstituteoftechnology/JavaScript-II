e00624b972952b0122dc0a653be8b3b901f8b54f
brian ruff <thelonewolf198927@gmail.com>
2018-08-07 19:01:29 -0400

    all exercises done with 10 mins till standup

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,4 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Notebook'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
@@ -43,4 +43,15 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  console.log(cb(array));
 }
+
+removeDuplicates(items, (items) => {
+  let unique = [];
+  for(let i = 0; i < items.length; i++){
+    if( !(unique.includes(items[i])) ){
+      unique.push(items[i]);
+    }
+  }
+  return unique;
+});
