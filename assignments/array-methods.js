0d1754a9154a59f6d94c2d00b34f801ca126ea13
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-07 17:24:44 -0500

    array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,66 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(function(item) {
+    fullName.push(item.first_name + " " + item.last_name);
+})
+
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+
+allCaps = runners.map(function(item) {
+    return item.first_name.toUpperCase();
+})
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+
+largeShirts = runners.filter((data) =>{
+    return data.shirt_size === "L";
+})
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+
+ticketPriceTotal = runners.reduce((theReducer, item) =>{
+    return theReducer += item.donation;
+}, 0)
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1, lists donors who gave over 100
 
-// Problem 2
+let whales = runners.filter((data) => {
+    return data.donation > 100;
+})
 
-// Problem 3
+console.log(whales);
+
+// Problem 2, sort runners by last name
+
+let runnersSorted = runners.map((item) =>{
+    return item.last_name;
+})
+
+console.log(runnersSorted.sort());
+
+// Problem 3, donation avg
+
+let donoTotal = runners.reduce((theReducer, item) =>{
+    return theReducer +=item.donation;
+}, 0)
+let donoAvg = donoTotal / runners.length;
+console.log("Average donation per runner: " + donoAvg);
