9e46e17824cb408750d2cbee76251c836e2d8def
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-07 23:28:08 +0100

    fixed up the return value of counter factory. and refactored one of the functions

--- assignments/closure.js
+++ assignments/closure.js
@@ -39,11 +39,19 @@ console.log(newCounter()); // 2
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (startNumber = 0) => {
+  count =  startNumber;
+  const counter = {
+    // `increment` should increment a counter variable in closure scope and return it.
+    increment: () => {count++; return count},
+    // `decrement` should decrement the counter variable and return it.
+    decrement: function() {count--; return count}
+  };
   // Return an object that has two methods called `increment` and `decrement`.
-  let count = 0;
-  // `increment` should increment a counter variable in closure scope and return it.
-  const increment = function() {count++; return count};
-  // `decrement` should decrement the counter variable and return it.
-  const decrement = function() {count--; return count};
+  return counter;
 };
+
+let myCounter1 = counterFactory(20);
+console.log(myCounter1.increment());
+console.log(myCounter1.increment());
+console.log(myCounter1.decrement());
