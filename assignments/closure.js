be637e2197bea628c85d3e79266c6d9ec68e337f
Michael Agard <mdagard@gmail.com>
2018-08-07 18:42:16 -0400

    I'll spend the rest of the evening learning about counters.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+closureExample() {
+  let name = 'Michael'
+  oneIn() {
+    console.log(name);
+  }
+}
+closureExample();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
