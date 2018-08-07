44a5dae5c8cc2c508cd1a85caadca58f13255880
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-07 21:35:04 +0100

    done task 4

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,44 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function characterActions(action) {
+  let charName = "bob";
+  function attack() {
+    let damage = 12;
+    console.log(charName + " deals " + 12 + " damage to their enemy!");
+  }
 
+  function run() {
+    console.log(charName + " ran away safely");
+  }
+
+  if(action === "attack") {
+    attack();
+  } else if (action === "run") {
+    run();
+  } else {
+    console.log("What you talking about " + charName + "?");
+  }
+}
+characterActions("attack");
+characterActions("run");
+characterActions("eat");
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+// const counter = () => {
+//   // Return a function that when invoked increments and returns a counter variable.
+//   return ++counter;
+// };
+
+const counter = function() {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  const inc = function() {count++; return count}
+  return inc;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
