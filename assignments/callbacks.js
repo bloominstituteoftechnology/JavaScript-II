0aa070020af9d591bf3199c076ee7e330e733cd4
CsillaG <csillagok@gmail.com>
2018-08-07 18:55:46 -0400

    Almost finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,28 +1,54 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-}
+
+// function declaration approach
+ function firstItem(arr, cb) {
+//   // firstItem passes the first item of the given array to the callback function.
+   return cb(arr);
+ }
+
+ function first(items) {
+   console.log(items[0]);
+ } 
+
+ firstItem(items, first);
+
+// Anonymous approach
+ function firstItem(arr, cb) {
+   return cb(arr[0]);
+ }
+
+ firstItem(items, function(first) {
+   console.log(first);
+ });
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
+ 
+
 }
 
 function last(arr, cb) {
+  return cb(arr [arr.length-1]);
   // last passes the last item of the array into the callback.
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
@@ -31,4 +57,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+ 
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -5,11 +5,18 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+let counter=0;
+return function(){
+  return ++counter;
+}
+};
+const newCounter = counter();
+newCounteer();
+newCounter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
