82d3fdff31a4a7049eccaefa5472f5f56ae02789
unknown <cornielleandres@gmail.com>
2018-08-07 18:17:40 -0400

    Stretch problems for Closures done.

--- assignments/closure.js
+++ assignments/closure.js
@@ -33,8 +33,19 @@ newCounter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (num) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  const counter = {
+    "increment": function() {
+      return num + 1;
+    },
+    "decrement": function() {
+      return num - 1;
+    }
+  };
+  console.log(counter.increment());
+  console.log(counter.decrement());
 };
+counterFactory(10);
