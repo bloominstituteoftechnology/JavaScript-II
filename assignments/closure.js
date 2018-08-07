ff4ac8289a09bca51921170c4cbf66a55cd8d1a6
Max <maxkajiwara@gmail.com>
2018-08-07 16:51:14 -0700

    Made closure functions more concise

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,11 +15,7 @@ fullName();
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   let count = 0;
-  const counterFunction = () => {
-    count++;
-    return count;
-  }
-  return counterFunction;
+    return () => count++;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -37,16 +33,15 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
   let count = 0;
-  const incDec = {
+  return {
     increment: () => {count++; return count},
     decrement: () => {count--; return count}
   }
-  return incDec;
 };
 
-const incDecCounter = counterFactory();
-console.log(incDecCounter.increment());
-console.log(incDecCounter.increment());
-console.log(incDecCounter.decrement());
-console.log(incDecCounter.decrement());
-console.log(incDecCounter.increment());
+const incDec = counterFactory();
+console.log(incDec.increment());
+console.log(incDec.increment());
+console.log(incDec.decrement());
+console.log(incDec.decrement());
+console.log(incDec.increment());
