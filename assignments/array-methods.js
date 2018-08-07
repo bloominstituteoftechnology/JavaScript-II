d9765df65ef9882cfc71f6a0f874e5ea1aed46dc
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-07 17:53:23 -0600

    Fixed problem 2 on challenge 5 of array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,30 +67,32 @@ allCaps = runners.map(function(runner){return runner.first_name.toUpperCase()});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
 largeShirts = runners.filter(function(runner){return runner["shirt_size"]==="L"});
 console.log(largeShirts);
 
-// ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+// // ==== Challenge 4: Use .reduce() ====
+// // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce(function(a, b){return (a += b.donation)}, 0)
+ticketPriceTotal = runners.reduce(function(a, b){return (a += b.donation)}, 0);
 console.log(ticketPriceTotal);
 
-// ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
+// // ==== Challenge 5: Be Creative ====
+// // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// // Problem 1
+// // // Problem 1
 let mailingList = [];
 runners.forEach(function(runner){ mailingList.push(runner.first_name + " " + runner.last_name + " <" + runner.email + ">")});
 console.log(mailingList);
 
 
 // // Problem 2
-let donationOrder = [];
-donationOrder = runners.map(function(runner){ return runner.donation}).sort(function(a, b){ return a - b});
+let donationOrder = runners;
+donationOrder = runners.slice(0).sort(function(a, b){ return a.donation - b.donation});
 console.log(donationOrder);
+console.log("\n\n\n")
+console.log(runners);
 
 
 // // Problem 3
