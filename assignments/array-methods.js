073614ed1fc694be9aabeb083bc17e4fb558e026
m-j-hacker <mjhacker@gmail.com>
2018-08-07 21:28:50 -0500

    Finished array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,23 +67,37 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 
-function toUpper(x) {
-  return x.toUpperCase();
-};
-let allCaps = [];
-allCaps += runners.map((first_name) => { return runners.first_name });
 
+let allCaps = runners.map((runner) => {
+  return runner.first_name.toUpperCase();
+});
+// Project comments --------
+// I used the solution for this one. I was writing my function
+// incorrectly      and   didnt even know it. -MH
+//--------------------------
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter((shirts) => {
+return shirts.shirt_size === "L" });
 console.log(largeShirts);
+
+//Project Comments --------
+// I didn't need the solution for this one! ^_^ -MH
+//-------------------------
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((donationTotal, donationAmount) => {
+  return donationTotal + donationAmount.donation;}, 0);
 console.log(ticketPriceTotal);
+
+//Project Comments --------
+// This one required me to reference the solution because
+// my function syntax was off. I need to practice them more -MH
+//-------------------------
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
