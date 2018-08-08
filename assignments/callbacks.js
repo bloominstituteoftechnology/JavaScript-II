8f91d4efa132ac2095167d7853427b87f49eaafa
CsillaG <csillagok@gmail.com>
2018-08-07 20:52:35 -0400

    finishedCB

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,44 +1,46 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum']
 
+function firstItem(arr, cb) {
+ 
+  console.log(cb(arr));
+}
 
-// function declaration approach
- function firstItem(arr, cb) {
-//   // firstItem passes the first item of the given array to the callback function.
-   return cb(arr);
- }
+let first = function (items) {
+  return items[0];
+}
 
- function first(items) {
-   console.log(items[0]);
- } 
-
- firstItem(items, first);
-
-// Anonymous approach
- function firstItem(arr, cb) {
-   return cb(arr[0]);
- }
-
- firstItem(items, function(first) {
-   console.log(first);
- });
-
+firstItem(items, first);
+  
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
- 
+  console.log(cb(arr);
+ }
+ let arrayLength = function(items){
+   return items.length;
+ }
+getLength(items, arrayLength);
 
-}
+
 
 function last(arr, cb) {
-  return cb(arr [arr.length-1]);
+  cb(arr [arr.length-1]);
   // last passes the last item of the array into the callback.
 }
+
+last(items, function(lastItem){
+  console.log(lastItem);
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x+y);
 }
+sumNums(10, 15, function (total) {
+  console.log(total);
+});
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
@@ -48,8 +50,21 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  cb(list.includes(item));
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return cb(true);
+    }
+  }
+  return cb(false);
 }
+
+contains('Bats', items, function (flag) {
+  console.log(flag); // should be false
+});
+
+contains('Notebook', items, function (flag) {
+  console.log(flag); // should be true
+});
 
 /* STRETCH PROBLEM */
 
