a569ebc1e5e7e7993f53bb63b6a85c9875b81e8b
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-07 19:20:18 -0700

    completed closure.js Challenge 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function faveActivity() {
+  let person = "Antionette";
+  function action() {
+    
+    console.log(person + " loves to sing!")
+  }
+  action();
+}
+console.log(faveActivity());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
