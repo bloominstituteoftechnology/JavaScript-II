56d1cfaa36f42cb289922a364255522d1ddbc790
Eva Li <evalee904@gmail.com>
2018-08-11 23:21:26 -0500

    Edited closure.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,23 +1,25 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 function theLittlePrince() {
-  let name = 'The Little Prince';
+  const name = 'The Little Prince';
 
   function want() {
-    console.log(name + ' wants you to draw him a sheep!');
+    return `${name} wants you to draw him a sheep!`;
   }
-  want();
+  return want();
 }
-theLittlePrince();
+console.log(theLittlePrince());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  
+    let count = 0;
+    return () => (++count);
 };
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
