7a52fd52daae737bc37f43cc17e9defa4d6aaf12
cferrell2 <cferrell12182@gmail.com>
2018-08-16 19:27:45 -0400

    complete closure.js assignments

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function foo(){
 
+  let a = 5;
+
+  function baz(b){
+    return a + b;
+  }
+  return baz;
+}
+myFunc = foo();  //returns baz function
+console.log(myFunc(7));  //12
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  function inc(){
+    count += 1;
+    return count;
+  }
+  return inc;
 };
+
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
