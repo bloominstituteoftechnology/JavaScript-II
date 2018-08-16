1e358f35044c51f19be25c11a6c0eaa4c13c5dbb
Tommy Collison <tommy@collison.ie>
2018-08-16 13:48:13 -0700

    finished challenges in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -31,38 +31,56 @@ function last(arr, cb) {
 return cb(arr);
 }
 
-function lastitem(arr, cb) {
-  console.log(items.length -1);
+function lastitem(items, last) {
+  console.log(items[3]);
 }
 
 last(items, lastitem);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-
-return cb(x+y);
-
+return cb(x, y);
 }
 
-function sum(x +y ) {
-
+function addNums(x, y) {
+    console.log(x+y);
 }
-
-sumNums(1, 3);
+    
+ sumNums(3, 4, addNums);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+return cb(x, y);
 }
+
+function multiplication_nums(x, y) {
+    console.log(x*y);
+}
+  
+ multiplyNums(12, 6, multiplication_nums);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+console.log(list.includes(item));
+} 
+
+let checkMcCheckerson = function(item, list) {
+  if (list.includes(item) === true) {
+    return true
+  } else {
+    return false
+  }
 }
+
+contains('yo-yo', items, checkMcCheckerson); // return true
+contains('John Spraul', items, checkMcCheckerson); // return false
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
