4ca9b70449f92c1e0bc1dc90f579bf04665210d1
paulapivat <paul.apivat@gmail.com>
2018-08-16 14:06:08 +0700

    first problem callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,11 +2,26 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr);
 }
+
+function first(items) {  //callback function
+  console.log(items[0]);
+}
+
+firstItem(items, first);
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+function length(items) {
+  console.log(arr.length);
+}
+
+gotLength(items, length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
