c819b5495d17894237e74bdd1261dbb7db32ae01
Kendra Williams <kmonique.williams@gmail.com>
2018-08-16 19:22:30 -0400

    Task 4: closures.js completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const name = (name) => {
+  let firstName = name;
 
+  const sayHi = () => {
+    console.log(`Hi ${firstName}!`);
+  }
+  return sayHi;
+}
+
+const hiKendra = name("Kendra");
+const hiJohn = name("John");
+hiKendra();
+hiJohn();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  
+  const increment = () => { 
+    console.log(++count);
+  };
+  return increment;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
