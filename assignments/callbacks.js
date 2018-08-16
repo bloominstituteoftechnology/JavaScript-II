5736256e6941bb86e14d714095bebe67d1411f84
kurtve <kurt@vanetten.us>
2018-08-16 10:07:21 -0400

    callbacks.js: added stretch task, added tests for all functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,5 +38,63 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  
+
+  // I learned this technique from one of the earlier CCs!
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (array.indexOf(item) === i) {
+        newArray.push(item);
+    }
+  }
+
+  cb(newArray);
 }
+
+
+/* test our work */
+
+// Let's have our callback just log stuff to the console
+function myCb(param) {
+    console.log(param);
+}
+
+
+// return the first item of an array
+console.log("\nfirstItem:");
+firstItem(items, myCb);
+
+// return the length of an array
+console.log("\ngetLength:");
+getLength(items, myCb);
+
+// return the last item of an array
+console.log("\nlast:");
+last(items, myCb);
+
+// return the sum of two numbers
+console.log("\nsumNums: 1 + 2 =");
+sumNums(1, 2, myCb);
+
+// return the product of two numbers
+console.log("\nmultiplyNums: 3 * 4 =");
+multiplyNums(3, 4, myCb);
+
+// return whether an array contains an item
+console.log("\ncontains: Pencil, Pen");
+contains('Pencil', items, myCb);
+contains('Pen', items, myCb);
+
+
+// return an array with duplicates removed
+
+// create a new list with a duplicate item
+const newItems = Array.from(items);
+newItems.push('Pencil');
+// now test it
+console.log("\nremoveDuplicates:");
+console.log('Before:');
+console.log(newItems);
+console.log('After:');
+removeDuplicates(newItems, myCb);
+
