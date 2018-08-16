a49da14c05fae11a098ceffd6bd05aaf44548179
Emmanuel Prado <pradoe.ep@gmail.com>
2018-08-16 16:11:21 -0600

    completed all .js files, no stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const sayName = (name) => {
+  const sayHello = () => {
+    return `What is up ${name}`
+  }
+  return sayHello;
+}
+const hiEman = sayName('Eman');
+console.log(hiEman());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  const addOne = () => {
+    return count++;
+  }
   // Return a function that when invoked increments and returns a counter variable.
+  return addOne;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+newCounter = counter();
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
