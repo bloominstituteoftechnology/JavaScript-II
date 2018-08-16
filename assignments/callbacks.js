2c703f6ddd536b98f87d09265eec36b05f5c84d6
Tommy Collison <tommy@collison.ie>
2018-08-16 10:55:31 -0700

    every fifteen lines

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -3,24 +3,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
 
-callback(item[0]);
+return cb(arr);
 }
+
+function first(items) {
+   console.log(items[0]);
+ } 
+
+firstItem(items, first);
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 
-callback(items.length);
-
+return cb(arr);
 }
+
+function length(items) {
+  console.log(items.length)
+}
+
+getLength(items, length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
+return cb(arr);
 }
+
+function lastitem(arr, cb) {
+  console.log(items.length -1);
+}
+
+last(items, lastitem);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+
+return cb(x+y);
+
 }
+
+function sum(x +y ) {
+
+}
+
+sumNums(1, 3);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
