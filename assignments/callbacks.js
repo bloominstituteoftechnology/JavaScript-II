31a96bad9b178063c049ee9915d8f81c08a1dca2
Clem <stacy.clem@e-hps.com>
2018-08-16 21:23:12 +0100

    finished callbacks > working with Streach on callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,33 +1,48 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-
 const firstItem = (arr, cb) => {
-  return items[0];
-}
-items.filter(firstItem);
-
-function getLength(arr, cb) {
+  return cb(arr[0]);
+};
+// firstItem passes the first item of the given array to the callback function.
+firstItem(items, firstItem => {
+  console.log(`The first item is: ${firstItem}`)
+})
+let getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
- console.log(items.length);
+  return cb(arr.length);
 }
-
-function last(arr, cb) {
+getLength(items, getLength => {
+  console.log(`The length of the array is: ${getLength}`)
+})
+const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
-}
+ return cb(arr[3]);
 
-function sumNums(x, y, cb) {
+}
+ last(items, last => {
+   console.log(`The last item is: ${last}`)
+ })
+ let sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
-
-function multiplyNums(x, y, cb) {
+sumNums(2, 3, sumNums => {
+  console.log(`The total of the numbers are: ${sumNums}`)
+})
+ multiplyNums = (x, y, cb)=> {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+ return cb(x * y)
 }
+multiplyNums(3, 4, multiplyNums => {
+  console.log(`The total of the numbers are: ${multiplyNums}`)
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
-
+contains("yo-yo", items, console.log);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
