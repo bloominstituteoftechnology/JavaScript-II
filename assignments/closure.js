d029f12c9660695727a9eebf13f28cb2ed1f3c04
darksamus6 <darksamus6@gmail.com>
2018-08-16 19:22:47 +0000

    finished function-conversion.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const hello =() => {
+  
+  function sayHello() {
+    console.log('Hello');
+  }
+  
+  return(sayHello);
+}
+
+const hi = hello();
+hi();
+
 
 // ==== Challenge 2: Create a counter function ====
+
+
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  function checkNum() {
+    console.log(num);
+  }
+
+  num++;
+  
+  
+  return(checkNum);
+  
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+newCounter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
