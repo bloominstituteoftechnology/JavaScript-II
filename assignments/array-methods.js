3d88bd74880a206fbbbfd103dd969b8e49aae2fd
Emmanium <pradoe.ep@gmail.com>
2018-08-16 16:31:58 -0600

    revised array-methods.js challenge 4

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -83,17 +83,15 @@ console.log(largeShirts);
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 
 let initialValue = 0;
-const ticketPriceTotal = runners.reduce((acc, item) => {
+const donationTotal = runners.reduce((acc, item) => {
     return acc + item.donation;
 }, initialValue);
+
+console.log(donationTotal);
+
+let ticketPriceTotal = [];
+ticketPriceTotal.push(donationTotal);
 console.log(ticketPriceTotal);
-
-totalPrice = [];
-runners.reduce((acc, item) => {
-    return totalPrice.push(acc + item.donation);
-}, initialValue)
-
-console.log(totalPrice);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
