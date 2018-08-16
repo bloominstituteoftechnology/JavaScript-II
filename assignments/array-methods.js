c7d1bc3e191e078530754016a7e2ca2a10c0b831
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-08-16 01:01:09 -0600

    Completed Task 3: Array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,81 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach((runner) => {
+    fullName.push(`${runner.first_name} ${runner.last_name}`);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map((runner) => {
+    return `${runner.first_name.toUpperCase()}`;
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter((runner) => {
+    return runner.shirt_size === 'L';
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((total, runner) => {
+    return total + runner.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 - An e-mail list for sending out news and updates to all the runners
+let emails = [];
+runners.forEach((runner) => {
+    emails.push(runner.email);
+});
+console.log(emails);
 
-// Problem 2
+// Problem 2 - The total donations for a given company (using Skinix as example)
+let companyTotalDonations = (runners, company_name) => {
+    // get only the runners for that company
+    let runnersForCompany = runners.filter((runner) => {
+        return runner.company_name === company_name;
+    });
+    // return the total of that company's donations
+    return runnersForCompany.reduce((total, runner) => {
+        return total + runner.donation;
+    }, 0);
+};
+console.log(companyTotalDonations(runners, 'Skinix'));
 
-// Problem 3
+// Problem 3 - An array that holds objects of each company name and their total donation
+let totalDonationsByCompany = (runners) => {
+    // get the full list of company names
+    let companies = [];
+    runners.forEach((runner) => {
+        companies.push(runner.company_name);
+    });
+    
+    // remove duplicates from companies array
+    companies.sort();
+    companiesNoDuplicates = [];
+    for (let i = 0; i < companies.length; i++) {
+        if (companies[i] != companies[i+1]) {
+            companiesNoDuplicates.push(companies[i]);
+        }
+    }
+
+    // for each company name use function from problem 2 (companyTotalDonations) to get total donations
+    companiesAndTotals = [];
+    companiesNoDuplicates.forEach((company_name) => {
+        companiesAndTotals.push({
+            "company_name": company_name,
+            "total_donation": companyTotalDonations(runners, company_name)
+        });
+    });
+
+    return companiesAndTotals; // return the array with companies and their total donations
+};
+console.log(totalDonationsByCompany(runners));
