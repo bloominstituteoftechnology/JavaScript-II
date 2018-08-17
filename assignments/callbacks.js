922c4f7b2efbca85df729d871f865f1aa8ebf8bc
paulapivat <paul.apivat@gmail.com>
2018-08-17 15:08:54 +0700

    add callback function to contains function callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -59,17 +59,15 @@ multiplyNums(8,24, multiplyTwoNums);
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
-// function checkItem(arr, item) {
-//   if (arr.includes(item)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+//anonymous function
+contains(items, 'Pen', function(boolean){
+  console.log(`This is ${boolean}`);
+});
 
-// filterItem(items, 'yo-yo');
+
 
 
 /* STRETCH PROBLEM */
