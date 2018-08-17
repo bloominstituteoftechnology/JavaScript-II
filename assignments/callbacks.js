a4aa18106697e23ecb9dbe06f3ec71495895233b
vy3191 <venkyafhs@gmail.com>
2018-08-16 22:56:59 -0400

    Initial Commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,27 +2,42 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    let length = arr.length;
+   return cb(length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let lastItem = arr[arr.length-1];
+  return cb(lastItem);
+
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+   let sum = x + y;
+   return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+   let multiply = x * y;
+   return cb(multiply);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.indexOf(item) >= 0) {
+      cb(true);
+  } else {
+      cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
@@ -31,4 +46,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let arr = []
+  for(var i = 0; i < array.length; i++) {
+      if(arr.indexOf(array[i]) < 0 ) {
+           arr.push(array[i]);
+      }
+  }
+  return cb(arr);
 }
