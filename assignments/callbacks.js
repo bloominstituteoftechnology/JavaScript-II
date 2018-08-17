047471e05091b385589a1331098d6fb5c622c374
Kendra Williams <kmonique.williams@gmail.com>
2018-08-16 23:50:24 -0400

    completed project function conversion included

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -7,14 +7,14 @@ const displayCallback = (arr) => {
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-  return cb(arr[0]);
+  cb(arr[0]);
 }
 
 firstItem(items, displayCallback);
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
+  cb(arr.length);
 }
 
 getLength(items, displayCallback);
@@ -22,21 +22,21 @@ getLength(items, displayCallback);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
+  cb(arr[arr.length - 1]);
 }
 
 last(items, displayCallback);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
+  cb(x + y);
 }
 
 sumNums(3, 4, displayCallback);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
+  cb(x * y);
 }
 
 multiplyNums(3, 4, displayCallback);
@@ -45,9 +45,9 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   if(list.includes(item)){
-    return cb(true);
+    cb(true);
   }else{
-    return cb(false);
+    cb(false);
   }
 }
 
@@ -59,16 +59,16 @@ contains("stick", items, displayCallback);
 function removeDuplicates(array, cb) {
   let newArr = [];
   array.forEach((element) => {
-    if (newArr.includes(element)){
-      console.log(`Duplicate element: ${element} skipped`);
-    } else {
+    if (!newArr.includes(element)){
       newArr.push(element);
+    } else {
+      console.log(`Duplicate element: ${element} skipped`);
     }
   });
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  return cb(array);
+  cb(newArr);
 }
 
 removeDuplicates(items, displayCallback);
