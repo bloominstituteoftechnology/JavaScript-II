41182924799f99254af76b2b7788f4dcb15b0254
Lloyd Napier <lloyd.a.napier@gmail.com>
2018-08-16 19:16:51 -0700

    Task 3 complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function out() {
+  let outVar = "this";
+  function in() {
+  console.log(outVar);
+  }
+  in();
+}
+out();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let counter = 0;
+  return function() {
+   counter++;
+   return counter;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
@@ -14,6 +27,11 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0;
+  return {
+    "increment": function(){counter++; return counter;},
+    "decrement": function(){counter--; return counter;}
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
