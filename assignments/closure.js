851b6d9c66f5983d208c301f0f92a9d365da0de3
jthomps838 <joe.thompson838@gmail.com>
2018-08-16 22:07:03 -0400

    finshed challenge 3 stretch goal in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,8 +32,30 @@ newCounter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (upDown ,num) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+let counter = {
+  increment : function(num) {
+    num++;
+    return num;
+  },
+  decrement : function(num) {
+    num--;
+    return num;
+  }
+}
+let increment = counter.increment(num);
+let decrement = counter.decrement(num);
+if (upDown === 'increment') {
+  return increment;
+}
+else {
+  return decrement;
+}
+}
+// counterFactory('increment', 5);
+// counterFactory('decrement', 100);
+// counterFactory('decrement', 8);
+// counterFactory('increment', 5);
