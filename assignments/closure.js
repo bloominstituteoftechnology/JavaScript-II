4135e6023edc8e6347ce998d3b5e0f9f6deb9b6e
Alex Ostrander <alexanderostrander23@gmail.com>
2018-08-16 23:29:50 -0400

    Completed Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
+const helloPerson = () => {
+    let name = 'Alex';
+    const response = () => {
+        return `Hello ${name}`;
+    }
+    
+    return response;
+}
 
+const greeting = helloPerson();
+
+console.log(greeting);
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
   // Return a function that when invoked increments and returns a counter variable.
+  function addOne(){
+    return count++;
+  }
+  return addOne;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+newCounter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
