2a350a6eb281153a05a1e14f1c73b48c830fd767
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-08-16 21:58:15 -0500

    Made more progress in assignments.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,34 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const sayHi = () => {
+  return 'Hello World!'
+}
+
+console.log(sayHi())
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  const addToCounter = () => {
+    return ++count;
+  }
+  return addToCounter;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
+const anotherCounter = counter();
+console.log(anotherCounter());
+console.log(anotherCounter());
+console.log(anotherCounter());
+console.log(anotherCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
