fc734d4d2c5e69de14f9bb4e2f9efa0897444b1f
Jason Hedrick <jasonleehedrick@gmail.com>
2018-08-16 19:28:10 -0700

    Working on array-methods.js everything else is done sans stretch goals

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,11 +5,19 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let countTotal = 0;
+  const counterIncrease =() => {
+	  return ++countTotal;
+  }
+  return counterIncrease;
+  
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
