42f8b7cd69d4d2e44bc22f7faab5c9f0428c0c0b
Louis <7.louislee@gmail.com>
2018-08-16 20:40:05 -0700

    forgot to commit first.. finished callbacks

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,11 +56,20 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+const createFullName = (item) => {
+fullName.push(`${item.first_name} ${item.last_name}`);
+}
+runners.forEach(createFullName);
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+const cbAllCaps = (item) => {
+  var result = item.first_name.toUpperCase();
+  allCaps.push(result);
+}
+runners.map(cbAllCaps);
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
