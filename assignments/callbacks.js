0e3d0d2c93c12c250d3da879e538bd89a4f90097
Matthew Meitl <darksamus6@gmail.com>
2018-08-16 19:28:48 -0500

    cleaned up formatting on all js files

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,85 +1,66 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-  cb(arr[0]);
+	// firstItem passes the first item of the given array to the callback function.
+	cb(arr[0]);
 }
-
 firstItem(items, (fitem) => {
-  console.log(fitem);
+	console.log(fitem);
 });
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-  cb(arr.length);
+	// getLength passes the length of the array into the callback.
+	cb(arr.length);
 }
-
-getLength(items, (titems) =>{
-  console.log(titems);
+getLength(items, (titems) => {
+	console.log(titems);
 });
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  cb(arr[arr.length-1]);
+	// last passes the last item of the array into the callback.
+	cb(arr[arr.length - 1]);
 }
-
-last(items, (litem) =>{
-  console.log(litem);
+last(items, (litem) => {
+	console.log(litem);
 });
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  
-  cb(x+y); 
+	// sumNums adds two numbers (x, y) and passes the result to the callback.
+	cb(x + y);
 }
-
-sumNums(5,6,(add) => {
-  console.log(add);
+sumNums(5, 6, (add) => {
+	console.log(add);
 });
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  cb(x*y);
+	// multiplyNums multiplies two numbers and passes the result to the callback.
+	cb(x * y);
 }
-
-multiplyNums(8,7, (product) => {
-  console.log(product);
+multiplyNums(8, 7, (product) => {
+	console.log(product);
 });
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false
-
-  
-  let count = 0;
-  list.forEach(litem => {
-    
-    if(item == litem) {
-      count++;
-      
-    } 
-
-
-    
-  });
-
-  if(count>0){
-    cb(true);
-  }else{
-    cb(false);
-  }
-  
-  
+	// contains checks if an item is present inside of the given array/list.
+	// Pass true to the callback if it is, otherwise pass false
+	let count = 0;
+	list.forEach(litem => {
+		if (item == litem) {
+			count++;
+		}
+	});
+	if (count > 0) {
+		cb(true);
+	} else {
+		cb(false);
+	}
 }
-contains('Banana', items,(listed)=> {
-  console.log(listed);
+contains('Banana', items, (listed) => {
+	console.log(listed);
 });
-
-
 /* STRETCH PROBLEM */
-
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+	// removeDuplicates removes all duplicate values from the given array.
+	// Pass the duplicate free array to the callback function.
+	// Do not mutate the original array.
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,40 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-const hello =() => {
-  
-  function sayHello() {
-    console.log('Hello');
-  }
-  
-  return(sayHello);
+const hello = () => {
+	function sayHello() {
+		console.log('Hello');
+	}
+	return (sayHello);
 }
-
 const hi = hello();
 hi();
-
-
 // ==== Challenge 2: Create a counter function ====
-
-
-
 let counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-  let num = 0;
-  
-  return counter = () => {num++;return num};
-  
+	// Return a function that when invoked increments and returns a counter variable.
+	let num = 0;
+	return counter = () => {
+		num++;
+		return num
+	};
 };
-
 counter();
 counter();
 counter();
-
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
-
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+	// Return an object that has two methods called `increment` and `decrement`.
+	// `increment` should increment a counter variable in closure scope and return it.
+	// `decrement` should decrement the counter variable and return it.
 };
