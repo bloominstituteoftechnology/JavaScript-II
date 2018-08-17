4f638d50955aeede58ed41e562ffabdcea827415
Kyle Baker <k@gnomiq.com>
2018-08-16 17:16:09 -0700

    Completed callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,29 +1,52 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0])
 }
+
+firstItem(items, function(firstItem) {
+    console.log(firstItem)
+});
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length)
 }
+
+getLength(items, function(length) {
+    console.log(length);
+});
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1])
 }
+
+last(items, function(lastItem) {
+    console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    cb(x + y)
 }
+
+sumNums(1, 2, function(sum) {
+    console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y)
 }
 
+multiplyNums(2, 2, function(prod) {
+    console.log(prod);
+});
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  cb(!!list.find(l => l === item))
 }
+
+contains("Pencil", items, function(exists) {
+    console.log(exists);
+});
 
 /* STRETCH PROBLEM */
 
