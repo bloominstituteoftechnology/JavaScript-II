50c65b4272aa6c867e8ad8bb92db60a73500a171
TheGameFreak720 <luis.alvarez@commithub.com>
2018-08-16 22:29:16 -0400

    finished challenge 4

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -81,7 +81,9 @@ console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(acc, curr) {
+    return acc += curr.donation
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
