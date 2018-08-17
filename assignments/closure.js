8240d1d1ac3985f3f9493721a06bae7516b3a263
Bruce Cabanayan <bcabanayan@Bruces-MBP.domain>
2018-08-16 20:00:20 -0700

    Completed closures exercises

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
+const divisibleBy = (x) => {
+  return checkDivisibility = (y) => {
+    return (y % x === 0);
+  } 
+}
 
+let divByFive = divisibleBy(5);
+
+divByFive(100); //returns true
+divByFive(62); //returns false
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  let addCount = () => {
+    return count = count + 1;
+  }
+  return(addCount);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+newCounter(); // returns 1
+newCounter(); // returns 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
