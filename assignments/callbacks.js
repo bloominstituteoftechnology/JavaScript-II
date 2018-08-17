b5677cab0ab9bf329291baa23d922dfa411a1d66
Joe Mercado <jryel1970@gmail.com>
2018-08-17 00:07:20 -0400

    changes to callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,47 +1,55 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
+const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
-  return cb(arr[0]);
-}
+  cb(arr[0]);
+};
 
-firstItem(items, callback);
+firstItem(items, firstItem => {
+  console.log(`The first item is ${firstItem}.`);
+});
 
-function getLength(arr, cb) {
+const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
-  let myArr = arr.length;
-}
+  cb(arr.length);
+};
+getLength(items, length => {
+  console.log(`The length of the array is ${length}.`);
+});
 
-getLength(items, callback);
-
-function last(arr, cb) {
+const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
-  return cb(arr(arr.length - 1));
-}
+  cb(arr[arr.length - 1]);
+};
+last(items, lastItem => {
+  console.log(`The last item in the array is ${lastItem}.`);
+});
 
-last(items, callback);
-
-function sumNums(x, y, cb) {
+const sumNums = (x, y, cb) => cb(x+y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x+y);
-}
-
 sumNums(4,5,console.log);
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x*y);
-}
+const multiplyNums = (x, y, cb) => cb(x*y);
+
+// multiplyNums multiplies two numbers and passes the result to the callback.
 
 multiplyNums(6,5,console.log);
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
+const contains = (arr, str, cb) => { 
+   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(list.includes(item));
-}
-
-contains('Notebook',items,console.log());
+  const isInArray = () => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === str) {
+        return true;
+      }
+    } return false;
+  };
+  cb(isInArray());
+};
+contains(items, 'Notebook', result => {
+  console.log(result ? 'Notebook is in the array' : 'Notebook is not in the array');
+});
 
 /* STRETCH PROBLEM */
 
@@ -50,8 +58,8 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
   const dupe = [];
-	for (let x = 0; x < items.length; x++) {
-		dupe[items[x]] = true;
-	}
+  for (let x = 0; x < items.length; x++) {
+    dupe[items[x]] = true;
+  }
   cb(Object.keys(dupe));
 }
