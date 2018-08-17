3b9791537ede1645234fc24cc595e1b9eb5e1144
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-08-16 20:49:04 -0500

    Finished callbacks.js problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,28 +1,64 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// firstItem passes the first item of the given array to the callback function.
+function firstItem(arr, cb) {  
+  return cb(arr[0]);
+};
+function firstItemCB(index) {
+  console.log(index);
+};
+firstItem(items, firstItemCB);
 
-function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-}
+ // getLength passes the length of the array into the callback.
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+function getLengthCb(length) {
+  console.log(length);
+}
+getLength(items, getLengthCb);
+
+// last passes the last item of the array into the callback.
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+function lastCb(index) {
+  console.log(index);
+}
+last(items, lastCb);
+
+// sumNums adds two numbers (x, y) and passes the result to the callback.
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+function sumNumsCb(num1, num2) {
+  console.log(num1 + num2);
+}
+sumNums(3, 4, sumNumsCb);
+
+// multiplyNums multiplies two numbers and passes the result to the callback.
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+function multiplyNumsCB(num1, num2) {
+  console.log(num1 * num2);
+}
+multiplyNums(3, 4, multiplyNumsCB);
+
+// contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return cb(true);
+    }
+  }
+  return cb(false);
 }
 
 /* STRETCH PROBLEM */
