df3485add137bac5349c5c29a09e85a5a8aa1b64
Rob Salzberg <robsalzberg@gmail.com>
2018-08-16 21:44:02 -0400

    Finished tasks 1-4 without stretch challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function sayHello() {
+  const hi = () =>{ console.log(hello); }
+    const hello = 'Hello, world!';
+  return hi;
+}
+const myClosure = sayHello(); 
+myClosure(); 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+  let count = 0;
+
+  const addOne = () => {
+    console.log(++count);
+  }
+
+  return addOne;
+}
+
+const newCounter = counter();
+newCounter()
+newCounter()
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
