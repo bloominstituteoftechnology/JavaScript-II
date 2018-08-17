9d85d5a156c67ab3a4f129f0c5362ff24c550872
Julie Jonak <juliejonak10@gmail.com>
2018-08-16 23:02:12 -0500

    Refactored some code in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,20 +55,23 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = runners.forEach((fullNameCallback = ((item)=>{console.log(`${item.first_name} ${item.last_name}`)})))
+let fullName = [];
+let wholeName = runners.forEach((fullNameCallback = ((item)=>{fullName.push(`${item.first_name} ${item.last_name}`)})))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = runners.map((item) => {
+let capitalize = runners.map((item) => {
     return item.first_name.toUpperCase()});
-  console.log(allCaps); 
+  console.log(capitalize); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = runners.filter(largeSorting = (item) => {
-    let large = (item.shirt_size === "L"); return large});
-  console.log(largeShirts);
+let bigShirts = runners.filter(item => {
+    return item.shirt_size === "L"
+      });
+    
+    console.log(bigShirts); 
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
@@ -92,33 +95,22 @@ console.log(bookletCopy);
 // Problem 3: The organizers need to know how many of each size shirt to order for the event. Calculate how many of each size are needed.
 
 let smallShirts = runners.filter((item)=>{
-    let shirtCount = (item.shirt_size === 'S');
-    return shirtCount;
-  });
+    return item.shirt_size === 'S'});
   
   let mediumShirts = runners.filter((item)=>{
-    let shirtCount = (item.shirt_size === 'M');
-    return shirtCount;
-  });
+    return item.shirt_size === 'M'});
   
   let largeShirts = runners.filter((item)=>{
-    let shirtCount = (item.shirt_size === 'L');
-    return shirtCount;
-  });
+    return item.shirt_size === 'L'});
   
   let extraLShirts = runners.filter((item)=>{
-    let shirtCount = (item.shirt_size === 'XL');
-    return shirtCount;
-  });
+    return item.shirt_size === 'XL'});
   
   let twoXLShirts = runners.filter((item)=>{
-    let shirtCount = (item.shirt_size === '2XL');
-    return shirtCount;
-  });
+    return item.shirt_size === '2XL'});
   
   
   console.log(`Order ${smallShirts.length} small shirts, ${mediumShirts.length} medium shirts, ${largeShirts.length} large shirts, ${extraLShirts.length} XL shirts, and ${twoXLShirts.length} 2XL shirts.`);
-
 
 
 
