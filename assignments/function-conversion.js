432186f49948583fa27be1b6895081a4c626de64
shannonhealy <sahealy525@gmail.com>
2018-08-16 23:26:07 -0400

    Completed almost all of function-conversion except one problem I don't know.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,23 +1,25 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = () => {};
+let myFunction = () => {};
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
+let anotherFunction = (param) => {
+  return param;
+};
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+ let add = (param1, param2) => {
+   return param1 + param2;
+ };
+ add(1,2);
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+ let subtract = (param1, param2) => {
+   return param1 - param2;
+ };
+ subtract(1,2);
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+ /// V I'm not sure for this one?
+
+ exampleArray = [1,2,3,4];
+ const triple = exampleArray.map(function (num) {
+   return num * 3;
+ });
+ console.log(triple);
