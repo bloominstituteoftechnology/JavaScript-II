073425e76c6d64101da999b1bec77cb955888466
Griffin <griffinshuth@gmail.com>
2018-08-16 21:08:00 -0600

    first commit callbacks done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,30 +1,47 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+function callBack(arg){
+  console.log(arg)
+}
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 }
+firstItem(items, callBack) 
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length)
 }
+getLength(items, callBack)
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr.pop())
 }
+last(items, callBack)
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+cb(x + y)
 }
+sumNums(3, 4, callBack)
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y)
 }
+multiplyNums(4, 5, callBack)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) cb(true);
+    else cb(false);
+  }
 }
-
+contains('Gum', items, callBack);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
