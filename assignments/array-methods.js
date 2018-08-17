ba458f96aee79ba7b7a9c5cec9d70dced6911955
Rob Salzberg <robsalzberg@gmail.com>
2018-08-16 23:21:09 -0400

    Finished Arrow Function Syntax Stretch Goal

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,7 +66,7 @@ console.log(fullName);
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 
 let allCaps = [];
-allCaps = runners.map( function(results) {
+allCaps = runners.map(results =>{
     return results.first_name.toUpperCase();
   });
 console.log(allCaps); 
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -6,18 +6,43 @@
 //   return param;
 // };
 
+ let myFunction = () => {};
+
+ let anotherFunction =  (param) => {
+   return param;
+ };
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let add = function (param1, param2) {
+   return param1 + param2;
+ };
+ add(1,2);
+
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+ let subtract = (param1, param2) => {
+   return param1 - param2;
+ };
+ subtract(1,2);
+
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+ exampleArray = [1,2,3,4];
+ const triple = exampleArray.map(num => {
+   return num * 3;
+ });
+ console.log(triple);
+
