0a516d10af7e51c5e242a03ccf2df071f795d6c3
Abigayle Franklin <abifanklin@gmail.com>
2018-08-16 20:40:00 -0500

    Counting Factory problem done.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function name(person) {
+  function displayName() {
+    console.log(`Hi ${person}!`);
+  }
+  return displayName;
+}
+
+let hiAbi = name('Abi');
+hiAbi();
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let total = 0;
+  function count() {
+    console.log(++total);
+  }
+
+  return count;
   // Return a function that when invoked increments and returns a counter variable.
 };
+
+let newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -13,8 +33,28 @@ const counter = () => {
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = {
+  total: 0,
+  plusOne: function() {
+    let counter = 0;
+    function increment() {
+      counter++;
+    }
+    return increment;
+  },
+  minusOne: function() {
+    let counter = 0;
+    function decrement() {
+      counter--;
+    }
+    return decrement;
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+let counting = counterFactory.plusOne;
+console.log(counting());
+
+
