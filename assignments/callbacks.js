d2d2318b21484875badb9260c03d880d6349450e
Jesse <theiseja@gmail.com>
2018-08-16 21:13:48 -0500

    Finished callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -12,25 +12,64 @@ firstItem(items, first);
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+
+  return cb(arr);
 }
+
+function length(items) {
+  console.log(items.length);
+}
+getLength(items,length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
+  return cb(arr);
 }
+
+function lastitem(items,last) {
+  console.log(items[3]);
+}
+last(items, lastitem);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+
+return cb(x, y);
 }
 
+function addNum(x, y) {
+  console.log(x+y);
+}
+sumNums(10, 20, addNum);
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+
+  return cb(x, y);
 }
+
+function multiplyNumbers(x, y) {
+  console.log(x*y);
+}
+multiplyNums(22, 4, multiplyNumbers);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+  console.log(list.includes(item));
 }
 
+let checkItem = function(item, list) {
+  if (list.includes(item) === true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+contains('Pencil', items, checkItem); // returns true because Pencil exists in array
+contains('tshirt',items, checkItem); // returns false because tshirt doesn't exist inside of the array
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
