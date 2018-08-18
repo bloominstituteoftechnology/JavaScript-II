00e986427415601170f4a9fb8335f448ff8031a8
Jennifer King <jenbellpgh@gmail.com>
2018-08-18 14:15:01 -0400

    Completed closures. I thought they weren't working but debugger shows they are.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,19 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const person = () => {
+  let name = 'Jen';
 
+  const speak = () => {
+    console.log( `${name} says, 'Hello!'` );
+  }
+  return speak;
+}
+const newPerson = person();
+newPerson();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  let count = 0;
-  function add() {
+  var count = 0;
+  const add = () => {
     count += 1;
+    debugger;
   }
   return add;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
