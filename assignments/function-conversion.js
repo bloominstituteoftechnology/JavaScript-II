6323b4c5682cd52e0f8f3116d4f8ef90a912ab38
unknown <jerrardsmith215@gmail.com>
2018-08-18 10:58:25 -0400

    Jerrard Smith JavaScript II completed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -21,5 +21,18 @@
 //   return num * 3;
 // });
 // console.log(triple);
-
-/*Answer*/
+var myFunction = function myFunction() {};
+var anotherFunction = function anotherFunction(param) {};
+var add = function add(param1, param2) {
+  return param1 + param2;
+};
+add(1, 2);
+var subtract = function subtract(param1, param2) {
+  return param1 - param2;
+};
+subtract(1, 2);
+exampleArray = [1, 2, 3, 4];
+var triple = exampleArray.map(function (num) {
+  return num * 3;
+});
+console.log(triple);
