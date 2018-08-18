0698d7deea343daf63ad0f958f8da9c6cfbc7490
mglittleton <michaelglittleton@gmail.com>
2018-08-18 10:06:59 -0400

    Task 4 complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,59 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function powerUp (x) {
+  let num = 2;
+  function elevate () {
+    num = Math.round(Math.pow(num, x) * 100) / 100;
+    return num;
+  }
+  return elevate;
+}
+
+let bacteriaPopulation = powerUp(1.5);
+let humanPopulation = powerUp(1.1);
+let mosquitoPopulation = powerUp (.9);
+
+console.log(bacteriaPopulation());
+console.log(bacteriaPopulation());
+console.log(bacteriaPopulation());
+console.log(bacteriaPopulation());
+console.log(bacteriaPopulation());
+console.log(bacteriaPopulation());
+console.log(humanPopulation());
+console.log(humanPopulation());
+console.log(humanPopulation());
+console.log(humanPopulation());
+console.log(humanPopulation());
+console.log(humanPopulation());
+console.log(mosquitoPopulation());
+console.log(mosquitoPopulation());
+console.log(mosquitoPopulation());
+console.log(mosquitoPopulation());
+console.log(mosquitoPopulation());
+console.log(mosquitoPopulation());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+  let num = 0;
+  function increment () {
+    num++;
+    return num;
+  }
+  return increment;
+}
+
+let numeroUno = counter();
+let numeroDos = counter();
+console.log (`Uno, ${numeroUno()}`);
+console.log (`Dos, ${numeroDos()}`);
+console.log (`Uno, ${numeroUno()}`);
+console.log (`Uno, ${numeroUno()}`);
+console.log (`Dos, ${numeroDos()}`);
+console.log (`Dos, ${numeroDos()}`);
+console.log (`Uno, ${numeroUno()}`);
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,3 +66,5 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+
