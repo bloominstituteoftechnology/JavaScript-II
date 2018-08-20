f2cc6750a951a651061de29f4a6c72eff4ec26d4
Obed Lorisson <Obed.lorisson@gmail.com>
2018-08-20 18:48:34 -0400

    fix sumNums and multiply challenge

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -14,36 +14,26 @@ function getLength(arr, cb) {
 }
 
 /////////////////////////////////////////////////////////////////////////////////
-const lastItem =(arr)=>{
-  return arr[arr.length -1]
-}
-
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr.pop);
+  return cb(arr.length - 1);
 }
-
-
-
 /////////////////////////////////////////////////////////////////////////////
 
 function sumNums(x, y,cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x,y);
+  return cb(x + y);
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x,y);
+  return cb(x * y);
 }
 
 
 ////////////////////////////////////////////////////////////////
-const check =(list, item)=>{
-  return list.includes(item) ? true : false ;
-}
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -58,7 +48,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  
   let results =[];
   for(i = 0; i < array.length ; i++){
     if (!results.includes(array[i])){
