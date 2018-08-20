d2a60849b8fc57f5cbe07328b8b9fdd63164cec5
MAbbas <mabbashm110@gmail.com>
2018-08-20 16:54:22 -0500

    math operations refactored and contains - callbacks complete, no stretch yet

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -15,6 +15,18 @@ function arrayItems(arr, type){
   }
 }
 
+//MAIN ARITHMETIC FUNCTIONS
+function doMath(x, y, operation){
+  switch (operation){
+    case "sum":
+      return x + y;
+      break;
+    case "multiply":
+      return x * y;
+      break;
+  }
+}
+
 //REDUNDANT
 function checkFirst(arr){
   return arr[0];
@@ -32,31 +44,51 @@ function firstItem(arr, cb) {
 
 firstItem(items, arrayItems);
 
-
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr, "length");
 }
 
+getLength(items, arrayItems);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr, "last");
 }
 
+last(items, arrayItems);
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y, "sum");
 }
+
+sumNums(3, 4, doMath);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y, "multiply")
+}
+
+multiplyNums(7, 10, doMath);
+
+function itemInArray(item, arr){
+  if (arr.indexOf(item) == -1){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list, itemInArray);
 }
+
+contains("yo-yo", items, itemInArray);
+contains("nope", items, itemInArray);
 
 /* STRETCH PROBLEM */
 
