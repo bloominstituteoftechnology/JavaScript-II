0c9094f09a6098ce2d7257694024ccd0c1c7aa2d
MAbbas <mabbashm110@gmail.com>
2018-08-20 16:36:50 -0500

    array callback refactored

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,15 +1,48 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
+//MAIN ARRAY CALLBACK
+function arrayItems(arr, type){
+  switch (type){
+    case "first":
+      return arr[0];
+      break;
+    case "last":
+      return arr[arr.length - 1];
+      break;
+    case "length":
+      return arr.length;
+      break;
+  }
+}
+
+//REDUNDANT
+function checkFirst(arr){
+  return arr[0];
+}
+
+function arrayLength(arr){
+  return arr.length - 1;
+}
+
+//CHALLENGE
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr, "first");
 }
+
+firstItem(items, arrayItems);
+
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr, "length");
 }
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr, "last");
 }
 
 function sumNums(x, y, cb) {
--- assignments/closure.js
+++ assignments/closure.js
@@ -13,8 +13,33 @@ const counter = () => {
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// const counterFactory = () => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+// };
+
+
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+ 
+  let count = 0;
+  const increment = () => {
+    return ++count;
+  }
+
+  const decrement = () => {
+    return --count;
+  }
+
+  return { increment, decrement };
 };
+const countFoo = counterFactory();
+console.log(countFoo.increment());
+console.log(countFoo.increment());
+console.log(countFoo.increment());
+
+console.log(countFoo.decrement());
+console.log(countFoo.decrement());
