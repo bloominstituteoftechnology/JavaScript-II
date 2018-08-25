fc499d7502764f3f98871e22e03149930be91b15
hraya <hraya@lssu.edu>
2018-08-24 21:26:21 -0500

    added semi-color that was missing

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -64,7 +64,7 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
  let allCaps =runners.map(function(runners) {
-    return runners.first_name.toUpperCase()
+    return runners.first_name.toUpperCase();
   })
   
 console.log(allCaps); 
