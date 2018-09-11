ef94d6f67bec83091ad73050a4030cc0cac913af
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-11 17:59:19 -0400

    closure stretch challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,6 +13,7 @@ nameTag();
 
 // ==== Challenge 2: Create a counter function ====
   // Return a function that when invoked increments and returns a counter variable.
+
 let current = 0;
 const counter = () => {
   function increaseCount(){
@@ -20,9 +21,6 @@ const counter = () => {
   }
   increaseCount();
 };
-
-counter();
-counter();
 
 
 // Example usage: const newCounter = counter();
@@ -32,8 +30,18 @@ counter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+
+  let number = 0;
+  const counterFactory = () => {
+    function increment(){
+      return number++;
+    }
+    increment();
+    function decrement(){
+      return number--;
+    }
+    decrement();
+  };
