b7a4b702819b68c7dd299a6f054578ad9f97ca0f
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-11 12:49:26 -0700

    Finished callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -24,24 +24,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(len) {
+  console.log(len);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop());
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sumNums(5, 4, add));
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiplyNums(5, 4, multiply));
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    cb(true);
+  } else {
+    cb(false);
+  }
 }
+
+contains('Pencil', items, function(bool) {
+  console.log(bool);
+});
+
 
 /* STRETCH PROBLEM */
 
