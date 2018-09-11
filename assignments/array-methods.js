4b9566e8b498e81f502888f1229c9cdd31f96778
Jake Thomas <jmaxt12@gmail.com>
2018-09-11 15:54:39 -0600

    Callbacks complete. working on array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,12 +56,18 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-console.log(fullName);
+runners.forEach (function(person){
+    fullName.push(person.first_name + ' ' + person.last_name);
+})
 
+console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+
+let allCaps = runners.map (function(person){
+    return person.first_name.toUpperCase()
+})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -20,25 +20,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
-
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+function cb(arg){
+  console.log(arg)
 }
+function getLength(arr, cb) {
+  return cb(arr.length)
+  // getLength passes the length of the array into the callback.
+  
+}
+getLength (items, cb)
+
 
 function last(arr, cb) {
+  return cb(arr[arr.length - 1])
+
   // last passes the last item of the array into the callback.
 }
 
 function sumNums(x, y, cb) {
+  return cb(x + y)
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
 function multiplyNums(x, y, cb) {
+  return cb(x * y)
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
 function contains(item, list, cb) {
+  return cb(list.includes(item))
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
