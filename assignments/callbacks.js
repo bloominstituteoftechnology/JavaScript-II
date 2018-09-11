13f32a0847059fe8dd8c002b65e3ae7c4bbae290
pedro <pedroluismontesinosrivera@gmail.com>
2018-09-11 16:37:33 -0400

    did 3 of the challenges in the callbacks.js file

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,22 +21,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop());
 }
+last(items, function(lastItem){
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+function add(x, y){
+  console.log(x + y);
+}
+sumNums(2, 3, add);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+function multiply(x, y) {
+  console.log(x * y);
+}
+sumNums(6, 2, multiply);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -50,3 +67,5 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+
