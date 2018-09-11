bc579cab57ab2c922abc9b92083d82adf4b8fa43
Lee Len Formento <leebatef@gmail.com>
2018-09-11 18:23:45 -0400

    Stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -55,11 +55,17 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-
-//   const func = {};
-//   for (let i = 0; i < items.length; i++) {
-//     func[items[i]] = true;      
-//   }
-//   cb(Object.keys(func));     
-// }
-}
+  let newArr = [];
+  for (let i = 0; i < array.length; i++){
+    if (!newArr.includes(array[i])){
+      newArr.push(array[i]);
+    }
+  }
+  return cb(newArr);
+  }
+  
+  function returnX(x){
+    return x;
+  }
+  
+  removeDuplicates(items,returnX);
