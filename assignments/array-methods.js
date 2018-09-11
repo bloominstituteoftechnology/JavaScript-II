387898467aba786bac1719f49b69f126bf3b8f98
theryanclausen <theryanclausen@gmail.com>
2018-09-11 14:57:49 -0700

    forgot to save array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,9 +55,9 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-
-runners.forEach(x =>  console.log(`${x['first_name']} ${x['last_name']}`));
-
+let fullName = []
+runners.forEach(x =>  fullName.push(`${x['first_name']} ${x['last_name']}`));
+console.log(fullName)
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = runners.map(x => x['first_name'].toUpperCase());
