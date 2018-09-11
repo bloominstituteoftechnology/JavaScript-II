0a0e651164de0a34894bcf6379730cd3f2a9a951
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-11 12:31:55 -0700

    completed two callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,11 +24,22 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let lastItem = items[arr.length-1];
+  return cb(lastItem);
 }
+
+last(items, function(cb) {
+  console.log(cb);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
