a4d8037d041b5b2f3470ce500ea2ded20c71d451
csdisciple <oleksstasyev@gmail.com>
2018-09-11 18:57:45 -0400

    WOrking on closures and stretch goals

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -50,7 +50,12 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-    
+    list.filter(arrayItem =>{
+        if(item == arrayItem){
+            return cb(true)
+        }
+        else{return cb(false)};
+    });
 }
 
 /* STRETCH PROBLEM */
