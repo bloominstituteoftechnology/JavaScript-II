5e0ca90a413fd880151d931d6b8439e22cb71ac5
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-11 15:29:23 -0600

    finished array-methods and closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const niceHello = () => {
+    return "Hello, Hunter! You are programming so well today!"
+}
+console.log(niceHello());
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let i =0
+  let addToCount = () => {
+    return i++;
+  }
+  return addToCount;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+const anotherCounter = counter();
+console.log(anotherCounter());
+console.log(anotherCounter());
+console.log(anotherCounter());
+console.log(anotherCounter());
+console.log(anotherCounter());
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
