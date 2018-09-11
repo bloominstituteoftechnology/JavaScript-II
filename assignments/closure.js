96a0a771e0655fcacbd2c3285d7820b5527b74fe
Rahul Desai <rd825@georgetown.edu>
2018-09-11 18:16:38 -0400

    Forgot to save the closure file before last commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,6 +17,10 @@ armory('sword');
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  //    Example usage: const newCounter = counter();
+  //    newCounter(); // 1
+  //    newCounter(); // 2
+
   let count = 0;
   return function() {
       count++;
@@ -25,23 +29,38 @@ const counter = () => {
   }
 };
 
-const increment = counter();
-
-increment(); // 1
-increment(); // 2
-increment(); // 3
-
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
-
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+newCounter(); // 3
 
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
-
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  var counter = 0;
+
+  function increment() {
+    counter++;
+    console.log(counter);
+  };
+
+  function decrement() {
+    counter--;
+    console.log(counter);
+  };
+
+  return {
+    increment : increment,
+    decrement : decrement
+  };
 };
+
+var counter1 = counterFactory();
+counter1.increment();
+counter1.increment();
+counter1.increment();
+counter1.decrement();
