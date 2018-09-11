fef34bc7662c6655da39acd6580c561fa9b0768b
Wanda <doubleuevans@gmail.com>
2018-09-11 17:47:15 -0500

    end of day commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -8,9 +8,17 @@ function goodMorn(timeOfDay) {
 goodMorn('evening')
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (num) => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = num
+  while (count < 20) {
+    count++
+  }
+  return count
 };
+
+console.log(counter(7));
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
