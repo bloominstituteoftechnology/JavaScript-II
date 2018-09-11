01823423a9b02210a5a951061e908cadd34a1bb6
Katie Cleary <katiecleary@airmaster.lan>
2018-09-11 18:23:36 -0400

    finished closures. Still need to read up on the arrow stuff, so I just made them with the syntax I knew how to use

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,30 +54,74 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  
+//Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-console.log(fullName);
+runners.forEach(function(item) {
+item = {
+    'first_name': item.first_name,
+    'last_name':item.last_name
+    }
+      fullName.push(item)
+  }); 
 
+console.log(fullName);
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+// The event director needs to have all the runner's first names converted to uppercase because the
+// director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map(function(item) {
+    allCaps.push(item.first_name.toUpperCase())
+})
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+// The large shirts won't be available for the event due to an ordering issue. 
+// Get a list of runners with large sized shirts so they can choose a different size.
+// Return an array named largeShirts that contains information about the runners that have 
+//a shirt size of L and log the result
+let largeShirts = runners.filter((size) => {
+	return size.shirt_size === 'L'
+})
 console.log(largeShirts);
 
+  
 // ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// The donations need to be tallied up and reported for tax purposes.
+// Add up all the donations into a ticketPriceTotal array and log the result
+let ticketPriceTotal = runners.reduce((total, totalDonation) => {
+    return total += totalDonation.donation;}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce(). 
+// I want you to think of potential problems you could solve given the data set and the 5k fun run theme. 
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 - Create an email list to send 5k updates to participants 
+let emailList= [];
+runners.forEach(function(item) {
+item = {
+    'first_name': item.first_name,
+    'last_name':item.last_name,
+    'email':item.email
+    }
+      emailList.push(item)
+  }); 
 
-// Problem 2
+console.log(emailList);
 
-// Problem 3
+// Problem 2 Need a list of top donors to recognize on website 
+let topDonors = runners.filter((totalDonation) => {
+	return totalDonation.donation >= 250;
+})
+console.log(topDonors);
+
+// Problem 3 Skinix is a big corporate donor and the 
+//5K wants to have a special cocktail hour for their employees. Make a contact lists.
+let skinixEmployees= runners.filter((employees) => {
+	return employees.company_name == "Skinix";
+})
+console.log(skinixEmployees); 
+//apparently this will be an awkard 3 person cocktail hour 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function animalNoise(sound) {
+	let soundMade = sound;
+  	function animal(soundMade) {
+      if(soundMade == 'quack') {
+        return 'duck'
+      } else {
+        return 'who knows what kind of animal that is!'
+      }
+    }
+    return animal;
+  };
+  
+let animalType = animalNoise();
+
+console.log(animalType('quack'));
+console.log(animalType('meow'));
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+// Return a function that when invoked increments and returns a counter variable.
+function counter() {
+  let count = 0;
+  	function counting() {
+  count = count + 1;
+  return count;
+  }
+  return counting;
+};
+
+let getCount = counter();
+
+console.log(getCount());
+console.log(getCount());
+
+  
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
