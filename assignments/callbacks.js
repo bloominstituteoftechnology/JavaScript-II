ca209f91d96663d4fe1f01dc29a451c542885ac1
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-11 15:29:40 -0500

    Finished MVP working on some more stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,47 +1,57 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
-
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // Potential Solution:
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
-*/
-
+function firstItem(arr, cb) {
+  // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
+}
+firstItem(item, function(Item){
+  console.log(`The first item is ${Item}`);
+})
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(`The length of the array is ${length}.`)
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length -1 ]);
 }
+last(items, function(item){
+  console.log(`The last item of the array is ${item}`)
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+cb(x,y);
 }
+sumNums(2,4,function(x,y){
+  console.log(`The result is`, x + y);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x,y)
 }
+multiplyNums(2,4,function(x , y){
+  console.log(`The result is ` x * y);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(item,list);
 }
+contains('orange',items, function(item, list){
+  for(i = 0; i < list.length; i++){
+    if(list[i] === item){
+      return true;
+    }
+    return false;
+  }
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,25 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function myName(name){
+  console.log(`My name is ${name}`);
+  let age = 20;
+  function myAge(){
+    console.log(`my age is ${age}`)
+  }
+  myAge()
+}
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (closure = 0) => {
   // Return a function that when invoked increments and returns a counter variable.
+  let val = closure;
+  return function counter(){
+    val += 1;
+    console.log(val);
+    return val;
+  }
 };
+const newCounter = counter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -14,7 +28,13 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0;
+  console.log(counter);
   // Return an object that has two methods called `increment` and `decrement`.
+  let increment = () => counter += 1;
+  let decrement = () => counter -= 1;
+  let resObj = {increment, decrement};
+  return resObj;
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
