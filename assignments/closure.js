642c11504d5286ef95004328632c290e1c0c0df7
Andre Myrick <amyrick.820@gmail.com>
2018-09-11 18:07:37 -0400

    attempted stretch on closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -30,8 +30,17 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let objCount = 0;
+
+  let objCounter = {
+    increment: function() {
+      return objCount++;},
+    decrement: function() {
+      return objCount--;}
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-  
+
 };
+let newCounterFactory = counterFactory;
