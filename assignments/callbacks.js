b4b32a3bcf6187f6c04585ca5baffe01747b5353
Stublink <chrisrey95@gmail.com>
2018-09-11 14:11:09 -0400

    Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Notebook'];
 
 /* 
 
@@ -24,23 +24,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length -1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i ++) {
+    if(list[i] === item) {
+      cb(true);
+      return;
+    }
+  }
 }
 
 /* STRETCH PROBLEM */
@@ -49,4 +59,22 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let newArr = [];
+  for(let i = 0; i < array.length; i++) {
+    let exist = false;
+
+    for(let j = 0; j <= newArr.length; j ++){
+      if(array[i] === newArr[j]){
+        exist = true;
+      }
+    }
+
+    if (!exist) {
+      newArr.push(array[i]);
+    }
+  }
+  cb(newArr);
 }
+
+
+removeDuplicates(items, (arr) => console.log(arr));
