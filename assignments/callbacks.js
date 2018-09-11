6d41b43bc1f47cdd5e44cdafec997e16215329b9
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-11 15:25:36 -0500

    callback 2

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,11 +30,15 @@ getLength(items, (length => console.log(length)));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+last(items, (last => console.log(last)));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y)
 }
+sumNums(3, 6, ((x, y) => console.log(x + y)));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
