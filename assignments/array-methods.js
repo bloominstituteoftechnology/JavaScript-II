fdf267335230dcc6ca55f224f3027234caff0de5
unknown <brandongrych@gmail.com>
2018-09-11 16:38:47 -0500

    Project Completed

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,22 +56,38 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(runner) {
+	return fullName.push(runner["first_name"] + " " + runner["last_name"]);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+allCaps = runners.map(function(runner) {
+	return runner["first_name"].toUpperCase();
+})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(function(runner) {
+	return runner["shirt_size"] === "L";
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+runners.forEach(function(runner) {
+	return ticketPriceTotal.push(runner["donation"]);
+});
+
+let sum = ticketPriceTotal.reduce((a,b) => a + b);
+
 console.log(ticketPriceTotal);
+console.log(sum);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
@@ -80,4 +96,25 @@ console.log(ticketPriceTotal);
 
 // Problem 2
 
+//The event director needs to know how many donations was 100 and above.
+
+const arr100 = runners.filter(function(runner) {
+	return runner["donation"] >= 100;
+});
+console.log(arr100.length);
+
 // Problem 3
+
+//The event director needs to know total amount of donations 99 and less.
+
+const arr99 = runners.filter(function(runner) {
+	return runner["donation"] <= 99;
+});
+
+const arrDonation99 = [];
+arr99.forEach(function(runner) {
+	arrDonation99.push(runner.donation);
+});
+
+let sum99 = arrDonation99.reduce((a,b) => a + b);
+console.log(sum99);
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+return cb(arr.length);
 }
-
+getLength(items, function(length) {
+  console.log(length)
+})
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return(sum(x,y))
 }
+sumNums(2,3,sum);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return(multiply(x,y));
 }
+multiplyNums(2,3,multiply);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return(cb(item,list));
 }
+contains("Pencil",items,arrContains);
 
 /* STRETCH PROBLEM */
 
