48da818d37bf7381f84e98a1ac2dfd4ca3e3fb4a
Sarah Tennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-09-11 16:36:49 -0400

    completed Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,55 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, (arrLength) => console.log(`Length of array: ${arrLength}`));
+getLength(items, (arrLength) => console.log(`Length of array: ${arrLength/2} + ${arrLength/2}`));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, (lastElement) => console.log(`Last element: ${lastElement}`));
+last(items, (lastElement) => console.log(`${lastElement} ${lastElement} for Dum Dum.`));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(5, 5, (sum) => console.log(`The sum is ${sum}`));
+sumNums(5, 1, (sum) => {
+  for(let x = 0; x < sum; x++){
+    console.log(`${sum} - ${x} = ${sum-x}`)
+  }
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+const eleven = 11;
+multiplyNums(eleven, eleven, (product) => console.log(`The product of ${eleven} and ${eleven} is ${product}.`));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  return cb(!!(list.find((arrayItem) => arrayItem === item)));
+};
+
+//const testItem1 = 'Pencil';
+const testItem1 = 'Banana';
+contains(testItem1, items, (boolValue) => {
+  if(boolValue){
+    console.log(`${testItem1} is in the array!`);
+  } else {
+    return console.log(`${testItem1} is NOT in the array!`);
+  }
+});
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +80,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const noDuplicates = array.filter((element, currentIndex, filteredArray) => filteredArray.indexOf(element) >= currentIndex);
+  return cb(noDuplicates);
 }
+
+const testArray1 = ['sarah', 'jeff', 'gregory', 'yawen', 'mandy sue'];
+const testArray2 = ['sarah', 'sarah', 'sarah', 'jeff', 'jimothy'];
+removeDuplicates(testArray1, (noDups) => console.log(`Never had any dups!\n${noDups}`));
+removeDuplicates(testArray2, (noDups) => console.log(`Had some dups, but now they're gone.\n${noDups}`));
