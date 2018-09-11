7f852f9d52ba73c062d1c59f45c3e73336d57562
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-11 16:38:10 -0500

    callback.js last callback done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -49,8 +49,24 @@ multiplyNums(3, 2, ((x, y) => console.log(x * y)));
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
-
+contains('yo-yo', items, (item, list) => {
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item) {
+      return console.log('true');
+    }
+  }
+  return console.log('false');
+});
+// contains('hello', items, function(item, list) {
+//   for(let i = 0; i < list.length; i++) {
+//     if(list[i] === item) {
+//       return console.log('true');
+//     }
+//   }
+//   return console.log('false');
+// });
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
