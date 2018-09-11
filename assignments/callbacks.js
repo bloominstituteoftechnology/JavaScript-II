5515e5fe750bd7dc01aa3a564c813235f4cfcb7f
bummings <sup@fauxkno.com>
2018-09-11 17:38:06 -0400

    done with callbacks, working thru array methods

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,46 +2,95 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+     // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  // Potential Solution:
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // // Potential Solution:
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
-*/
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
 
+
+
+// getLength passes the length of the array into the callback.
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
+getLength(items, function(arr){
+  console.log(arr)
+});
+
+
+
+
+// last passes the last item of the array into the callback.
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1])
 }
 
+last(items, function(arr){
+  console.log(arr)
+})
+
+
+
+
+
+// sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
 
+function sum(num1, num2){
+  console.log(num1 + num2);
+}
+
+sum (109, 48, sum);
+
+
+
+
+// multiplyNums multiplies two numbers and passes the result to the callback.
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+function multiply(num1, num2){
+  console.log(num1 * num2);
 }
+
+multiplyNums(88, 400, multiply);
+
+
+
+
+
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
+function contains(item, list, cb) {
+  return cb(item, list);
+}
+
+function containsInList(item, list) {
+  console.log(list.includes(item));
+}
+
+contains('Gum', items, containsInList);
+
+
+
+
 
 /* STRETCH PROBLEM */
 
