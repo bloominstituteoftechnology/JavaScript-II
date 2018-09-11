8caefc11887e54c99288c9b044e4331334163d2d
Lee Len Formento <leebatef@gmail.com>
2018-09-11 19:41:54 -0400

    Stretch closure after meeting

--- assignments/closure.js
+++ assignments/closure.js
@@ -38,7 +38,17 @@ newCounter();
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
+  return {
+    increment: () => (++count),
+    decrement: () => (--count),
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+const newCounterFactory = counterFactory();
+newCounterFactory.increment(); // 1
+newCounterFactory.decrement(); // 0
+newCounterFactory.decrement(); // -1
