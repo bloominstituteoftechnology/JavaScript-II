033dea6eb109460b46bf9f83c5f1c47feebfb32b
ssmodish <smodish@hotmail.com>
2018-09-11 16:28:32 -0400

    Not sure if the fifth question is supposed to be a stretch goal since nothing is marked specifically as a challenge

--- README.md
+++ README.md
@@ -11,9 +11,10 @@ With some basic JavaScript principles in hand, we can now expand our skills out 
 
 ### Task 2: Callbacks
 This task focuses on getting practice with callback functions by giving you an array of values and instructions on what to do with that array.
-* [ ] Review the contents of the [callbacks.js](assignments/callbacks.js) file.  Notice you are given an array at the top of the page.  Use that array to aid you with your callback functions.  **Remember that a callback function is a function that is being passed around to other functions.**
-* [ ] Write out each function using the `ES5`  `function` keyword syntax.
- * [ ] Solve the problems listed.  Save the stretch problems until you have completed Tasks 1-4.
+* [*] Review the contents of the [callbacks.js](assignments/callbacks.js) file.  Notice you are given an array at the top of the page.  Use that array to aid you with your callback functions.  **Remember that a callback function is a function that is being passed around to other functions.**
+* [*] Write out each function using the `ES5`  `function` keyword syntax.
+* [*] Solve the problems listed.  Save the stretch problems until you have completed Tasks 1-4.
+#### THERE ARE FIVE CHALLENGES PLUS STRETCH? || IS THE FIFTH CHALLENGE IS SUPPOSED TO BE STRETCH?
 
 
 ### Task 3: Array Methods
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,29 +21,77 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// ========== Challenge 1 ?????????????
+console.log('========== Challenge 1 ?????????????');
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+const lengthSquared = function(length){
+  return length * length;
+}
+
+console.log(getLength(items, lengthSquared));
+
+
+
+// ========== Challenge 2 ?????????????
+console.log('\n\n========== Challenge 2 ?????????????');
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+const lastItemCapped = function(lastItem){
+  return lastItem.toUpperCase();
+}
+
+console.log(last(items, lastItemCapped));
+
+
+
+// ========== Challenge 3 ?????????????
+console.log('\n\n========== Challenge 3 ?????????????');
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+const halved = function(whole){
+  return whole / 2;
+}
+
+console.log(sumNums(9, 7, halved));
+
+
+
+
+// ========== Challenge 4 ?????????????
+console.log('\n\n========== Challenge 4 ?????????????');
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+console.log(multiplyNums(9, 7, halved));
+
+
+
+// ========== Challenge 5 ?????????????
+console.log('\n\n========== Challenge 5 ?????????????');
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
 }
 
+
+
+
 /* STRETCH PROBLEM */
+// ========== Stretch Challenge 
+console.log('\n\n========== Stretch Challenge ');
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
