d05f670e12fefbe6b79a39bf106e548dabb8f294
lajawanti <lajawanti.dhake@gmail.com>
2018-09-11 15:52:17 -0400

    callbacks.js Done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,23 +24,34 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(X * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item) {
+      return cb(true);
+    } else {
+      return cb(false);
+    }
+  }
 }
 
 /* STRETCH PROBLEM */
@@ -49,4 +60,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const set1 = new Set(array);
+  return cb(set1);
 }
