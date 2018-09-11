cdab18d951c74484d15129f2e16f1273be07d74a
Cwright0428 <cwright0428@yahoo.com>
2018-09-11 16:23:57 -0500

    proj progress

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,25 +55,41 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
-let fullName = runners.map((item) =>{
-    `${runners.first_name} ${runners.last_name}`
-});
+let fullName = runners.map( runners => `${runners.first_name} ${runners.last_name}`);
 
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps);
-
+    runners.forEach(each => {
+        allCaps.push(
+            each.first_name
+            .split("")
+            .map(eachletter => eachletter.toUpperCase())
+            .join("")
+        )
+    });
+    console.log(allCaps)
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(each => {
+    return each.shirt_size === "L"
+})
+
 console.log(largeShirts);
 
+
+let smallShirts = runners.filter(each => {
+    return each.shirt_size === "S"
+})
+
+console.log(smallShirts)
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((previous, current) => {
+
+}, initial);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
