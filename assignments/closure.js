d3f0378a01593ccd812df73370155068e8633d3a
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-11 16:42:23 -0500

    Closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+  function simple(x) {
+    const newNum = x
+    console.log(x)
 
+      function multiple() {
+        const y = 7
+        console.log(x,y)
+      }
+      multiple();
+  }
+simple(7);
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+let counter = 0;
+function increase() {
+  return counter++;
+}
+increase();
+increase();
+increase();
+increase();
+console.log(counter)
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
