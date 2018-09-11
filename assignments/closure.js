8288efe77324d71ee626ea7bb7ab14baa2155f86
Lee Len Formento <leebatef@gmail.com>
2018-09-11 15:25:50 -0400

    Finished closure.js. Stretch to do later.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,35 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+var multiplyTo = function(passed) { // function
+  var multiply = function(inner) { // function
+    return passed * inner; // returns value
+  };
+  return multiply; // returns function itself
+}
+
+// multiplyTo(3); // 3 / inner. Closure 3.
+var multiplyThree = new multiplyTo(3); // new variable
+var multiplyFour = new multiplyTo(4); 
+
+// Print
+console.log(multiplyThree(1)); // 3 x 1
+console.log(multiplyFour(2)); // 2 x 4
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+const counter = () => {
+  let counter = 0;
+  let plusOne = function() {
+  return ++counter;
+}
+  return plusOne;
+
+}
+const newCounter = counter();
+newCounter();
+newCounter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
