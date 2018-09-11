528ee917ec5f720bd23e2633807d2290f11b89cf
Josh Knell <bigknell@gmail.com>
2018-09-11 10:06:26 -0600

    added instruction

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,26 @@
+// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
-}
+/* 
+
+  //Given this problem: 
+  
+  function firstItem(arr, cb) {
+    // firstItem passes the first item of the given array to the callback function.
+  }
+
+  // Potential Solution:
+  function firstItem(arr, cb) {
+    return cb(arr[0]);
+  }
+
+  firstItem(items, function(first) {
+    console.log(first)
+  });
+
+*/
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
