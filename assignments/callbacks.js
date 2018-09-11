708c7995008223b049735a13a02fdff8136ca906
Ayinde K. Williams <ayindetec10@gmail.com>
2018-09-11 17:00:37 -0500

    Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,13 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work.
+//You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -22,18 +24,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+function getLength(arr, cb,) {
+return cb(arr);  // getLength passes the length of the array into the callback.
 }
+
+getLength(items.length, function() {
+  console.log(items.length)
+});
+
+
+
+
+
+
+
+
+
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+return cb(arr[3]);  // last passes the last item of the array into the callback.
 }
+
+last(items, function(last){
+  console.log(last)
+})
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+return cb(arr)  // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
+sumNums(items )
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
