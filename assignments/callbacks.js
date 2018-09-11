e9a34f572602222b6655d8535f53f65cb950852b
samsisle <samko9522@gmail.com>
2018-09-11 16:09:10 -0700

    Update callbacks assignment (#1)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,7 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have
+// been provided an example of a problem and a solution to see how this works with
+// our items array.  Study both the problem and the solution to figure out the rest
+// of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -24,24 +27,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  let arg = arr.length;
+  return cb(arg);
 }
+
+getLength(items, function(arg) {
+  console.log(arg);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let arg = arr[arr.length - 1];
+  return cb(arg);
 }
+
+last(items, function(arg) {
+  console.log(arg);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let arg = x + y;
+  return cb(arg);
 }
+
+sumNums(1, 2, function(arg) {
+  console.log(arg);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let arg = x * y;
+  return cb(arg);
 }
+
+multiplyNums(1, 2, function(arg) {
+  console.log(arg);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+  let arg = list.includes(item);
+  return cb(arg);
 }
+
+contains('Gum', items, function(arg) {
+  console.log(arg);
+});
+
 
 /* STRETCH PROBLEM */
 
