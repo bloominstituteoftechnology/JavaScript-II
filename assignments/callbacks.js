504e44b90053ec4febda63883550afc1c92bf900
Edward J <edjeong99@gmail.com>
2018-09-11 18:39:05 -0400

    complete all project and all stratched

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum", "Pencil", "yo-yo"];
 
 /* 
 
@@ -21,27 +21,51 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+sumNums(3, 5, function(x, y) {
+  console.log(x + y);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+multiplyNums(3, 5, function(x, y) {
+  console.log(x * y);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
+
+contains("Pencil", items, function(item, list) {
+  console.log(list.includes(item));
+});
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +73,17 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array.lastIndexOf(array[i]) === i) {
+      newArr.push(array[i]);
+    }
+  }
+  return cb(newArr);
 }
+
+console.log(
+  removeDuplicates(items, function(array2) {
+    console.log(array2);
+  })
+);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,50 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+// function greeting(namePara, meal, beverage){
+//   const name = namePara;
+ 
+
+//   console.log (`hi ${name}`);
+
+//   function eat(meal){
+//     console.log(`${name}, eat your ${meal}`);
+
+//     function drink (beverage) {
+//       console.log(`${name}, drink your ${beverage}`);
+//     }
+
+//     drink(beverage);
+//   }
+
+//   eat(meal);
+// }
+
+// greeting ("ed", "dinner", "wine");
+
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let i = 0;
+  return function temp(){
+    return i++;
+  }
+
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -17,4 +53,28 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let i = 0;
+
+  return {
+    increment: function() {
+      return ++i;
+    },
+    decrement: function() {
+      return --i;
+    }
+  };
 };
+
+const newCounterFactory = counterFactory();
+
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+
