28b26850caf60c524eea239d69d1c4a6690bc394
csdisciple <oleksstasyev@gmail.com>
2018-09-11 18:13:10 -0400

    Finished arrays and callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -22,25 +22,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length);
+
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+    let result = x+y;
+    return cb(result);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    let multiplyResult = x * y;
+    return cb(multiplyResult);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+    
 }
 
 /* STRETCH PROBLEM */
