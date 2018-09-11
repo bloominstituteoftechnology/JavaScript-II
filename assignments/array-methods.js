c1969b9452154db79515f075b659b66170ca5041
Wanda <doubleuevans@gmail.com>
2018-09-11 16:39:35 -0500

    complete array-methods challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -72,6 +72,15 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = []; //shirt_size
+runners.filter(function(person) {
+  if(person.shirt_size === 'L') {
+    largeShirts.push(person)
+  }
+})
+//`${person.first_name} ${person.last_name}`
+
+//const largeShirts = runners.filter(person => person.shirt_size === 'L')
+//console.log(largeShirt)
 
 console.log(largeShirts);
 
