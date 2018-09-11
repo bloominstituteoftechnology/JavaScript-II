7f53d70759ce36d417123007430a7e3861f682ea
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-11 15:50:12 -0700

    finished some of closure.js and some of callbacks.js files, but didn't complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function ClassBegins(whatDay) {
+  const day = whatDay;
+  console.log(`Class is on ${day}`);
 
+  function className() {
+    const className = "FSW14";
+    console.log(`Hello ${className}, class is on ${day}.`);
+
+    function time() {
+      const time = "5:00 PM";
+      console.log(`Hello ${className}, class is on ${day} at ${time}.`);
+    }
+    time();
+  }
+  className();
+}
+
+ClassBegins("Monday");
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let num = 0;
+  function plusOne() {
+    return num++;
+  };
+  return plusOne; {
   // Return a function that when invoked increments and returns a counter variable.
 };
+let newCounter = counter();
+console.log(newCounter());
+// ????? CONFUSED ^
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
