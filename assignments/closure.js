65cf12cb302c65b86caabe3354bace4ad01a085e
Michael Checo <checomichael2@gmail.com>
2018-09-11 15:49:08 -0400

    finished 1st challenge on closures assignment.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,26 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function anActivity(activityParameter) {
+  let activity = activityParameter;
+  console.log(`Hey, lets go to the ${activity}`);
 
+  function boringActivity() {
+    let boring = "Calculus HW";
+    console.log(
+      `Hey, let's not go to the ${activity} and do our ${boring} instead.`
+    );
+    function funActivity() {
+      let fun = "Ice Cream";
+      console.log(
+        `${boring} is lame, and I'm too lazy for the ${activity}, let's instead go out for ${fun}`
+      );
+    }
+    funActivity();
+  }
+  boringActivity();
+}
+anActivity("gym");
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
