47f94e1beee498465310d7827c93f894fe9db242
Riley-Brown <riley@riley.gg>
2018-09-11 17:58:37 -0400

    Finishing up tasks

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const myClosure = 123;
+function returnClosure() {
+  return myClosure;
+}
+console.log(returnClosure());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
 };
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
