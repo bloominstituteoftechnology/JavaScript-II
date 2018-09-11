8bcf592645470aa31a44177e5475bce7bd86a575
zacharyarney <zachary.arney@gmail.com>
2018-09-11 15:09:55 -0500

    initial commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,6 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
