708d5154d6db095564861fc670b16b17c7334095
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-11 14:40:59 -0500

    Callbacks started

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,7 +24,12 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+function length(arr){
+  console.log(arr.length);
+}
+getLength(items, length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
