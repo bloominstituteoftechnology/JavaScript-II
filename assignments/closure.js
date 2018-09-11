d1827c415e6e9565a00ffd351dca72af144062e0
MEBonham <programmer@mckaybonham.com>
2018-09-11 14:33:28 -0600

    Got simple closure written and counter function working

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,42 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function example() {
+  const d = new Date();
 
+  function displayDate() {
+    console.log(d);
+  }
+
+  displayDate();
+}
+
+example();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let timesPinged = 0;
+
+  displayCount = function() {
+    timesPinged++;
+    console.log(timesPinged);
+  }
+  return displayCount;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
+newCounter();
+newCounter();
+
+const anotherInstance = counter();
+anotherInstance();
+anotherInstance();
+newCounter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
