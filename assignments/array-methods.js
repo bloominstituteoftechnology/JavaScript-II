f37f1ffe373888e2ad9b00194db3876202605a08
rytwalker <rytwalker@gmail.com>
2018-09-12 08:40:15 -0400

    Updated console.logs to be more informative

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -461,37 +461,37 @@ let fullName = [];
 runners.forEach(runner =>
   fullName.push(`${runner['first_name']} ${runner['last_name']}`)
 );
-console.log(fullName);
+console.log('Full names: \n', fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = runners.map(runner => runner['first_name'].toUpperCase());
-console.log(allCaps);
+console.log('Uppercase FIRST NAMES: \n', allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = runners.filter(runner => runner['shirt_size'] === 'L');
-console.log(largeShirts);
+console.log('Runners with large shirt sizes: \n', largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = runners.reduce((reducer, runner) => {
   return reducer + runner['donation'];
 }, 0);
-console.log(ticketPriceTotal);
+console.log(`Total donations: $${ticketPriceTotal}`);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-// Runners that work at Skinix still need to fill out their biograpies for the fun run program. Find all the runners that work at Skinix then put their emails into an array so they can be sent a reminder email.
+// Runners that work at Skinix still need to fill out their bios for the Fun Run program. Find all the runners that work at Skinix then put their emails into an array so they can be sent a reminder email.
 const emails = runners
   .filter(runner => runner['company_name'] === 'Skinix')
   .map(runner => runner['email']);
-console.log(emails);
+console.log('Send a reminder email to: \n', emails);
 
 // Problem 2
-// The event committee needs to tier the donations for the program. Group the runners into donor categories based on their contribution (below) and print out the companies in each category
+// The event committee wants to tier the donations for the program. Group the runners into donor categories based on their contribution (below) and print out the companies in each category
 
 // Gold: $300-$250
 // Silver $249 - $150
@@ -507,14 +507,25 @@ const bronze = runners
   .filter(runner => runner['donation'] <= 149)
   .map(company => company['company_name']);
 
-console.log('Our gold company donors giving between $250-$300 are:', gold);
-console.log('Our silver company donors giving between $150-$249 are:', silver);
-console.log('Our bronze company donors giving between $1-$149 are:', bronze);
+console.log('Our Gold company donors giving between $250-$300 are: \n', gold);
+console.log(
+  'Our Silver company donors giving between $150-$249 are: \n',
+  silver
+);
+console.log('Our Bronze company donors giving between $1-$149 are: \n', bronze);
 
 // Problem 3
 // Every runner that donated over $150 was entered into a contest to win a new iPad (or something!) Put all the runners that have donated $150 or more into an array and pick a winner.
-
-const entries = runners.filter(runner => runner['donation'] >= 150);
+const entries = [];
+runners
+  .filter(runner => runner['donation'] >= 150)
+  .forEach(runner =>
+    entries.push(
+      `${runner['first_name']} ${runner['last_name']} from ${
+        runner['company_name']
+      }`
+    )
+  );
 const randomNum = Math.floor(Math.random() * entries.length);
 const winner = entries[randomNum];
-console.log(winner);
+console.log(`${winner} won an iPad (or something!)`);
