070ad7ab64b6d5169acb6f6da2498663456ddc91
Joe Bugajski <yogurtmystery@gmail.com>
2018-09-11 23:08:19 -0500

    completed callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -20,28 +20,44 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+function hey(x) {
+  console.log("The answer is " + x)
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length)
 }
 
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[(arr.length - 1)])
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y)
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y)
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  hits = 0;
+  for(i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      hits += 1;
+    }
+  } if (hits > 0) {
+    cb(true)
+  } else {
+    cb(false)
+  }
 }
 
 /* STRETCH PROBLEM */
