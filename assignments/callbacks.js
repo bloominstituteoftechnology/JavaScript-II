c66317fa13f34cfc99e2af8d2a41e80568b8fc67
rytwalker <rytwalker@gmail.com>
2018-09-12 08:52:48 -0400

    Added comments for readability and ES5 callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,32 +21,59 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+// Callback #1
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr);
+  return cb(arr.length);
 }
 
-getLength(items, () => console.log(`There are ${items.length} items.`));
+// ES5
+getLength(items, function(length) {
+  console.log(`There are ${length} items.`);
+});
 
+// Arrow Function
+getLength(items, length => console.log(`There are ${length} items.`));
+
+// Callback #2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length - 1]);
 }
 
+// ES5
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
+
+// Arrow Function
 last(items, lastItem => console.log(lastItem));
 
+// Callback #3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
+// ES5
+sumNums(10, 4, function(sum) {
+  console.log(sum);
+});
 
+// Arrow Function
 sumNums(5, 6, sum => console.log(sum));
 
+// Callback #4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x * y);
 }
 
+//ES5
+multiplyNums(30, 2, function(product) {
+  console.log(product);
+});
+
+// Arrow Function
 multiplyNums(10, 6, product => console.log(product));
 
 function contains(item, list, cb) {
@@ -55,26 +82,27 @@ function contains(item, list, cb) {
   console.log(cb(item, list));
 }
 
-function containsItem(item, list) {
+//ES5
+contains('Pencil', items, function(item, list) {
   for (let i = 0; i < list.length; i++) {
     if (list[i] === item) {
       return true;
     }
   }
   return false;
-}
+});
 
-contains('Pencil', items, containsItem);
+// Arrow Function
+contains('Pencil', items, (item, list) => {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return true;
+    }
+  }
+  return false;
+});
 
 /* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-  const noDoubles = array.filter((item, index) => array.indexOf(item) >= index);
-  return cb(noDoubles);
-}
 
 const itemsWithDuplicates = [
   'Pencil',
@@ -90,5 +118,13 @@ const itemsWithDuplicates = [
   'Binder',
   'Binder'
 ];
+
+function removeDuplicates(array, cb) {
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
+  const noDoubles = array.filter((item, index) => array.indexOf(item) >= index);
+  return cb(noDoubles);
+}
 
 removeDuplicates(itemsWithDuplicates, items => console.log(items));
