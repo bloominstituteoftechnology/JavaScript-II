b8459975f24d3470755b584a5c124e6dcf821552
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-14 07:43:16 -0700

    finished with callbacks file

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,19 +26,18 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr);
 }
-
 length(items, function(length) {
   console.log(length);
 });
 
-
+// ////////
 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr);
 }
-function lastItem(arr) {
+lastItem(arr) {
   console.log(arr[arr.length - 1]);
 }
 last(["hi", "how", "are", "you"], lastItem);
