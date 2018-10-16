189c3abc53b7f1b7a75c756872511e014c0bca9f
Joseph Eastman <josephryan96@gmail.com>
2018-10-16 13:20:37 -0700

    callbacks.js MVP, closure.js MVP, array-methods.js WIP

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,54 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+const add = function(x, y){
+  console.log(x + y);
+}
+
+sumNums(5,2,add);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+function multiply(x, y){
+  console.log(x * y);
+}
+
+multiplyNums(2, 4, multiply);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+ if (cb(list.includes(item))) {
+   return true;
+ } else {
+   return false;
+ }
 }
+
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function sayHello(hello){
+  const say = hello;
+  console.log(`${say}`);
+
+  function whatsUp(){
+    const item = "what's up?";
+    console.log(`${say} ${item}`)
+  }
+  whatsUp();
+}
+
+sayHello('Hey');
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => (++count)
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+ //newCounter(); // 1
+ //newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
