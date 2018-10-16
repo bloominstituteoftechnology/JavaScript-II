9ffaf263c193a85125b91ada07a1d3364f5a997f
jheinz1999 <jonathan.heinz@aol.com>
2018-10-16 13:20:49 -0700

    finished closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,10 +5,17 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return () => console.log(++counter);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
