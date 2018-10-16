d5263e19836aedf42aad44dae4e73435be57ad14
Aman <amsingh714@gmail.com>
2018-10-16 18:03:56 -0400

    finished all requirments

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function sayHello() {
+  let employee = "Dwight Schrute";
+  let job = "Salesman";
+  console.log(`Hello, my name is ${employee}. I am a ${job}.`);
+  function newJob() {
+    let newJob = "Assistant Regional Manager";
+    console.log(`Hello, my name is ${employee}. I am now an ${newJob}.`);
+
+    function demotion() {
+      let demotedJob = "Assistant to the Regional Manager";
+      console.log(`Hello, my name is ${employee}. I am now an ${demotedJob}. =( `);
+    }
+    demotion();
+  }
+  newJob();
+}
+
+sayHello();
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  return () => (++count);
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -17,4 +43,17 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  let counters = {
+    increment: function(count) {
+      return count++;
+    },
+    decrement: function(count) {
+      return count--;
+    }
+  }
 };
+
+let counting = counterFactory();
+
+console.log(counting.counter.increment);
