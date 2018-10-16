ea9f94f0cc9725796df6ce6abe74c388e6e3bdab
Jarrad Miller <jm020801@gmail.com>
2018-10-16 16:26:22 -0600

    trying to be better at pushing

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -22,9 +22,18 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
+
   // getLength passes the length of the array into the callback.
-}
+
+function getLength(arr, cb) {
+    return cb(arr[0]);
+  }
+  getLength(items, items.length) {
+    console.log(getLength)
+  };
+console.log(getLength);
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
