2dcc5bcd050244ef6529d5b952660e7e4027211a
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-10-16 12:46:13 -0700

    add answers to callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -22,26 +22,75 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
+//function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+//}
+
+function getLength(arr, cb) {
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+  console.log(length);
+})
+
+
+// function last(arr, cb) {
+  // last passes the last item of the array into the callback.
+// }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
-function sumNums(x, y, cb) {
+last(items, function(last){
+  console.log(last);
+})
+
+
+//function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+//}
+
+function sumNumbs(x, y, cb){
+  return cb(x+y);
 }
 
-function multiplyNums(x, y, cb) {
+sumNumbs(1, 2, function(sum){
+  console.log(sum);
+})
+
+
+//function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+//}
+
+function multipleNumbs(x, y, cb) {
+  return cb(x*y);
 }
 
-function contains(item, list, cb) {
+multipleNumbs(2, 3, function(product){
+  console.log(product);
+})
+
+
+//function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+//}
+
+function contains(item, list, cb){
+  
+  if (list.indexOf(item) === -1){
+    return cb(false);
+  }
+  return cb(true);
 }
+
+contains('yo-yo', items, function(result){
+  console.log(result);
+})
+
 
 /* STRETCH PROBLEM */
 
