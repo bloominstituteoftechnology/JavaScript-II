71275d614f8429b63136cc2dbfba45def19a6429
brooks <bpoltl1@gmail.com>
2018-10-16 16:46:38 -0500

    javascript exercises

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,46 +2,69 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+ 
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  // Potential Solution:
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // // Potential Solution:
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+getLength(items, function(strLength){
+  console.log(items.length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1])
 }
+last(items, function(lastValue){
+  console.log(lastValue);
+})
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y) ;
 }
-
+sumNums(2, 3,  function(sum) {
+  console.log(sum);
+});
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+multiplyNums(3, 4, function(mult){
+  console.log(mult);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+    cb(list.includes(item))
+}  
+  
+
+
+contains('Pencil',items,function(contain){
+  console.log(contain);
+});
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,35 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function square(){
+  let num = 5;
+  function logNum(){
+    console.log(num);
+  }
+
+  num = num**2;
+
+  return logNum;
+
+}
+let number = square();
+number();
+
+
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  // Return a function that when invoked increments and returns a counter variable
+  let count = 0;
+
+  return()=>(++count);
+  
 };
+const county = counter();
+
+console.log(county())
+console.log(county())
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
