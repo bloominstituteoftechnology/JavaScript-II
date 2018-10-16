4f2f77a124ee5a4707472de93cb14c6ca667597c
jamespagedev <jamespagedev@outlook.com>
2018-10-16 11:17:50 -0700

    callbacks.js mvp complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,30 +21,77 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+/*************************************************************
+ ************************* Problem 1 *************************
+ *************************************************************/
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+getLength(items, function(arrLength){
+  console.log(arrLength);
+});
+
+/*************************************************************
+ ************************* Problem 2 *************************
+ *************************************************************/
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+last(items, function(lastItem){
+  console.log(lastItem);
+});
+
+/*************************************************************
+ ************************* Problem 3 *************************
+ *************************************************************/
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+sumNums(2, 3, function(sum){
+  console.log(sum);
+});
+
+/*************************************************************
+ ************************* Problem 4 *************************
+ *************************************************************/
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+multiplyNums(2, 3, function(product){
+  console.log(product);
+});
+
+/*************************************************************
+ ************************* Problem 5 *************************
+ *************************************************************/
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)){
+    return cb(true);
+  }
+  return cb(false);
 }
 
-/* STRETCH PROBLEM */
+contains('Notebook', items, function(item){
+  console.log(item);
+}); // true
 
+contains('Notepad', items, function(item){
+  console.log(item);
+}); // false
+
+/*************************************************************
+ ********************** STRETCH PROBLEM **********************
+ *************************************************************/
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
