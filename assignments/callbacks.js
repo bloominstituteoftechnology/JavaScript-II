bcf46e96195ece0141b5d2399492659902db426f
Mr. Metelus <Mdmetelus@gmail.com>
2018-10-16 19:12:39 -0400

    "MVP Complete"

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -24,24 +24,87 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  let outPut = cb(arr);
+  return outPut;
 }
+function callback( arr ){
+  console.log(arr.length);
+  return arr.length;
+}
+
+getLength(items, callback);
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let output = cb(arr[arr.length -1]);
+  return output;
 }
 
-function sumNums(x, y, cb) {
+function callback0(arr) {
+  console.log();
+  return console.log()
+}
+
+last( items , callback0);
+
+// function callback( arr ){
+//   console.log(arr.length);
+//   return arr.length;
+// }
+// function callback1(){
+//   console.log(
+//   return output;
+// }
+
+last(items,callback0);
+
+
+function sumNums(x, y) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let output = (x + y);
+  return outPut;
 }
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+publishSum(sumNums()) {
+  let pub = console.log(sumNums());
+  return pub;
 }
+
+publishSum(sumNums(10,20));
+
+function multiplyNums(x, y) {
+  // multiplyNums multiplies two numbers and passes the result to the callback.
+  let output = ( x * y );
+  return output;
+}
+
+function publishMul(multiplyNums){
+  return console.log(multiplyNums);
+}
+
+publishMul(multiplyNums(4,3));
+
+
+
 
 function contains(item, list, cb) {
+  tOrF = [];
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0 ; i < list. length; i++){
+    if (list[i].item. !== '') {
+      console.log(true);
+      tOrF.push(true);
+    } else {
+      console.log(false);
+      tOrF.push(true);
+    }
+  }
+  return (Callback => {return console.log(tOrF)};
 }
+contains(items,arr.length,);
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,45 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function yell(name){
+  const n = name;
+  console.log(`Can you hear Me ${n}!!!!!!`);
+
+  function speak(nickName){
+    const nn = "pebbles";
+    console.log(` Hey, ${ nn } is your given name ${ n } , i'd never heard it before.`);
+
+    function whisper() {
+      const lower = "I'm sorry for speaking so loud." ;
+      console.log (`{lower} Hey,Would you rather me call you  ${ nn } or ${ n }, i knowsome people havea preference.`)
+    }
+    whisper();
+  }
+  speak();
+}
+yell();
+
+
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => (++count);
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
