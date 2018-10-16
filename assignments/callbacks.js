5f5f0ee13042c37e040888a7026c121dd43e592c
Kai Lovingfoss <kailovingfoss@Kais-iMac.local>
2018-10-16 14:15:19 -0700

    wow, so amaze, so callback, commiting now cuz trevor said to do so

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -10,7 +10,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     // firstItem passes the first item of the given array to the callback function.
   }
 
-  // Potential Solution:
+  // Potential Solution:.
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
@@ -24,20 +24,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length)
+});
 
-function last(arr, cb) {
+function lastItem(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+lastItem(items, function(last) {
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(5, 8, function(sum) { //forget it, ill plug something random in
+  console.log(sum)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
+multiplyNums(3, 6, function(product) {
+  console.log(`Hey, here is the product : ${product}`);
+})
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
