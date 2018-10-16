3f9c0b3258642b711fb63bc191f2a60899022cdb
Jasmine <jasmineanderson315@yahoo.com>
2018-10-16 19:31:42 -0400

    updated a few files

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+fuction subtract (a) {
+  let subtractNum = a;
+
+  return function (b) {
+    return a - b;
+  }
+}
+
+let subtractSix = subtract(6);
+let subtractFive = subtract(5);
+let 
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return function () {
+    return ++counter;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
