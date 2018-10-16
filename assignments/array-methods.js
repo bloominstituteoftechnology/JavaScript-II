d1d170b34d6752a09ee5ae271930bd2622c35912
Addison-Stavlo <AddieStavlo@gmail.com>
2018-10-16 16:13:14 -0400

    array-methods complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,38 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach( item => fullName.push(item.first_name + ' ' + item.last_name));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(item => item.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(item => item.shirt_size === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce((total,item)=> total + item.donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// change everyones first name to their last name!?
+runners.forEach( item => item.first_name = item.last_name);
+console.log(runners);
 
 // Problem 2
+// new array with first name, last name, and email to upper case
+let someCaps = runners.map( item => [item.first_name.toUpperCase(), item.last_name.toUpperCase(), item.email.toUpperCase()]);
+console.log(someCaps);
 
 // Problem 3
+// new array with list of people of shirt size L who donated less than $10, or size XL who donated less than $20
+let largeShirtsSmallDonations = runners.filter(item => (item.shirt_size === "L" && item.donation < 10) || (item.shirt_size === "XL" && item.donation < 20));
+console.log(largeShirtsSmallDonations);
