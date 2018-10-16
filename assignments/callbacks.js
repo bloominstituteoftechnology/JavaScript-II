e3e1868c2dc571df825ab22262ab31e8606cbb22
Civoremo <omerovic_n@hotmail.com>
2018-10-16 15:42:25 -0400

    callbacks.js complete w/o stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -22,27 +22,58 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(3, 19, function(sum){
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 5, function(multi){
+  console.log(multi);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
+  let bCheck = false;
+    if (list.includes(item)) {
+      return cb (!bCheck);
+    }
+    else {
+      return cb(bCheck);
+    }
 }
+
+contains('Pencil', items, function(avail){
+  console.log(avail);
+});
 
 /* STRETCH PROBLEM */
 
