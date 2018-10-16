b39a7493cf7e642d4f0aa17feaca9855b9426b9e
Kai Lovingfoss <kailovingfoss@Kais-iMac.local>
2018-10-16 15:05:04 -0700

    I am done with Task 2, Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -56,12 +56,29 @@ multiplyNums(3, 6, function(product) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < item.length; i++) {
+    if (item[i] === list) {
+      return cb(true);
+    } 
+  }
+  return cb(false);
 }
 
+contains(items, 'Pencil', function(result) {
+  console.log(result ? 'Pencil is there, yo!' : 'Pencil is not in the array');
+  console.log(result ? '${result} is there, yo!' : 'Nothing here, man...')
+});
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
+
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  return cb(Array.from(new Set(array)));
 }
+removeDuplicates(items, function(uniqueItems) {
+  console.log('Here is our new, array with only unique items: ' + uniqueItems);
+  //crap, it slams the uniqueItems together, need to space them out or something...
+});
