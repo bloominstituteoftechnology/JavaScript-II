686a2bd7fb14628e476a442ac3ce2a88d0fb1781
wcolts2000 <wcolts2000@gmail.com>
2018-10-16 13:51:25 -0700

    completed task 4 closures mvp, beginning to work on stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,55 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function animalSpeak(animalSound) {
+  const sound = animalSound;
+//  debugger;
+
+  if(sound === "meow") {
+  //  debugger;
+    function catSpeak() {
+      const catSentence = `${sound} ${sound} ${sound}`;
+      console.log(catSentence);
+  //    debugger;
+      
+    }
+    catSpeak();
+   // debugger;
+  } 
+  if(sound === "woof") {
+   // debugger;
+    function dogSpeak() {
+      const dogSentence = `${sound} ${sound} ${sound} ${sound}`;
+      console.log(dogSentence);
+     // debugger;
+    }
+     dogSpeak()   
+  } 
+}
+animalSpeak("woof");
+animalSpeak("meow")
 
 
 // ==== Challenge 2: Create a counter function ====
+// const counter = () => {
+//   // Return a function that when invoked increments and returns a counter variable.
+//   let count = 0;
+//   return function() {
+//     return count += 1;
+//   }
+// };
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let count = 0;
+  return () => count += 1;
+  };
+// Example usage: 
+const newCounter = counter();
+console.log(newCounter());
+newCounter(); // 1
+console.log(newCounter());
+newCounter(); // 2
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
