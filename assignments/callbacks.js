ff9b4a44cb2187d44132d8f35a0b37bd33b31b04
Cengiz Family <cengizbug@Cengizs-iMac.local>
2018-10-17 09:58:24 -0600

    I got as far as I could

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,5 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution 
+// to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -22,26 +23,73 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr)
 }
+
+const length = function(arr){
+  console.log(arr.length);
+}
+
+getLength(items, length);
+
+// ___________________________________________________________________________
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr);
 }
+
+const theLast =  function(arr){
+  console.log(arr.pop());
+}
+
+last(items, theLast);
+
+// ___________________________________________________________________________
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
+
+const sum = function(x,y){
+  console.log(x + y);
+}
+
+sumNums(3,2,sum);
+
+// ___________________________________________________________________________
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+    return cb(x,y);
+  }
+  
+  const multiply = function(x,y){
+    console.log(x * y);
+  }
+  
+  multiplyNums(3,2,multiply);
+
+// ___________________________________________________________________________
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item,list);
 }
+
+const check = function(item,list){
+  console.log(item.includes(item));
+}
+
+contains('yo-yo', items, check);
+
+// ___________________________________________________________________________
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,53 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function movies(title) {
+  const movieName = title;
+  console.log(`My favorite movie is ${movieName}`)
+  
+  function actor() {
+    const favActor = "Denzel Washington";
+    console.log(`My favorite actor is ${favActor} and my favorite movie is ${movieName}`);
+
+    function director() {
+      const favDir = "Frank Capra";
+      console.log(`${favDir} is my favorite movie director, ${favActor} is my favorite movie actor, and ${movieName} is my favorite movie. `)
+    }
+    director();
+  }
+  actor();
+}
+
+movies("Braveheart");
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => (++count)
+  
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
+  return () => (++count)
+  return () => (--count)
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
