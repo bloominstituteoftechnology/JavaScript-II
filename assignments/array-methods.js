72b08a6a38a6a02eb6976d6d9242cf1c7167b678
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-10-19 15:01:13 -0500

    Completed 1-3 w/o Stretch Challenges

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,23 +54,27 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
+runners.forEach((element, index) => fullName.push(`${runners[index].first_name} ${runners[index].last_name}`));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+allCaps = runners.map((element, index) => `${runners[index].first_name.toUpperCase()} ${runners[index].last_name.toUpperCase()}`);
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter((element, index) => runners[index].shirt_size === 'L');
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((total, runner, index) => total + runners[index].donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -20,27 +20,53 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+getLength(items, length => console.log(length));
+last(items, last => console.log(last));
+sumNums(5, 10, sum => console.log(sum));
+multiplyNums(5, 10, multiply => console.log(multiply));
+contains("Pencil", items, contain => console.log(contain));
+contains("Notebook", items, contain => console.log(contain));
+contains("yo-yo", items, contain => console.log(contain));
+contains("Gum", items, contain => console.log(contain));
+contains("Games", items, contain => console.log(contain));
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x+y;
+  return cb(result);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x*y;
+  return cb(result);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let tempArr = [];
+  for(let i = 0; i < list.length; i++){
+    if(list[i] === item){
+      tempArr.push(item);
+    }
+  }
+  if(tempArr.length > 0){
+    cb(true);
+  }
+  else {
+    cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
