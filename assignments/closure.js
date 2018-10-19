4d9f702074c9c5f94a129c0f2a18cbc4bee9dfef
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-10-19 15:51:48 -0500

    Completed Task 4 w/o Stretch Challenge.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+let name = "Xavier Puentes";
+function helloWorld() {
+  console.log("Hello World, my name is " + name);
+}
+helloWorld();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return function() {
+    counter++;
+    console.log(counter);
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
+newCounter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
