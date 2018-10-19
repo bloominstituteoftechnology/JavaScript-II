3ec2f9943398c39fb136e63639122b85579a1e3c
Jonathan Dorety <kyullog@gmail.com>
2018-10-18 23:40:32 -0400

    Complete contains

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -58,7 +58,15 @@ multiplyNums(6, 7, function(product) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) return cb(true);
+  else return cb(false);
 }
+contains("yo-yo", items, function(doesIt) {
+  console.log(doesIt);
+})
+contains("contraband", items, function(doesIt) {
+  console.log(doesIt);
+})
 
 /* STRETCH PROBLEM */
 
