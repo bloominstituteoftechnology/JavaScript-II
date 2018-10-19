1481cfb13faa75e4db5592d217e265f061ac9e45
Jonathan Dorety <kyullog@gmail.com>
2018-10-18 23:34:51 -0400

    Complete sumNums

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,7 +40,11 @@ last(items, function(last) {
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(4, 6, function(sum) {
+  console.log(sum);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
