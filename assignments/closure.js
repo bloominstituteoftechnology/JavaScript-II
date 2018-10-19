ee18fe3ed6a8812395becbdae29d2d9cfb492192
Leila Berrouayel <nb.leila10@gmail.com>
2018-10-19 16:24:56 -0500

    complete stretch problem in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,8 +15,11 @@ console.log(totalLength([1,2,3], [4,5,6]));
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  let count = 1;
-  return () => count ++;
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -32,4 +35,30 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  const increment = () => {
+    count++;
+    return count;
+  }
+  const decrement = () => {
+    count--;
+    return count;
+  } 
+  return {
+          increment: increment, 
+          decrement: decrement
+        };
 };
+
+const operationObj = counterFactory();
+console.log("*****");
+console.log(operationObj.increment());
+console.log(operationObj.increment());
+console.log(operationObj.increment());
+console.log(operationObj.increment());
+console.log(operationObj.decrement());
+console.log(operationObj.decrement());
+console.log(operationObj.increment());
+
+
+
