1036363da26b80aa3b5d1f51deff6e0a3e984e98
ThomasUtsey <tomutsey@yahoo.com>
2018-10-18 21:35:58 -0400

    complete array portion

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -6,9 +6,9 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // Potential Solution:
   function firstItem(arr, cb) {
@@ -20,27 +20,45 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 
-
+  // const arr = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-
+//testing the getLength cb
+getLength(items, function(len){
+  console.log(len)
+});
+// end of test
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length-1]);
 }
-
+// testing the cb named last
+last(items, function(lst){
+  console.log(lst);
+});
+// end of test
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
+// testing the sumNums cb
+sumNums(2 ,4 , function(sum){
+  console.log(sum);
+})
+// end of test
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x * y);
 }
+// testing the MultiplyNums cb
+multiplyNums(2 , 4 , function(mpl){
+  console.log(mpl);
+});
+//  end of test
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -51,6 +69,11 @@ function contains(item, list, cb) {
     }
   }
 }
+// testing contains cb
+contains('yo-yo',items,function(con){
+  console.log(con);
+})
+//end of test
 
 /* STRETCH PROBLEM */
 
