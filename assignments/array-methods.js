81ab29d90f457b761be0273a44d853715fbdd22e
Trip1701 <bj2336@email.vccs.edu>
2018-10-19 07:45:41 -0400

    Completed Array-Methods Tasks

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -82,7 +82,7 @@ console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function (acc, obj) { return acc + obj.donation; }, 0);
 
 console.log(ticketPriceTotal);
 
