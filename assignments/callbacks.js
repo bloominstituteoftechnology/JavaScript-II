5124a9bfe94b2489e11671f15009d9d8f1140175
Jonathan Dorety <kyullog@gmail.com>
2018-10-18 23:36:31 -0400

    Complete multiplyNums

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -48,7 +48,12 @@ sumNums(4, 6, function(sum) {
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(6, 7, function(product) {
+  console.log(product)
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
