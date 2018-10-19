98a5d7e60ce138a1f688373464d527dd098c470e
Jason Cook <the.root.user@gmail.com>
2018-10-19 17:02:59 -0400

    Complete array methods challenge

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,62 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(x => fullName.push(`${x.first_name} ${x.last_name}`))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(x => x.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(x => x.shirt_size === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let mappedDonations = runners.map(x => x.donation)
+let ticketPriceTotal = mappedDonations.reduce((acc, cur) => acc + cur);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+const dotComCheck = x => {
+    let split = x.email.split('.')
+    if (split[1] === 'com') {
+        return `Runner ${x.id} has ${x.email} which ends with .com`
+    } else {
+        return `Runner ${x.id} has ${x.email} which ends with something else`
+    }
+}
+let dotComChecked = runners.map(dotComCheck);
+console.log(dotComChecked);
 // Problem 2
 
+const sortContributors = (x, i, thisArr) => {
+    let sorted = [...thisArr].sort((a, b) => b.donation - a.donation);
+
+    return `${sorted[i].company_name} donated ${sorted[i].donation}`
+}
+let sortedContributors = runners.map(sortContributors);
+console.log(sortedContributors);
+
 // Problem 3
+let unshuffledRunners = [...runners]
+let shuffled = []
+//Durstenfeld shuffle
+function shuffle(arr) {
+    for (let i = arr.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        shuffled.push(arr[i])
+    }
+}
+shuffle(runners);
+const simulatedWinners = shuffled.filter((x, i) => i < 3)
+console.log(simulatedWinners)
