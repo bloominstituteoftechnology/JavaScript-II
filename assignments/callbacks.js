e9dc4e1e59be0d16300149d1bc0ce68cb69dae70
Alando Appling <alandoappling@gmail.com>
2018-10-20 09:02:02 -0700

    Started working on stretch goals

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -24,24 +24,46 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+getLength(items,function(x){
+console.log(x)
+});
+
+
 function last(arr, cb) {
+  return cb(arr[arr.length-1])
   // last passes the last item of the array into the callback.
 }
+last(items,function(x){
+  console.log(x);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y)
 }
-
+sumNums(10,10,function(x){
+  console.log(x)
+})
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
+multiplyNums(10,10, function(x){
+  console.log(x)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  items.forEach(element => {
+    cb(element === item);
+  });
 }
+contains('Gum',items, check =>
+  console.log(check));
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,20 +1,45 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function closureTest(){
+  let test = "Does it work???"
+  itWorks();
+  function itWorks(){
+    console.log(test)
+  };
+};
 
+closureTest();
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const newCounter = counter => {
   // Return a function that when invoked increments and returns a counter variable.
+  increment();
+  function increment(){
+    count+=1;
+    console.log(count)
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
-
+let count = 0;
+newCounter();// 2
+newCounter();
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+let newCount = 0;
+const counterFactory = {
+  increment: (amount) =>  {
+    newCount = newCount+amount;
+    console.log(newCount);
+  },
+  decrement: (amount) => {
+    newCount = newCount - amount;
+    console.log(newCount);
+  }
+};
+
+counterFactory.increment(5);
+counterFactory.increment(5);
+counterFactory.decrement(5);
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
