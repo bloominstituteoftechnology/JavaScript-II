7f3099df950cb594ed6abef9a5efb1f1efd7afa8
karlie0 <kamabrapa88@gmail.com>
2018-10-20 04:42:18 -0400

    Finished callbacks.js assignment + stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,99 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+function giveLength(length){
+  console.log('Array length: ' + length);
+}
+
+getLength(items, giveLength);
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+function getLast(lastItem){
+  console.log('Last item of the array: ' + lastItem);
+}
+
+last(items, getLast);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+sumNums(2, 3, function(num1, num2){
+  //Number() used here to avoid string concatenation when using +
+  console.log('Sum of two numbers: '+ Number(num1 + num2));
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+multiplyNums(2, 3, function (num1, num2){
+  console.log('Multiplication of two numbers: '+ num1 * num2);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
 
+function containItem(unit, collection){
+  let found = 0;
+  for(let i = 0; i< collection.length; i++){
+    if(unit === collection[i])
+    { found++ }
+  }
+
+  if (found > 0)
+  { console.log(true);}
+  else 
+  { console.log(false);}
+}
+
+contains("Gum", items, containItem);
+
 /* STRETCH PROBLEM */
+
+//  my example
+const arrayContainsDuplicate = ['A', 'B', 'A', 'C', 'B'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(array);
 }
+
+function findDuplicate(arrayOfItems){
+  //start at zero
+  //if same position, skip and go to next array position
+  // compare: if same, remove element with splice(position, 1)
+  //if not, go to next position
+  let arrayCopy = arrayOfItems;
+  for(let i = 0; i < arrayOfItems.length; i ++){  //outer loop
+    for(let j = 0; j < arrayOfItems.length; j++){ //inner loop
+      if(i === j){  
+        /*skip same position*/}
+      else{
+        if (arrayOfItems[i] === arrayOfItems[j]){
+          arrayCopy.splice(arrayOfItems[i], 1);
+        }
+      }
+    }//inner loop
+  }//outer loop
+  console.log(arrayCopy);
+}
+
+removeDuplicates(arrayContainsDuplicate, findDuplicate);
+
+
