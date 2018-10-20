741a4f59b51fe19ab9c58e90dfcf20b6f65abc5b
Tevvin <Tevvin7395@icloud.com>
2018-10-20 15:05:42 -0400

    What I have so far

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function bestVideoGame() {
+  let game = "RainBow Six Siege";
+  console.log(game);
+  favoriteGame();
 
+  function favoriteGame() {
+    let game = "COD MW3";
+    console.log(game);
+  }
+  allTimeFavoriteGame();
+
+  function allTimeFavoriteGame() {
+    let game = "Forza";
+    console.log(game);
+  }
+}
+bestVideoGame();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
