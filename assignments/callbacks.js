4808b4285330f683cc837c5ee838273885f38719
mustafa inaya <moose.inaya@icloud.com>
2018-10-19 23:24:26 -0700

    array

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -79,6 +79,10 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+function contains(item, list, cb) {
+  return cb (item,list)
+}
+
 
 /* STRETCH PROBLEM */
 
