cbc8a71a3e815f34f12f9dde81ce1b3e42e50ab3
Angie Lawrence <lawrenceangie45@gmail.com>
2018-10-20 12:20:29 -0400

    Completed Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,13 +1,26 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function add (x) {
+  let addMe = x;
+   return function (y) {
+    return x + y;
+  }
+}
+ let addThree = add(3);
+let addSix = add(6);
+let addTen = add(10);
+add(3);
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let counter = 0;
+  return function () {
+    return ++counter;
+  }
+};
+const newCounter = counter();
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
