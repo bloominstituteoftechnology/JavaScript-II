2918c9fc6a32ee9eac91b58c6583cb01160f9f69
Jason Cook <the.root.user@gmail.com>
2018-10-19 21:11:22 -0400

    Complete closures challenge (I think)

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const closure = () => {
+  let candidClosure = 'begin';
+  
+  const closeItOut = () => {
+    candidClosure = 'end';
+    return candidClosure;
+  }
+  
+  return closeItOut();
+}
+console.log(closure());
+const newCandid = closure();
+console.log(newCandid)
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+  let count = 0;
+
+  return () => {
+    return count++
+  }
+} ;
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+const newCounterTest = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounterTest());
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
