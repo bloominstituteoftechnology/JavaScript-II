4e8d31846505734154249842b5f48f32ce91bf49
brettmadrid <brettmadrid9@gmail.com>
2018-10-20 00:55:25 -0700

    completed stretch goals arrow function exercicses

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -2,22 +2,39 @@
 
 // let myFunction = function () {};
 
+let myFunction9 = () => {};
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
+let anotherFunction = (param) => {return param};
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-// add(1,2);
+
+let adder = (param1, param2) => { 
+    return param1 + param2;
+};
+
+adder(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-// subtract(1,2);
 
-// exampleArray = [1,2,3,4];
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+
+subtract(1,2);
+
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-// console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
