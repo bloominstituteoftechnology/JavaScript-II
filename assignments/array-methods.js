3b1c4fbc12267195a16da9b03f42f5be8de8e0bf
Jonathan Dorety <kyullog@gmail.com>
2018-10-20 12:55:24 -0400

    Complete challenge 4

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -80,6 +80,11 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.map(function (item) {
+  return item.donation;
+}).reduce(function (sum, item) {
+  return sum += item;
+});
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
