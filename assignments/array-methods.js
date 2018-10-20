1054f74f0c2d51e98c9a23698e38618a261b6d50
Andrew <andrewryanogle@gmail.com>
2018-10-20 16:51:34 -0700

    I think I somehow messed up my earlier commits because I am not seeing them on git hub finished array methods and closures

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -79,13 +79,18 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+runners.forEach(element=> ticketPriceTotal.push(element.donation))
+ticketPriceTotal= ticketPriceTotal.reduce((x,y)=>x + y);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
-
+// Problem 1 output emails so thank you messages can be sent
+let thanks = [];
+runners.forEach(element => thanks.push(element.email));
+console.log(thanks);
 // Problem 2
+
 
 // Problem 3
