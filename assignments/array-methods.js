186fe48f73b583e2f2406760dea0920edaffb769
Daniel Lim <danheelim@gmail.com>
2018-10-20 11:40:39 -0700

    added the callbacks

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,79 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(function(list) {
+  let a = list.first_name + " " + list.last_name;
+  fullName.push(a);
+})
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+
+const upperCase = runners.map(function(x) {
+  return x.first_name.toUpperCase();
+})
+console.log(upperCase);
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-console.log(largeShirts);
+
+let large = runners.filter(function(runners){
+  if(runners.shirt_size === "L") {
+    largeShirts.push(runners)
+  }
+})
+console.log(largeShirts)
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+ticketPriceTotal = []
+runners.forEach(function(object){
+  let a = object.donation;
+  ticketPriceTotal.push(a);
+})
+let total = ticketPriceTotal.reduce(function(accumulator, currentValue, currentIndex, array) {
+  return accumulator + currentValue;
+})
+
+console.log(total)
+// console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+ticketPriceTotal = []
+runners.forEach(function(object){
+  let a = object.donation;
+  if(a < 100){ 
+    ticketPriceTotal.push(a);
+  }
+})
+let total = ticketPriceTotal.reduce(function(accumulator, currentValue, currentIndex, array) {
+  return accumulator + currentValue;
+})
+console.log("The amount of people that donated less that $100 is " + total)
 
 // Problem 2
 
+Alphabet = [];
+runners.forEach(function(object){
+  let a = object.first_name;
+  Alphabet.push(a);
+})
+console.log(Alphabet.sort());
+
+
 // Problem 3
+
+
+topDonator = [];
+runners.forEach(function(object){
+  let a = object.donation;
+  topDonator.push(a);
+})
+console.log(topDonator.sort());
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,22 +23,51 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+  return cb(arr.length)
   // getLength passes the length of the array into the callback.
 }
+getLength(items, function(length){
+  console.log(items.length);
+});
+
+
 
 function last(arr, cb) {
+  return(cb(arr[arr.length-1]))
   // last passes the last item of the array into the callback.
 }
 
+last(items, function(getLast){
+  console.log(items[items.length-1]);
+});
+
+
+
 function sumNums(x, y, cb) {
+    return x+y;
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+}
+sumNums(2,3,function(result) {
+  console.log(result)
 }
 
 function multiplyNums(x, y, cb) {
+  return x * y;
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+multiplyNums(4,3,function(result) {
+  console.log(result);
+}) 
 
-function contains(item, list, cb) {
+
+let list1 = ["asdf","qawerq","qawefasdf","awergb"];
+
+function contains(item, list, cb) { 
+  list.forEach(function(object){
+    if(object === item){
+      cb(true);
+    }
+  })
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
