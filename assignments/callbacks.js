2437e97e8a45fc94a94d72a4e83362451f0f2105
Andrew <andrewryanogle@gmail.com>
2018-10-20 10:04:40 -0700

    finished with callback functions. used .length and foreach

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -33,20 +33,45 @@ getLength(items, function(length){
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length -1]);
 }
+last(items, function(lastItem){
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(2,4,function(summed){
+  console.log(summed);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+multiplyNums(4,5, function(product){
+  console.log(product);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.forEach(element => {
+
+    if (item === element){
+      return cb(true);
+    }
+    else {
+    return cb(false);
+    
+    }
+  });
 }
+contains('Gum',items,function(check){
+  console.log(check);
+});
 
 /* STRETCH PROBLEM */
 
