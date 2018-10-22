03d1135df7cf3c1a9dfd7bc4226a859cd3b81d05
Bao Pham <ryanvuepham@gmail.com>
2018-10-22 00:37:50 -0500

    finished project. will check out stretch problems later if have time

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,41 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function greeting() {
 
+  let name = 'Bao';
 
+  function device() {
+    let laptop = "Windows";
+    console.log("Hello I\'m " + name);
+
+    function school() {
+
+    console.log("I am using a " +laptop + " laptop" + " for Lambda School");
+    }
+    school();
+  }
+  device();
+}
+
+greeting();
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+//const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+function addOneCounter() {
+  let counter = 0;
+   function resultIncrement() {
+    counter++;
+    return counter;
+    }
+    return resultIncrement;
+}
+const newCounter = addOneCounter();
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
