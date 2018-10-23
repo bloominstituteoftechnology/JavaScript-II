5d48df427b400e00b4fc4b55ccace21b59befafa
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-10-23 18:59:55 -0400

    First Commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,11 +24,21 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+getLength(items, function(length) {
+    console.log(length)
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(length-1)
 }
+
+last(items, function(lastElem) {
+  console.log(lastElem)
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
