8f5324a0bc2cc8bb4df8bc6f3d6365ffe4d88e0b
Joseph Eastman <josephryan96@gmail.com>
2018-10-23 12:23:21 -0700

    closure.js MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,39 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function sayName(nameParam) {
+  const name = nameParam;
+  const closure = 'closure';
+  console.log(`Hello, my name is ${name} and this is an example of ${closure}!`);
 
+  function test1() {
+    const item1 = "What's up";
+    console.log(`${item1}, this is example one of ${closure}!`)
+
+    function test2() {
+      const item2 = "we're nesting functions and creating";
+      console.log(`${item1}, ${item2} ${closure}!`)
+    }
+    test2();
+  }
+  test1();
+}
+sayName('Joseph');
 
 // ==== Challenge 2: Create a counter function ====
-
-  // Return a function that when invoked increments and returns a counter variable.
+// Return a function that when invoked increments and returns a counter variable.
 
 // Example usage: const newCounter = counter();
  //newCounter(); // 1
  //newCounter(); // 2
+ const counter = () => {
+   let count = 0;
+   return () => (++count);
+ }
+
+ const newCounter = counter();
+ newCounter();
+ newCounter();
+ console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
