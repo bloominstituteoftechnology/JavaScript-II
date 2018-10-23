817bcf539aa76b0ac8bbaf866fe5e794cee35cc3
Xavier Vick-West <xaviervickwest1@gmail.com>
2018-10-23 13:03:38 -0400

    update

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,15 +1,24 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+/*
+Create a callback function and invoke the function to test your work.
+You have been provided an example of a problem and a solution to see
+how this works with our items array.  Study both the problem and the
+solution to figure out the rest of the problems.
+*/
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
+
+
+
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
+    cb(arr[0])
   }
 
+/*
   // Potential Solution:
   function firstItem(arr, cb) {
     return cb(arr[0]);
@@ -24,23 +33,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length)
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[3])
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = (x + y);
+  cb(result)
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = (x * y)
+  cb(result)
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item)
+    {
+      let result = true;
+    }
+    else
+    {
+      let result = false;
+    }
+  cb(result);
 }
 
 /* STRETCH PROBLEM */
@@ -49,4 +74,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
+}
 }
