305f2bccdabea10bd4aadd0bce5fe300eb3c61e5
jackgipson <jackjgipson@gmail.com>
2018-10-23 15:48:59 -0700

    Updated

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function spock(greeting) {
+  const hello = (greeting);
+  console.log(hello);
 
+  function neelix() {
+    const sayHello = 'Howdy';
+    console.log(`${hello} but I say ${sayHello}`);
+
+    function phlox() {
+      const sayHi = 'How do you do?';
+      console.log(`${hello} but I say ${sayHello} and I say ${sayHi}`)
+    }
+    phlox();
+  }
+  neelix();
+}
+spock('Live Long And Prosper');
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => (++count);
 };
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+ console.log(newCounter()); // 1
+ console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
