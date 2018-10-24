46f07a46de6a7e362d7e640b34257eb785cf58f8
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-10-23 20:40:14 -0400

    Completed second Q on Call backs Section

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -33,7 +33,9 @@ getLength(items, function(length) {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(length-1)
+ let lastItem = (arr.length-1);
+ let lastItemIndex = (arr[lastItem]);
+ cb(lastItemIndex);
 }
 
 last(items, function(lastElem) {
