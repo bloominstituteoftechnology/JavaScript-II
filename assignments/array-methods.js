a64806ffc2bb95f9ea5dc275453d841f9aaf741c
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-10-26 18:25:14 -0500

    array-methods.js Removed unnecessary code from Challenge 2

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -61,8 +61,7 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-allCaps = runners.map((element, index) => `${runners[index].first_name.toUpperCase()} ${runners[index].last_name.toUpperCase()}`);
+let allCaps = runners.map((element) => `${element.first_name.toUpperCase()} ${element.last_name.toUpperCase()}`);
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
