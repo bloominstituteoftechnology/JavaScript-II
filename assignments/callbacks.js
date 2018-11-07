05da301673230e5ce4031e6658ab4d7954c60665
edreeseg <edward.reeseg@gmail.com>
2018-11-07 08:37:20 -0500

    Added invocations

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,24 +26,36 @@ function getLength(arr, cb) {
   return cb(arr.length);
 }
 
+getLength(items, (length) => console.log(length)); // Should be 4
+
 function last(arr, cb) {
   return cb(arr[arr.length - 1]);
 }
+
+last(items, (lastItem) => console.log(lastItem)); // Should be 'Gum'
 
 function sumNums(x, y, cb) {
   return cb(x + y);
 }
 
+sumNums(1, 9, (z) => console.log(z)); // Should be 10
+
 function multiplyNums(x, y, cb) {
   return cb(x * y);
 }
 
+multiplyNums(2, 5, (z) => console.log(z)); // Should be 10
+
 function contains(item, list, cb) {
   return list.includes(item) ? cb(true) : cb(false);
 }
+
+contains('Gum', items, (z) => console.log(z ? 'Item is included!' : 'Item is not included')); // Should be 'Item is included!'
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
   return cb([...new Set(array)]);
 }
+
+removeDuplicates([1, 1, 1, 1, 2, 3, 4, 4, 5, 6, 5, 1], (arr) => console.log(arr)); // Should be [1, 2, 3, 4, 5, 6]
