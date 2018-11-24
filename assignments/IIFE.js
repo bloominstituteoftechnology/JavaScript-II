5533024c74916130704a99e6a31ca0381b84c1b6
linkel <ironcarbide@gmail.com>
2018-11-24 14:01:57 -0800

    immediately invoked function expressons and the ES5 to ES6 stretch goal

---
+++ assignments/IIFE.js
@@ -0,0 +1,6 @@
+console.log((function() {
+    var name = "Nutso";
+    return name;
+})());
+
+// Immediately invoked function expression uses () after the definition to indicate that it is run as soon as it is defined. 
