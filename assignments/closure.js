15e8666873125beaf5af9f44f86c01b4d2a4db0a
cecil john tantay <cjbtantay@gmail.com>
2018-11-27 14:01:13 -0800

    finished closure mvp

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,7 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-const simpleClosure = ()=> {
+function simpleClosure() {
   const amountOfChildren = ["Pedro", "Alan", "Samantha"]
   function childClosure() {
     let nameOfChild = "Pedro"
@@ -15,12 +15,16 @@ const simpleClosure = ()=> {
 };
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+function counter() {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter= 0;
+  return function() {
+    return counter++;
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
