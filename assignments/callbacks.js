1126dab591c15608d2549ffe39198426c2707860
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-27 12:50:05 -0800

    completed second part of callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -32,9 +32,17 @@ getLength(items, function(length){
 console.log(length)
 });
 
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+function last(arr, cb){
+  return cb(arr[arr.length - 1]);
+}
+last(items, function(last){
+  console.log(last);
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
