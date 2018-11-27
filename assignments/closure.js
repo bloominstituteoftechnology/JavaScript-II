795c7cc64c4891e19ed3a7b258a8e145ac56268d
Daniel Weinman <namniewd@gmail.com>
2018-11-27 16:44:26 -0500

    completed all challenges in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function tellAge(age) {
+  const currentAge = age;
+  console.log(`I am ${age} years old`);
+  
+  function older() {
+    const newAge = 50;
+    console.log(`In ${newAge - age} years I will be ${newAge} years old`);
+  }
+  older();
+}
+
+tellAge(30);
 
 
 // ==== Challenge 2: Create a counter function ====
+let count = 0;
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  count++;
+  console.log(count);
 };
+counter();
+counter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,3 +35,8 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+function newFunction() {
+  const count = 0;
+  console.log(count + 1);
+}
+
