fabe279ab775fa8c0b104c5f4fe4c365f67f7b16
bhumip214 <bhumip214@gmail.com>
2018-11-27 18:10:50 -0500

    Completed Closure assignment and started on callbacks stretch questions

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function sayGreet(greeting) {
+  const greet = greeting;
+
+  function sayname(name) {
+    const saying = "My name is";
+    console.log(`${greet}, ${saying} ${name}`);
+  }
+  sayname("Bhumi");
+}
+sayGreet("Hello");
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let i = 0;
+
+  return function() {
+    i += 1;
+    return i;
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
