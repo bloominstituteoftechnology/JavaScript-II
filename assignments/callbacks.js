35cbb7e1e9b3888cd7f8265e7803860b475ba63c
Marc Gutnayer <mgutnayer@gmail.com>
2018-11-27 15:25:58 -0600

    Javascript-II, completed Callbacks.js file, now working on Array-Methods.js file

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,59 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return(cb(arr.length));
 }
+
+getLength(items, function(length) {
+  console.log(length);
+})
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return(cb(arr[arr.length - 1]));
 }
+
+last(items, function(lastitem) {
+  console.log(lastitem);
+})
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return(cb(x + y));
 }
+
+sumNums(7, 8, function(sum) {
+  console.log(sum);
+})
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return(cb(x * y));
 }
+
+multiplyNums(5, 6, function(result) {
+  console.log(result);
+})
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i].toLowerCase() === item.toLowerCase()) {
+      return(cb(true));
+    }
+  }
+  
+  return(cb(false));
 }
+
+contains("NOTEBOOK", items, function(result) {
+  console.log(result);
+})
 
 /* STRETCH PROBLEM */
 
