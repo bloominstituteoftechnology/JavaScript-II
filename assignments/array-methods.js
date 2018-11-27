bdb9ea227731b5f74b51130c4572b975fe8d917a
McNultyPT <mcnultypt@gmail.com>
2018-11-27 15:23:58 -0800

    array-methods complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,22 +55,31 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
-console.log(fullName);
+let fullName = runners.forEach(function(name) {
+    console.log(name.first_name + " " + name.last_name);
+});
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+let allCaps = runners.map(function(upper) {
+    console.log(upper.first_name.toUpperCase());
+});
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(function(large) {
+        return large.shirt_size === "L";
+    });
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners.reduce(function(totalDon, currentValue) {
+    return totalDon + currentValue.donation;
+}, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +87,22 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+let nameAndEmail = runners.forEach(function(contact) {
+    console.log(contact.first_name + " " + contact.last_name + " " + contact.email);
+});
+
 // Problem 2
 
+let companyCaps = runners.map(function(company) {
+    return company.company_name.toUpperCase();
+});
+
+console.log(companyCaps);
+
 // Problem 3
+
+let smallShirts = runners.filter(function(small) {
+    return small.shirt_size === "S";
+});
+
+console.log(smallShirts);
