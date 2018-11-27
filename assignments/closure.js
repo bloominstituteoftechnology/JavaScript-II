b9ecc982e5c6d18b9ca8dece40ff194cd9872871
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-11-27 16:30:00 -0600

    completed the closure.js challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,39 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function Healthpoints(hp) {
+  const hpValue = hp;
+  console.log(`You have ${hp} hp.`);
+  //debugger;
+  
+  function takeDamage() {
+    const tookdmg = hp-1;
+    console.log(`${tookdmg} you know have ${hp} left`);
+    //debugger;
+    
+    function faint() {
+      const fainted = "You have fainted"
+      console.log(`${fainted} because your hp was ${hp} but is now ${tookdmg}`);
+      //debugger;
+    }
+    
+    faint();
+  }// faint
+  
+  takeDamage();
+}// takeDamage
+Healthpoints(1);
 
 // ==== Challenge 2: Create a counter function ====
+let num = 0;
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  num++
+  return num;
 };
+
+console.log(counter());
+console.log(counter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
