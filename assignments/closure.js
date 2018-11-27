37bcdce26a0ebd6d9fa0aad30ae8c969e27d244e
kjohna <43828763+kjohna@users.noreply.github.com>
2018-11-27 16:29:39 -0700

    fixed closure.js: line 20 and lines 39,40 to increment/decrement before returning

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,9 +13,11 @@ itemStorage();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-  let count = 1;
+  let count = 0;
+  debugger;
   return () => {
-    return count ++;
+    debugger;
+    return ++ count;
   }
 };
 //counter();
@@ -34,8 +36,8 @@ const counterFactory = () => {
   // `decrement` should decrement the counter variable and return it.
   let count = 1;
   return {
-    "increment" : () => count ++,
-    "decrement" : () => count --
+    "increment" : () => ++ count,
+    "decrement" : () => -- count
   };
 };
 
