6b3e22bab92d750c9737c491e572e033af2628c7
McNultyPT <mcnultypt@gmail.com>
2018-11-27 12:19:07 -0800

    callbacks first problem complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,8 +23,12 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
