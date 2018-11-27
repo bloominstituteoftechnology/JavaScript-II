306d3835558428957db9c0ffb29bdead9d423fb6
umbur <work1415@gmail.com>
2018-11-27 15:11:37 -0700

    completed the callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,5 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works 
+// with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -24,25 +25,60 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+  console.log(length);
+})
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop());
 }
+
+last(items, function(pop){
+  console.log(pop);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
+const add = function(a, b){
+  console.log(a + b);
+}
+sumNums(2, 3, add);
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+const multiply = function(x, y){
+  console.log(x * y);
+}
+multiplyNums(5, 5, multiply);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let ispresent = 0;
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item) {
+      ispresent++;
+    }
+  }
+  if(ispresent > 0) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
-
+contains('Pencil', items, function(check){
+  console.log(check);
+});
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
