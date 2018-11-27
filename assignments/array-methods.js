319f669b1c908f4df4147100df7d7174f02b1f40
rmejia-tajc <rmejia.tajc@gmail.com>
2018-11-27 14:59:23 -0700

    complete array-methods.js allCaps problem

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -62,12 +62,19 @@ let fullName = [];
 runners.forEach(function(currentValue){
     fullName.push(`${currentValue.first_name} ${currentValue.last_name}`);
   });
-  
+
 console.log(fullName);
+
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+
+runners.map(function(currentValue){
+    return allCaps.push(currentValue.first_name.toUpperCase())
+  });
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
