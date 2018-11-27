00658b33fb51eed7dcbda9991653f7cbd02ca016
KROHNICK <business.kanekperez@gmail.com>
2018-11-27 13:44:36 -0800

    Completed callback.js except last challenge

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function (length) {
+  console.log(length)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3])
 }
+
+last(items, function (last) {
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+
+sumNums(3, 5, function (sum) {
+  console.log(sum)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 5, function (multiply) {
+  console.log(multiply)
+});
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item.string['']);
 }
+
+contains('Pencil', items, function (contain) {
+  console.log(contain)
+});
 
 /* STRETCH PROBLEM */
 
