2ae06e158e5c08c3c25b7dd565bacb263b7eb083
Matt Basile <matthewbasile2@gmail.com>
2018-11-27 15:18:55 -0500

    Completed Challenge 3, .filter(). LINES: array-methods.js 73-75.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,12 +66,13 @@ console.log(fullName);
 let allCaps = runners.map(function(runner){
     return `${runner.first_name.toUpperCase()} ${runner.last_name}`
 })
-
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(function(runner){
+    return runner.shirt_size == "L";
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
