5760d22a42e7d5079e46eb8336eb08562c535473
stellarluminant <stellarluminant@gmail.com>
2018-11-27 13:24:06 -0800

    Update closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function concatenator(string, count) {
+  let concatenate = function(string) {
+    return result + string;
+  }
+
+  let result = ``;
+
+  for (let i = 0; i < count; i++) {
+    result = concatenate(string);
+  }
+  return result;
+}
+
+console.log(concatenator(`cake `, 5, (string) => result + string));
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => count++;
   // Return a function that when invoked increments and returns a counter variable.
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
