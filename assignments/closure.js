d2bc874fa3cc47b7a6ac6ebb6f50d2f9a197277f
Matt Basile <matthewbasile2@gmail.com>
2018-11-27 18:25:39 -0500

    Completed Stretch for Closures. Did a lot of deeper research into object functions. Need to continue to do more, a subject I know little about but realize the potential with introducing methods. LiNES: closures.js 26-49.

--- assignments/closure.js
+++ assignments/closure.js
@@ -11,7 +11,7 @@ const counter = () => {
   let count = 0;
   return function(){
     count ++;
-    return count;
+    return count
   }
  }
 const newCounter = counter();
@@ -19,11 +19,35 @@ console.log(newCounter());
 console.log(newCounter()); 
 
 
+
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+//Researched on the interwebs and modified this solution. Near to continue diivng deeper on the power of object functions.
+
+const counterFactory = (function() {
+  var count = 0;
+  return {
+  increment: function() {
+    return count+= 1;
+  },
+  decrement: function() {
+    return count-= 1;
+  },
+  value: function() {
+    return count;
+  }
+  };   
+})();
+console.log(counterFactory.value()); // 0
+counterFactory.increment();
+counterFactory.increment();
+console.log(counterFactory.value()); // 2
+counterFactory.decrement();
+console.log(counterFactory.value()); // 1
+ 
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+
