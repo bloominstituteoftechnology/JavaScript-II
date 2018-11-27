8523d90f8b66c8e302ba3a811e700cf7a7a2e66e
McNultyPT <mcnultypt@gmail.com>
2018-11-27 15:42:15 -0800

    project complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,36 @@
 // ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
+
+function christmasCheer(name) {
+  const nameHolder = name;
+  console.log(`Merry Christmas, ${name}`);
+
+  function moreChristmas() {
+    const hollyJolly = "Have a holly, jolly,";
+    console.log(`${hollyJolly} Merry Christmas, ${nameHolder}!`);
+  }
+  moreChristmas();
+}
+
+christmasCheer("James");
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return ++count;
+  }
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
