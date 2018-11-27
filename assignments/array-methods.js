7a8fb0827d9998fb4db1acdf7ff2e473c4117d82
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-27 14:22:37 -0800

    completed up to challenge 4 in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -67,7 +67,7 @@ runners.forEach(function(value){
     fullName.push(`${value.first_name} ${value.last_name}`)
     //console.log(value.first_name)
 });
-//console.log(fullName);
+console.log(fullName);
 
 
 
@@ -82,12 +82,18 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = []; 
+largeShirts = runners.filter(function(value){
+return value.shirt_size === "L"
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce(function(ticketPriceTotal,item){
+return ticketPriceTotal += item.donation;
+},0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
