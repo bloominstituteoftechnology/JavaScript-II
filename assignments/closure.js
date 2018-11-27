b2952f6b6eca01527a2b082b4a2b838b8496187d
ltrii <lelandtr@gmail.com>
2018-11-27 15:40:49 -0800

    Work on many files...

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,47 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+  var totalxp = 0;
+  var initlvlxp = 75;
+  var lvl = 1;
+  var xproof = 0;
+
+  function battleTime(userlvl,enemylvl,buff) {
+    if ((userlvl + buff) > enemylvl) {
+      totalxp += 100;
+      return "You win!";
+    } else {
+      totalxp += 33;
+      return "You lose but gained some knowledge.";
+    }
+  }
+
+  function levelUp(xptotal, xpnext){
+    xptotal = totalxp;
+    xpnext = initlvlxp;
+    if (xptotal >= xpnext) {
+      lvl += 1;
+      xpnext = (xpnext * .33);
+      xproof = xpnext;
+    }
+  }
+
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (() => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+  let cnt = 0;
+  return () => {
+      cnt += 1;
+      return cnt;
+  }
+})();
+
+// ???
+//function counter() {
+//  counter += 1;
+//}
+// ???
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
