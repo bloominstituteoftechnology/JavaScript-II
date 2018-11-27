964e344a5c044b6517d4aa0087d21862bd0db3ce
Nate Boyette <nateboyette@icloud.com>
2018-11-27 13:22:34 -0800

    Completed the callback main challenges.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -76,7 +76,18 @@ multiplyNums(4, 5, function(product) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    cb(true);
+  } else {
+    cb(false);
+  }
+
 }
+
+contains("pencil", items, function(doesContainItem) {
+  console.log(doesContainItem);
+})
+
 
 
 /* ======================== */
