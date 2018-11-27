21a9cfcaafe36e9de5bd85f6b0458d1f60e5ab56
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-11-27 15:44:51 -0500

    Completed closure assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const firstLetter = 'A';
+  const alphabet = function() {
+    const secondLetter = 'B';
+    return (`${firstLetter} ${secondLetter}`);
+  };
+console.log(alphabet());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function () {
+    return ++count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
