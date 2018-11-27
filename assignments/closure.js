a65a276e5f13ecfbd30e980ab2d3472bd5b7b522
Moonkaman <nickbatmanglidj@gmail.com>
2018-11-27 15:13:17 -0700

    Complete all challenges in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,10 +3,10 @@
 function saySomething(theThing){
   const theSaying = theThing;
   console.log(theThing)
-  debugger;
+  //debugger;
   function saySomethingElse(){
     console.log(`There's a closure in my boot! and I think it's this: ${theSaying}`);
-    debugger;
+    //debugger;
   }
   saySomethingElse();
 }
@@ -17,7 +17,18 @@ saySomething('Hello World!')
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count++;
+    return count;
+  }
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
