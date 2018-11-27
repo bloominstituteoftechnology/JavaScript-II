7a4618e274a0975657b531b99ce8e5ae74e2418f
Matt Basile <matthewbasile2@gmail.com>
2018-11-27 16:23:51 -0500

    added counter function on challenge 2. LINES: 11-20.

--- assignments/closure.js
+++ assignments/closure.js
@@ -8,10 +8,16 @@ console.log(whatsUrName());
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let count = 0;
+  return function(){
+    count ++;
+    return count;
+  }
+ }
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter()); 
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
