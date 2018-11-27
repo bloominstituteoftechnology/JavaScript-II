0940a4fe7c07d0dec80a3ae78ab47d4c927e2994
Daniel Weinman <namniewd@gmail.com>
2018-11-27 16:04:40 -0500

    completed callbacks.js problem set

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(last) {
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+sumNums(3,4, function(x, y) {
+  console.log(x+y);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+multiplyNums(12, 13, function(x, y) {
+  console.log(x*y);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+
+contains('Pencil', items, function(bool) {
+  console.log(bool);
+})
 
 /* STRETCH PROBLEM */
 
