eb80840145829eec5a57c8eb5c8df814da392724
ajfrierson <alvinfrierson@fuller.edu>
2018-11-27 14:44:38 -0800

    made syntactical change to challenge 4 of array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,8 +56,8 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-runners.forEach(function(fullName){
-    return fullName;
+fullName = runners.forEach(function(fullName) {
+    return fullName.first_name;
 });
 
 console.log(fullName);
@@ -65,7 +65,7 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-allCaps = runners.map( function(currentValue) {
+allCaps = runners.map(function(currentValue) {
   return currentValue.first_name.toUpperCase()
 });
 
@@ -85,7 +85,7 @@ console.log(largeShirts);
 let ticketPriceTotal = [];
 ticketPriceTotal = runners.reduce(function(totalPrice, currentValue) {
     return totalPrice + currentValue.donation;
-}, 0));
+}, 0);
   
   console.log(ticketPriceTotal);
 
