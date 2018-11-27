4121efd3d2fbc4705a6031981febc6ff7a9f88bf
cecil john tantay <cjbtantay@gmail.com>
2018-11-27 15:15:02 -0800

    finished callbacks

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,7 +58,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 let fullName = [];
 runners.forEach( (item) => fullName.push(`${item.first_name} ${item.last_name}`) )
 console.log(fullName);
-
+ 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = runners.map( (item)=> item.first_name.toUpperCase() );
