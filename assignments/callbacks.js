f32d9e4144aaa44563f8871979a4725e1a060bdb
David Flack <davidscottflack@gmail.com>
2018-11-27 15:11:30 -0600

    Complete callbacks.js homework

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,7 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const itemsDuplicates = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Notebook', 'Notebook', 'Gum'];
 
 /* 
 
@@ -24,29 +25,37 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
-
+getLength(items, (length) => {console.log(length)});
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
-
+sumNums(items[0], items[1], (sumOfPerams) => {console.log(sumOfPerams)});
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
+multiplyNums(33, 3, (productOfPerams) => {console.log(productOfPerams)});
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+contains('yo-yo', items, (trueOrFalse) => {console.log(trueOrFalse)});
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  
+  return cb(array.filter(function(item, index){
+    return array.indexOf(item) >= index;
+  }));
 }
+
+removeDuplicates(itemsDuplicates, (item) => console.log(item));
