57cd633e56cf4c99400dc33e326da3db2036c945
rosilioroman <rosilioroman@gmail.com>
2018-11-27 16:39:42 -0700

    Completed tasks in closure.js (excluding stretch), added Problem 1 to Challenge 5 in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -100,6 +100,9 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//Make a list of runners from the UK by filtering for .uk email addresses
+let britishRunners = runners.filter(person => person.email[person.email.length - 1] === 'k' && person.email[person.email.length - 2] === 'u');
+console.log(britishRunners);
 
 // Problem 2
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,39 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function firstFunction(){
+  const firstVar = 36;
+  //debugger;
+  function secondFunction() {
+    const secondVar = 42;
+    //debugger;
+    function thirdFunction() {
+      let thirdVar = firstVar + secondVar;
+      //debugger;
+      console.log(`The sum of ${firstVar} + ${secondVar} = ${thirdVar}`)
+    }//thirdFunction
 
+    thirdFunction();
+  }//secondFunction
+
+  secondFunction();
+}//firstFunction
+
+//invoke firstFunction
+firstFunction();
 
 // ==== Challenge 2: Create a counter function ====
+let counterVar = 0;
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  counterVar++;
+  console.log(counterVar);
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+counter();
+counter();
+counter();
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
