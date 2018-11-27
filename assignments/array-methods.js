db6af5eedd3d4da177a8dec25947f813a1d970ff
Alex Bott <Abott1222@gmail.com>
2018-11-27 13:26:05 -0800

    finished array methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -90,8 +90,32 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 Kick out the rich people by excluding donations larger than 200
 
-// Problem 2
+let remainingRunners = runners.filter( function(runnerObj) {
+    return runnerObj.donation < 200;
+})
 
-// Problem 3
+// Problem 2 combine everyones last name
+
+const reducer2 = function(acc, runnerObj) {
+    return acc + runnerObj.last_name;
+}
+
+let hugeLastName = runners.reduce(reducer2, '');
+console.log(hugeLastName);
+
+// Problem 3 Get #s for each shirt size
+
+let shirtSizeCount = {}
+
+runners.forEach( function(runnerObj) {
+    let size = runnerObj.shirt_size;
+    if(!shirtSizeCount[size]) {
+        shirtSizeCount[runnerObj.shirt_size] = 1;
+    } else {
+        shirtSizeCount[runnerObj.shirt_size]++;
+    }
+})
+
+console.log(JSON.stringify(shirtSizeCount));
