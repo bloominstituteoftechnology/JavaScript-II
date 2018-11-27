dc95c713902e3a5b00aa1e03e904ef46d9cc231f
Olivia Osborn <olioz95@gmail.com>
2018-11-27 16:49:20 -0500

    used .map() to change donations in dollars to euros for challenge 5 in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -63,6 +63,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // });
 
 // console.log(fullName);
+// console.log(runners);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
@@ -81,16 +82,21 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-const ticketPriceTotal = runners.reduce(function(totalValue, currentValue){
-    return totalValue + currentValue.donation;
-}, 0);
+// const ticketPriceTotal = runners.reduce(function(totalValue, currentValue){
+//     return totalValue + currentValue.donation;
+// }, 0);
 
-console.log(ticketPriceTotal);
+// console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 -- use .map() to change dollars to euros 
+const euroDonations = runners.map(function(element) {
+    return element.donation * 0.89;
+});
+
+console.log(euroDonations);
 
 // Problem 2
 
