de11ab3ae4af955bfa6bdb359e45f323e1578ce5
yannafaith <yannafaith97@gmail.com>
2018-11-27 15:39:17 -0700

    finished closure challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let parentFunc = () => {
+  const sampleVar = [1,2,3];
+  let childFunc = () => {
+    const childLaugh = 'hahaha';
+    return childLaugh};
+};
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let i = 0;
+  const newCounter = (i) => {
+    i++;
+    return i;
+  }
+  return newCounter;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const counter = () => {
+  // Return a function that when invoked increments and returns a counter variable.
+  let i;
+  const newCounter = () => {
+    if (i == undefined) {i = 0}
+    i = i+1;
+    return i;
+  }
+  return newCounter();
+};
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
