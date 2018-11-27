540733c7086836330066767187e6b15a8d86aa70
Matt Basile <matthewbasile2@gmail.com>
2018-11-27 15:16:28 -0500

    Completed Challenge 2, .map(). LINES: array-methods.js 66-69.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -63,7 +63,10 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(function(runner){
+    return `${runner.first_name.toUpperCase()} ${runner.last_name}`
+})
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
