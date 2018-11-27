f96ec618416f144596985265ba1e25109dc4a3bc
Lenna <lenna@lennamau.com>
2018-11-27 15:02:17 -0700

    JavaScript 2 Lenna
    Callbacks almost completed

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,12 +54,18 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+// The event director needs both the first and last names of each runner for their running bibs.  
+//Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+let fullName = runners.forEach(function(runnerName){
+    console.log(`${runnerName.first_name} ${runnerName.last_name}`);
+});
 console.log(fullName);
 
+
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+// The event director needs to have all the runner's first names converted to uppercase 
+//because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
 console.log(allCaps); 
 
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,10 @@
-// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have
+// been provided an example of a problem and a solution to see how this works with our items array.
+//  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
   //Given this problem: 
   
@@ -24,19 +26,47 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+
+console.log(cb(arr));
 }
+let length = function(items) {
+  return items.length;
+};
+
+getLength(items,length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  console.log(cb(arr));
 }
+let lastItem = function(items) {
+  return items[items.length -1];
+};
+
+last(items,lastItem);
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  console.log(cb(x,y));
+
 }
+let sum = function(x,y) {
+  return x + y;
+};
+
+sumNums(8, 11, sum);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  console.log(cb(x,y));
+
 }
+let multiplies = function(x,y) {
+  return x * y;
+};
+
+multiplyNums(12, 5, multiplies);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
