6f50b411fd37fe41799d0f31e170980cfdbd8d62
Jamie Hall <hall.jamie@gmail.com>
2018-11-27 15:52:15 -0500

    Finished challenge II

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -63,7 +63,14 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+//let allCaps = [];
+
+
+
+let allCaps = runners.map(element =>
+    element.first_name.toUpperCase());
+
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
