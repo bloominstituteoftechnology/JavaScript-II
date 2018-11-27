a91d886960db725c7e7a9f678a96be54159466c5
Nathan Thomas <nwthomas@me.com>
2018-11-27 12:33:06 -0800

    Finish second question in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -464,7 +464,7 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(runner => runner.first_name.toUpperCase());
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
