4d18925a02d08a51ee962488f911aff0d370a7ed
BrannanC <brannanconrad@gmail.com>
2018-11-27 14:24:36 -0600

    Actually finished callbacks...

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,7 +1,9 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+const callback = function(x){
+  console.log(x);
+}
 /* 
 
   //Given this problem: 
@@ -27,10 +29,13 @@ function getLength(arr, cb) {
   return cb(arr.length);
 }
 
+getLength(items, callback);
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length - 1]);
 }
+last(items, callback);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
@@ -38,17 +43,24 @@ function sumNums(x, y, cb) {
   return cb(sum)
 }
 
+sumNums(1,2, callback);
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   const total = x * y;
   return cb(total);
 }
 
+multiplyNums(1,2,callback);
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   return cb(list.includes(item));
 }
+
+contains("Pencil", items, callback);
+contains("Uranium", items, callback);
 
 /* STRETCH PROBLEM */
 
@@ -59,3 +71,5 @@ function removeDuplicates(array, cb) {
   const newArray = Array.from(new Set(array));
   return cb(newArray);
 }
+
+removeDuplicates(['a', 'a', 'b', 'c', 'a'], callback);
