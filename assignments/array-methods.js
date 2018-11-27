4e58b47b16eff2b8635e00cb852f1a5b3dab23df
rmejia-tajc <rmejia.tajc@gmail.com>
2018-11-27 15:44:36 -0700

    complete array-methods.js Be Creative problem 1

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -94,14 +94,24 @@ let ticketPriceTotal = [];
 ticketPriceTotal.push(runners.reduce(function(totalDonation, item){
     return totalDonation + item.donation;
   }, 0));
-  
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// You need to send out a mass email to everyone on the list:
+
+let massMail = [];
+
+runners.map(function(currentValue){
+    return massMail.push(currentValue.email)
+  });
+
+console.log(massMail);
 
 // Problem 2
+
 
 // Problem 3
