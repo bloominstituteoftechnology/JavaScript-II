999ed12253bf20924b33c8a0cbe90d4a919cce08
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-27 16:15:02 -0800

    completed closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -16,13 +16,24 @@ favTeam();
 
 
 // ==== Challenge 2: Create a counter function ====
-count = 0;
-const counter = (count) => {
+let count = 0;
+const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+return function(){
+  count++;
+  return count;
+}
+
 };
 
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
+
+
 // Example usage: const newCounter = counter();
- counter(count); // 1
+ //newCounter(); // 1
 // newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
