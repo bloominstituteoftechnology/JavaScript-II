ff02e0c5ea03863f38be344932c9f5252734f53d
joshua-disney <joshua.a.disney@gmail.com>
2018-11-27 17:15:57 -0700

    Completed closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function talkFood(food) {
+  console.log(`My favorite food is ${food}.  What's yours?`);
+
+  function gross() {
+    const nasty = 'tuna fish';
+    console.log(`Even though I love ${food}, my least favorite food is ${nasty} by itself.`);
+  }
+  gross();
+} 
+
+talkFood('tuna cassarole');
 
 // ==== Challenge 2: Create a counter function ====
+let myCount = 0;
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  myCount += 1;
 };
+
+console.log(myCount);
+counter();
+console.log(myCount);
+counter();
+console.log(myCount);
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
