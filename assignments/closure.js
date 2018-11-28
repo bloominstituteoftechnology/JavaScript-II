3f77626452d7597e3c08f53d28f25e66fec208ca
samjeffcoat <samjeffcoat@gmail.com>
2018-11-27 18:17:24 -0600

    closure done have a few more left to do

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function Boomer(opponent){
+  console.log(`Boomer Sooner! we are going to beat ${opponent}! and go to the National Championship!`);
+}
+Boomer("Texas");
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let wins = 901;
+  return function(){
+    wins = wins+1;
+    return wins;
+  }
 };
+const newCounter = counter();
+//Hooray! We beat Teas and we are on our way to 903!
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -17,4 +28,4 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+}
