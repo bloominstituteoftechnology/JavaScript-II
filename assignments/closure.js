48d64df4b68430c5c6670221cfe751394962fe05
cecil john tantay <cjbtantay@gmail.com>
2018-11-27 22:35:44 -0800

    finished closure stretch... closure is still a mystery to me

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,10 +17,12 @@ function simpleClosure() {
 // ==== Challenge 2: Create a counter function ====
 function counter() {
   // Return a function that when invoked increments and returns a counter variable.
-  let counter= 0;
-  return function() {
+  let counter = 1;
+  function counterrr() {
     return counter++;
   };
+
+  return counterrr;
 };
 const newCounter = counter();
 console.log(newCounter()); // 1
@@ -33,4 +35,22 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 1;
+  return objectThatCatIncrementAndDecrement = {
+    increment: ()=>++counter,
+    decrement: ()=>--counter
+    // let me know if counter++/counter-- should have been the right logic. 
+    // it just doesn't look pretty on a console log.
+  }
 };
+
+let idk = counterFactory();
+
+// initial value = 1
+console.log(counterFactory().decrement()) // 0
+console.log(idk.increment()) // 1
+console.log(idk.increment()) // 2
+console.log(idk.increment()) // 3
+console.log(idk.decrement()) // 2
+
+// i had to look around google to call this method. 
