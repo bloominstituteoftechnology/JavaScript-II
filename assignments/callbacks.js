678b448e12dd7e438f11cf5e21d35f135e72f8d1
joepound <14357797+joepound@users.noreply.github.com>
2018-11-29 14:00:35 -0700

    Added sample uses for the callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,21 +26,33 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   cb(arr[arr.length - 1]);
 }
+last(items, function(last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   cb(x + y);
 }
+sumNums(123, 456, function(sum) {
+  console.log(sum);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   cb(x * y);
 }
+multiplyNums(11, 12, function(product) {
+  console.log(product);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -49,6 +61,12 @@ function contains(item, list, cb) {
     return list_element === item;
   }));
 }
+contains("yo-yo", items, function(isContained) {
+  console.log(`Item is${isContained ? " in " : " not in "}the list.`);
+})
+contains("yo", items, function(isContained) {
+  console.log(`Item is${isContained ? " in " : " not in "}the list.`);
+})
 
 /* STRETCH PROBLEM */
 
@@ -56,7 +74,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  cb(arr.filter(function(item, i, arr) {
+  cb(array.filter(function(item, i, arr) {
     return i === arr.indexOf(item);
   }))
 }
+const testArr = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 7, 7, 0];
+removeDuplicates(testArr, function(cloneFilteredArr) {
+  console.log(cloneFilteredArr);
+});
