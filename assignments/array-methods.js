f904c175b1be630a09d9258343202e4d4c2f955a
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-04 16:14:06 -0600

    changed error in array-methods-js challenge 2

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,7 +66,8 @@ runners.forEach(function(name) {
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+// The event director needs to have all the runner's first names converted to 
+//uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 // let allCaps = [];
 // console.log(allCaps); 
 
@@ -76,7 +77,6 @@ for(let i = 0; i < runners.length; i++){
 }
 
 console.log(allCaps); 
-allCaps();
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
@@ -84,12 +84,9 @@ allCaps();
 // console.log(largeShirts);
 
 let largeShirts = [];
+largeShirts = runners.filter(function(shirts) {
+  return shirts.shirt_size === "L"});
 
-for(let i = 0; i < runners.length; i++) {
-  if(runners[i].shirt_size === "L") {
-    largeShirts.push(runners[i]);
-  }
-}
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
@@ -113,7 +110,10 @@ function ticketPriceTotal(a,b,donation) {
   ticketPriceTotal(2,4,add);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  
+//I want you to think of potential problems you could solve given the data 
+//set and the 5k fun run theme.  Try to create and then solve 3 unique problems 
+//using one or many of the array methods listed above.
 
 // Problem 1
 
