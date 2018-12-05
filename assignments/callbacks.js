e3ea9a2dcc9654c3ee8fb78278039776b8ace79c
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-12-04 18:06:25 -0600

    finished challenge 2 of callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -20,18 +20,29 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+// getLength passes the length of the array into the callback.
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+      
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+console.log(length);
+});
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+// last passes the last item of the array into the callback.
+function last(array, callback) {
+  callback(array[array.length - 1]);
 }
+last(items, (last) => {
+  console.log(last);
+});    
+  
+// sumNums adds two numbers (x, y) and passes the result to the callback.
+ 
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+
 }
 
 function multiplyNums(x, y, cb) {
--- assignments/closure.js
+++ assignments/closure.js
@@ -31,14 +31,19 @@ console.log(newCounter());
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-
-
-
-
-
-
-};
+const counterFactory = function() {
+  let i = 0
+  return function increment () {
+     i = i +1
+    return i;
+  
+  return function decrement () {
+    i = i - 1
+    return i;
+}
+  };
+}
+const newCounterFactory = counterFactory();
 
 // Return an object that has two methods called `increment` and `decrement`.
 // `increment` should increment a counter variable in closure scope and return it.
