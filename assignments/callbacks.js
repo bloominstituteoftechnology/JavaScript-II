0c16d408745a535a7d5d34a4aef281ca2e57ce23
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-05 08:26:02 -0600

    finishing

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -78,8 +78,7 @@ multiplyNums(7, 7, function(multiply) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-
-
+}
 
 
 
