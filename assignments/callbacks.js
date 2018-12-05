997a81db39b3e65e8e2ceb33f4ec8dae299ac9b2
drkfogle <rodriguez.kevin.094@gmail.com>
2018-12-04 19:37:38 -0500

    Now updated CBs even more

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -59,6 +59,7 @@ function product(x,y){
 multiplyNums(30,420,product)
 
 function contains(item, list, cb) {
+  return cb(item,list)
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
