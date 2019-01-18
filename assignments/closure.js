4d93e3ac757340b191ef3c325cb28015af545a53
renegade27 <foemaster@live.com>
2019-01-17 19:36:56 -0600

    Update closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,25 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const closure = () => {
+  var a = 4;
+}
+
+// console.log(a);
+// The above statement would return "a is not defined", because you're console.log'ing it outside of its scope.
+// The variable a is definied inside of the function scope, because of that it is a variable that is local to its function.
 
 
 // ==== Challenge 2: Create a counter function ====
+let count = 0;
 const counter = () => {
+  return function() {
+    return count += 1;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
+newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
