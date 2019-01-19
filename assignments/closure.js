80e62857828fb1ec493ec3f7ce341a18d0998cd3
max-keller427 <max.keller427@gmail.com>
2019-01-19 11:16:18 -0700

    Think I did closure right..

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function isThisClosure () {
+  const closure = 'closure'
+  console.log(closure)
+  thisIsClosure();
+  function thisIsClosure () {
+    console.log(`Yea, this is ${closure}.`)
+  }
+}
+isThisClosure();
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (x) => {
+  let count = x;
+  count++;
+  letsCount();
+  function letsCount () {
+    console.log(count);
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
+counter(3);
+
+// const newCounter = counter();
+// newCounter;
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
