11498b45f2e9099208e38b34e57c5161a5768a75
stevekutz <stkutz@gmail.com>
2019-01-19 12:56:33 -0600

    STRETCH IIFE version of inc/dec counter completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -63,4 +63,27 @@ countUpDown.increment();
 countUpDown.decrement();
 
 
+/////////////////   Experiments with IIFEs   /////////////////
+const counterFactory_IIFE = ( () => {
+  // Return an object that has two methods called `increment` and `decrement`.
+  // `increment` should increment a counter variable in closure scope and return it.
+  // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+
+  const countObj = {
+    increment: function() {return ++count},
+    decrement: function() {return --count}
+
+//    increment: ()  => ++count,
+//    decrement: () => --count
+
+  };
+
+  return countObj
+})();
+
+let countUpDown_IIFE = counterFactory_IIFE;  // no () here since IIFE
+countUpDown_IIFE.increment();
+counterFactory_IIFE.decrement();
+
 
