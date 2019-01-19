8605bb6fe6f9620677d75ac356902a17df44e4e3
Antony Nyagah <antonynyagah@antonys-MacBook-Pro.local>
2019-01-19 13:22:41 -0500

    intial commit

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,22 +55,61 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+
 let fullName = [];
+
+runners.forEach(function(person)
+{
+	const name = person.first_name +" "+ person.last_name;
+	fullName.push(name);
+})
+  
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+
 let allCaps = [];
+
+runners.map(function(person)
+
+{
+
+	const name = person.first_name.toUpperCase()
+	allCaps.push(name)
+
+
+})
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+
+
 let largeShirts = [];
+
+const filterLargeShirts = runners.filter((person) => {
+  return person.shirt_size == "L";
+});
+
+largeShirts.push(filterLargeShirts)
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+
+
 let ticketPriceTotal = [];
+
+const reduceDonation = runners.reduce((total, person) => {
+  return total += person.donation;
+}, 0);
+
+ticketPriceTotal.push(reduceDonation)
+
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
