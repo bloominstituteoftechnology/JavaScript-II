dca8c5a05820c10a5c218c3f918c339a5733e55a
Wes-White <wesleyrwhite86@gmail.com>
2019-01-19 11:22:07 -0800

    callbacks completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -47,6 +47,12 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item) 
+    let result = true;
+    else let result = false;
+  }
+  cb(result);
   
 }
 
