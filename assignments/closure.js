a9f805c8a7f0b207b56cd265462499a40a7ed230
Scott Grobe <gitgrobe@gmail.com>
2019-01-19 23:18:15 -0500

    update stretch &  closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,10 +5,20 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count = count + 1;
+    return count;
+  }
 };
+
+const newCounter = counter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+console.log(newCounter());
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
