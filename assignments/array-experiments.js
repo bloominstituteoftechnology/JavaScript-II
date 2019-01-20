3571beabcea18553e0c5dbbcc36da0a87cac4ef0
stevekutz <stkutz@gmail.com>
2019-01-20 01:21:49 -0600

    POST SPRINT - experimenting with array of objects, added functions to clone, sort by key, generate random dates, and add key with date to array of objects

--- assignments/array-experiments.js
+++ assignments/array-experiments.js
@@ -1,8 +1,3 @@
-// A local community center is holding a fund raising 5k fun run and has invited 50 small
-// businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
-// ADDED
-// Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
-
 const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"cseiler0@wired.com","shirt_size":"2XL","company_name":"Divanoodle","donation":75},
 {"id":2,"first_name":"Whitaker","last_name":"Ierland","email":"wierland1@angelfire.com","shirt_size":"2XL","company_name":"Wordtune","donation":148},
 {"id":3,"first_name":"Julieta","last_name":"McCloid","email":"jmccloid2@yahoo.com","shirt_size":"S","company_name":"Riffpedia","donation":171},
@@ -54,51 +49,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":49,"first_name":"Bel","last_name":"Alway","email":"balway1c@ow.ly","shirt_size":"S","company_name":"Voolia","donation":107},
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
-// ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
-runners.forEach(el => fullName.push(`${el.first_name} ${el.last_name}`));
 
-console.log(fullName);
-
-// ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-allCaps = runners.map(runner => runner.first_name.toUpperCase() );
-console.log(allCaps);
-
-
-
-// ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-largeShirts = runners.filter(runner => runner.shirt_size === 'L');
-console.log(largeShirts);
-
-// ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce( (acc, cur) => acc + cur.donation, 0 );
-console.log(ticketPriceTotal);
-
-// ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
-
-// Problem 1 - Get list of runners that have XL, 2XL, or 3XL  shirt size
-let xLargeDonorList = [];
-xLargeDonorList = runners.filter( runner => runner.shirt_size.includes('XL'));
-console.log(xLargeDonorList);
-
-// Problem 2 - Instead of using list from Prob2, get total amount of donations of runners that have XL, 2XL, or 3XL  shirt size
-let largeDonorDonationTotal = [];
-largeDonorDonationTotal = runners.filter( runner => runner.shirt_size.includes('XL'))
-                                 .reduce( (acc, curr) => acc + curr.donation, 0);
-console.log(largeDonorDonationTotal);
-
-// Problem 3 - find max email length
-let max_emailLength = [];
-max_emailLength = runners.map(runner => runner.email.length);
-console.log(Math.max(...max_emailLength));
 
 ////////////////////////////////////////////
 // Deep clone array for some extra experimenting
@@ -106,3 +57,4 @@ let runnerClone = function(arr) {
     return JSON.parse(JSON.stringify(arr));
 }
 
+runnerClone(runners);
