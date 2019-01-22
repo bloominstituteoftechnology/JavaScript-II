32609c5e98efa8dc9e9125a2efb223eb6c1d155b
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-01-22 14:33:02 -0800

    added all answers including stretch question to callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,23 +24,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
+}
+
+function cb(result) {
+  console.log(result);
 }
 
 /* STRETCH PROBLEM */
@@ -49,4 +62,15 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let arr = [];
+  array.forEach((val) => {
+    if (!arr.includes(val)) {
+      arr.push(val);
+    }
+  });
+  return arr;
 }
+
+// removeDuplicates([1,1,2,3,4,4,5,6], function(val){
+//   console.log(val);
+// });
