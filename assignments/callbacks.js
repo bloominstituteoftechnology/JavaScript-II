51dc790b2e89f0daace0be9b583919343938e4e3
UnknownMonk <smodcartoon@gmail.com>
2019-01-22 17:32:25 -0500

    finised call backs array methods still working of closures just forgot to commit

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,47 +1,74 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = [
+  'Pencil',
+  'Notebook',
+  'Notebook',
+  'Notebook',
+  'yo-yo',
+  'Gum',
+  'Gum'
+];
 
-/* 
+//Given this problem:
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+// function firstItem(arr, cb) {
+//   // firstItem passes the first item of the given array to the callback function.
+// }
 
-  // Potential Solution:
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+// Potential Solution:
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
-*/
-
+firstItem(items, first => {
+  console.log(first);
+});
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, length => {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
+  return cb(arr[3]);
 }
 
+last(items, last => {
+  console.log(last);
+});
+
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y); // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+
+sumNums(2, 2, add => {
+  console.log(add);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(3, 4, times => {
+  console.log(times);
+});
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  // contains checks if an item is present inside of the given array/list.	  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.	  // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+
+contains('Pencil', items, Inarr => {
+  console.log(Inarr);
+});
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +76,17 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  let filtredArr = [];
+
+  array.forEach(element => {
+    if (filtredArr.indexOf(element) === -1) {
+      filtredArr.push(element);
+    }
+    return cb(filtredArr);
+  });
 }
+
+removeDuplicates(items, dups => {
+  console.log(dups);
+});
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,34 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const taco = meat => {
+  const typeOfmeat = meat;
+
+  console.log(`On my tacos I like ${typeOfmeat} `);
+
+  const sandWitch = meat => {
+    console.log(`On my sandwitch I like ${typeOfmeat}`);
+  };
+  sandWitch('ham');
+};
+taco('beef');
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+
+  let count = 0;
+  return function counter() {
+    count += 1;
+    console.log(count);
+    return count;
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
