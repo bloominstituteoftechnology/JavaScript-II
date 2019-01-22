69c6a3cc6c29e73a16a9200839cd4a75fbdcd588
UnknownMonk <smodcartoon@gmail.com>
2019-01-22 17:52:42 -0500

    counterFactor stretch goal added

--- assignments/closure.js
+++ assignments/closure.js
@@ -33,6 +33,13 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0;
+  return (add_subtract = {
+    add: () => {
+      counter += 1;
+      return counter;
+    }
+  });
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
