d8f22fad8a42c3411758031536676d1ed87e4790
blubrandi <blubrandi0@gmail.com>
2019-01-22 21:26:59 -0700

    Final commit.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function age (numberOfAge, monthsOrYears) {
+  const ageDeclaration = "My age is: ";
+   
+  function myAge() {
+    return ageDeclaration + numberOfAge + ' ' + monthsOrYears;
+  }
+  return myAge();
+}
+
+console.log(age(22, 'years'));
+
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => (++count);
+
 };
+
+const newCounter = counter();
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
