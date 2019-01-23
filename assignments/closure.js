4952dbd0165f04aa24e4fa65eabf71516a8ecfcd
Tristan Grovender <therealtristangrovender@gmail.com>
2019-01-23 15:30:07 -0600

    AH example

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,8 +13,26 @@ const counter = () => {
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+
+  let count = 0
+
+  return {
+    increment: () => ++count,
+    decrement: () => --count
+  }
+}
+
+const newCounterFactory = counterFactory()
+newCounterFactory.increment()
+newCounterFactory.increment()
+newCounterFactory.increment()
+newCounterFactory.increment()
+newCounterFactory.increment()
+newCounterFactory.increment()
+newCounterFactory.increment()
+
+console.log(newCounterFactory);
