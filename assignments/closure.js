cca2d75f87bdf3966257b0e68ace059a9d272fd9
ItelSunday <iteldomingotech@gmail.com>
2019-01-23 16:14:32 -0800

    solved problems

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const counter = () => {
+  function mySuccess(myClass, timeline) {
+    const className = myClass;
+    function text() {
+      const time_line = timeline;
+      console.log(`Hello, ${className} I'm back to the ${time_line}`);
+    }
+    text();
+  // Return a function that when invoked increments and returns a counter variable.
+}
+mySuccess("WEBFT 17", 'future');
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let num = 0;
+  function increase() {
+    num++;
+    return num;
+  }
+  return increase;
+}
+let newCount = counter();
+console.log(newCount());
+
   // Return a function that when invoked increments and returns a counter variable.
-};
+
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
