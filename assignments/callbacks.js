b34d10fd1704871861e2545d00e39fdd78ae8d79
myasia barbee <myasiabarbee@icloud.com>
2019-01-31 23:30:05 -0500

    Pull Request

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,8 +23,6 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-
 }
 
 function last(arr, cb) {
