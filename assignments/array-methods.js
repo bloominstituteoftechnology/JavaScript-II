d308ed047a1dacbe0163562d54b64125004da301
myasia barbee <myasiabarbee@icloud.com>
2019-02-11 18:56:57 -0500

    Array C5 SetUp

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -99,7 +99,12 @@ ticketPriceTotal = ticketPriceTotal.reduce(function (accumulator, currentValue) 
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Combining the company name with the donation in a new array.
+
 
 // Problem 2
+// Re ordering the runners in alphabetical order by last name 
 
 // Problem 3
+// Find the representative for the company Bluejam 
+ 
