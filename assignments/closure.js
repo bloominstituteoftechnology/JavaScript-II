4172ceced0d05d432f51815c80895b74abde586d
Garrett Weems <gwgraphicdesign@gmail.com>
2019-02-26 18:43:25 -0500

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,19 @@
-// ==== Challenge 1: Write your own closure ====
+// === Challenge 1: Write your own closure ===
 // Write a simple closure of your own creation.  Keep it simple!
 
+const letItRip = () => {
+  const name = "Captian Falcon";
+  const falconPunch = () => name;
+  return falconPunch();
+};
+
+console.log(letItRip());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+const counter = () => {};
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
