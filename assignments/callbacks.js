3d53941286b08fedb725a7a6d00e478a0436c0a7
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-03-28 20:56:32 -0700

    MAybe these are solutions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,21 +30,25 @@ function getLength(arr, cb) {
   return cb(arr.length)
 }
 
+// last passes the last item of the array into the callback.
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[3])
 }
 
+// sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
 
+// multiplyNums multiplies two numbers and passes the result to the callback.
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
 
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  return cb(items & list)
 }
 
 /* STRETCH PROBLEM */
