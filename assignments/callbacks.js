59cb48ca89ffad4ea9f9657aea46e70a6688fd57
Cory Hale <Cory@CoryHalsMacbook.sumo.io>
2019-03-30 12:10:58 -0600

    Finished almost all of MVP except for array-methods Challenge 5

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -25,31 +25,99 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+
+
+
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+function returnLength(x) {
+    return x;
+}
+
+console.log(getLength(items, returnLength));
+
+
+
+
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    // last passes the last item of the array into the callback.
+    return cb(arr[arr.length - 1]);
 }
 
+function lastItem(x) {
+    return x;
+}
+
+console.log(last(items, lastItem));
+
+
+
+
+
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x * y);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+function returnNum(x) {
+    return x;
 }
+
+console.log(sumNums(4, 5, returnNum));
+console.log(multiplyNums(4, 5, returnNum));
+
+
+
+
+
+function contains(item, list, cb) {
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+
+    // list.forEach(function(value) {
+    //     if (value === item) {
+    //         return cb(true);
+    //     } else {
+    //         return cb(false);
+    //     }
+    // })
+
+    let tempVal = 0;
+
+    list.forEach(function(value) {
+        if (value === item) {
+            tempVal++;
+        }
+    })
+
+    if (tempVal > 0) {
+        return cb(true);
+    } else {
+        return cb(false);
+    }
+}
+
+function log(bool) {
+    console.log(bool);
+}
+
+let item = "Notebook";
+
+contains(item, items, log);
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+//function removeDuplicates(array, cb) {
+// removeDuplicates removes all duplicate values from the given array.
+// Pass the duplicate free array to the callback function.
+// Do not mutate the original array.
+//}
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function engaged() {
+    console.log("I asked my girlfriend to marry me...");
+
+    function sheSaidYes() {
+        console.log("AND SHE SAID YES!!!");
+    }
+
+    sheSaidYes();
+}
+
+engaged();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+//const counter = () => {
+// Return a function that when invoked increments and returns a counter variable.
+//};
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+//const counterFactory = () => {
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+//};
