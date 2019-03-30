0264ae129dfd7fe4e07418336b71cc348897e910
Cory Hale <Cory@CoryHalsMacbook.sumo.io>
2019-03-30 13:02:05 -0600

    Finished 1 of 2 stretch problems in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,12 +17,19 @@ engaged();
 
 
 // ==== Challenge 2: Create a counter function ====
-//const counter = () => {
-// Return a function that when invoked increments and returns a counter variable.
-//};
+const counter = () => {
+    // Return a function that when invoked increments and returns a counter variable.
+    let count = 0;
+    return function() {
+        count++;
+        return count;
+    }
+};
+
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 //const counterFactory = () => {
