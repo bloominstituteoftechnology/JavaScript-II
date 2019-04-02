c679efc82dff62cc3b1d3863961aac4e8c0e7483
unknown <bejolo018@gmail.com>
2019-04-02 17:40:19 -0500

    completed one stretch goal

--- assignments/closure.js
+++ assignments/closure.js
@@ -14,12 +14,12 @@ scramble();
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const increment = () => { n++;
+  return n;};
+var n = 0;
+
+increment();
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
