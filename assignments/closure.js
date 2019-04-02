060ad4acf05e0b13ad7ed9cf77245f0763a8c2fd
Shadowborn <james.starks023@gmail.com>
2019-04-02 15:53:03 -0600

    Finished array method challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function mightyKingdom(){
+    const greatWarriors = "Great Warriors";
+    const GoldAmount = 1000000;
 
+    function cityCapital(){
+      const castle = (`This castle is filled with ${GoldAmount} gold`);
+      console.log(castle);
+    }
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
