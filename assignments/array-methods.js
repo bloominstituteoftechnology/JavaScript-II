1986664a1e5971d68199930ecf2f0d62718aaec0
AlphaRomulus <rjordan1352@gmail.com>
2019-04-02 18:06:33 -0700

    Array-Methods Completed

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,56 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+    runners.forEach(runner =>{
+       fullName.push(runner.first_name + ' ' + runner.last_name);
+    });
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+allCaps = runners.map(runner =>{
+   return runner.first_name.toUpperCase();
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+    largeShirts.push(runners.filter(runner => runner.shirt_size == "L" ));
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal.push(runners.reduce((x,y) => x + y.donation,0));
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let totalShirtsBySize = [];
+let countShirtsBySize = [];
+shirtsBySize = [...new Set(runners.map(runner => runner.shirt_size))];
+shirtsBySize.forEach(x => {
+    countShirtsBySize.push(runners.filter(runner =>{
+       return runner.shirt_size === x;
+    }))
+});
 
+countShirtsBySize.forEach(x => {
+    totalShirtsBySize.push(x[0].shirt_size + ' ' + x.length);
+})
+console.log(totalShirtsBySize);
 // Problem 2
-
+let emailList =[];
+emailList.push(runners.map(runner => runner.email));
+console.log(emailList);
 // Problem 3
+
+let nameDonation =[];
+runners.forEach(runner => {
+    nameDonation.push(`${runner.first_name} ${runner.donation}`);
+});
+console.log(nameDonation);
