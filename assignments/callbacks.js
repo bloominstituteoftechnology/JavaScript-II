3d8263a8beb5178b5e3d2e1ae2e42825749731d5
efrain <efarinva98@gmail.com>
2019-04-09 19:44:13 -0400

    done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,27 +30,43 @@ function getLength(arr, cb) {
   return cb(arr.length) ;
 }
 
+getLength(items,function(call){ console.log(call) });
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr.length -1);
 }
-last(items,any());
+last(items,function(mycall){console.log(mycall,1)});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x+y);
 }
+sumNums(10,8,function(funcName){
+console.log(funcName);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x*y);
 }
+multiplyNums(40,2,function(func){
+console.log(func);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(item,list);
+  for(let i = 0; i<list.length ;i++){
+    if(item === list[i]){
+      return console.log(("true"));
+    }else{
+      return console.log(("false"));
+    }
+  }
+  
 }
+contains("Pencil",items,function(mycb){console.log(mycb)});
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,16 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-const close = () => {
+const close = (phrase) => {
   const func = () => {
-
+    return console.log(`goodbye ${phrase}`);
   }
+  func();
   const and = () => {
-    
+    return console.log(`hello ${phrase}`);
   }
+  and();
 }
 
-
+close("john");
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
