3473748a103932d684d54970475f4aeba81a43a6
efrain <efarinva98@gmail.com>
2019-04-09 15:26:26 -0400

    there we go

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -54,7 +54,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
 
-// efrain-vazquez
+// efrain
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
