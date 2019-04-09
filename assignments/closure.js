09433f3ff19c6245234f749f6bc2650786a1a7a3
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-09 16:57:34 -0500

    adds first answer to closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function yharnam() {
+  const eileen = "Prepare yourself for the worst. There are no humans left. They're all flesh-hungry beasts, now.";
+  console.log(`${eileen}`)
+
+  function centralYharnam() {
+      const connection = "Cathedral Ward";
+      console.log(`Central Yharnam is the upper-class district of the Gothic city of Yharnam, and connectes directly to the ${connection}`)
+  }
+  centralYharnam();
+}
+yharnam();
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
