e4b1fca11a63c81f7ffeb739101cbd29df79e5d7
SKC <kaursheela87@gmail.com>
2019-05-17 19:35:45 -0400

    Closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const cat = 'meow';
+function returncat () {
+  return cat;
+}
+returnFoo();
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
