42624f3149f0542d061de3099ec5227ad03ae252
SKC <kaursheela87@gmail.com>
2019-05-17 21:19:27 -0400

    Arrays

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -29,23 +29,34 @@ Function invocation
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
 
-showFirst(items, (firstItem) => {
-  console.log(firstItem);
+
+showLength(items, (LengthOfList) => {
+  console.log(LengthOfList);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+showLength(items, (LastofItems) => {
+  console.log()
+}
+myArray[myArray.length-1];
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+}
+
+const add = function(x, y) {
+  return x + y;
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
