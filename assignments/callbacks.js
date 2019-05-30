d4c02c7d1f03dc0ae1f1e5a9d6a0aa905330f99d
turkworks <andrewcengiz@gmail.com>
2019-05-29 21:23:51 -0600

    almost done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,55 +1,79 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work.
+// You have been provided an example of a problem and a solution to see how this works with
+// our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
-/* 
+function higherFunction(arr, callback) {
+  return callback(arr);
+}
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // Potential Solution:
-
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
+const getLowerCaseFirstLetter = function(arr) {
+  words = arr.map(word => {
+    return word.split("")[0].toLowerCase();
   });
+  return words;
+};
 
-*/
-
-
+// Get Length
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
 
+const length = function(arr) {
+  return arr.length;
+};
+
+// Last Item
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr);
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+const lastItem = function(arr) {
+  return arr.pop();
+};
+
+// Sum Nums
+function higherOrderMath(x, y, cb) {
+  return cb(x, y);
 }
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+let sumNums = function(x, y) {
+  return x + y;
+};
+
+// Multiply Nums
+let multiplyNums = function(x, y) {
+  return x * y;
+};
+
+// Contains
+function higherFunction(listItem, arr, cb) {
+  return cb(listItem, arr);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+let contains = function(listItem, arr) {
+  let inList = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (listItem === arr[i]) {
+      inList = true;
+    } else if (listItem !== arr[i]) {
+      continue;
+    }
+  }
+  return inList;
+};
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+function higherFunction(array, cb) {
+  return cb(array);
 }
+
+let removeDups = function(array) {
+  console.log(
+    array.filter(function(item, index) {
+      return array.indexOf(item) >= index;
+    })
+  );
+};
