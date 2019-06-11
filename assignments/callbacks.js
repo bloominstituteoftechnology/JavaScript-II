31d1938b446307ad97c1ad5816b02a019cff0cd4
Nicolas Brett <nicolasb59@gmail.com>
2019-06-11 18:49:59 -0400

    Callbacks completed.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
+  cb(arr.length);
+};
+getLength(items, length => {
+  console.log(`The length of the array is ${length}.`)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+  cb(arr[arr.length-1]);
+};
+last(items, last => {
+  console.log(`The last item on the list is ${last}.`)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  cb(x + y);
+};
+sumNums(2, 3, sum => {
+  console.log(`The sum is ${sum}.`);
+});
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  cb(x * y);
+};
+multiplyNums(2, 3, product => {
+  console.log(`The product of these numbers is ${product}.`)
+});
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  const listArray = () => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === str){
+        return true;
+      }
+    }  return false;
+  };
+  cb(listArray());
+};
+//^^Not sure if correct^^. Tried using arrow on line 62... displayed syntax error
+
 
 /* STRETCH PROBLEM */
 
