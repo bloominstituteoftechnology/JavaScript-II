473e81cefbdffec8fb340c92961044de8f03df06
Greg Johnston <johng3269@gmail.com>
2019-06-11 17:41:47 -0400

    Closure done, one callback left

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -79,7 +79,13 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+function contains(item, list, cb) {
+    const result = list.includes(item);
+}
+function result(result) {
+    console.log(result);
+}
+contains('gum', items, result());
 
 ///////////////////////////////////////////////////////////////////
 /* STRETCH PROBLEM */
