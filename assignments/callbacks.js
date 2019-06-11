3034ac36f78f37d25c49e741468720fbfae9d26e
Kelly M <moreira.kd@outlook.com>
2019-06-11 18:59:34 -0400

    JS2

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,23 +27,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+
+  return cb(arr.length);
 }
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+
+  cb(x+y);
+};
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+
+  cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+
 }
 
 /* STRETCH PROBLEM */
@@ -53,3 +65,8 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+
+
+// Went through TK from yesterday and today's again for further understanding. Did follow along 
+// assaignments and challeneges in codepen. Started functional programing curriculum on freecodecamp again.
