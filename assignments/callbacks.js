e25db5fd56bf77f63f9d48ce081d3af204ce7185
meganwhite <mfw247@gmail.com>
2019-06-11 17:00:38 -0400

    callback functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  return cb(sum);
 }
+
+sumNums(1, 2, function(answer) {
+  console.log(answer);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x * y;
+  return cb(result);
 }
+
+multiplyNums(10,20,function(product) {
+  console.log(product);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  }
+  else {
+    return cb(false);
+  }
 }
+
+contains("Gum",items,function(result) {
+  console.log(result);
+})
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +84,15 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+ let cleanedArray = array.filter(function(elem,index,self) {
+   return index === self.indexOf(elem);
+ });
+ return cb(cleanedArray);
 }
+
+duplicateArray = ["Gum","Pizza","Gum","Gum","Pizza","Gum","Hat"]
+
+removeDuplicates(duplicateArray, function(result) {
+  console.log(result);
+})
+
