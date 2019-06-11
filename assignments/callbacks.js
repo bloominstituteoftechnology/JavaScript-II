dde4b50e25367e74cc408d0883fa2f5176273359
Julie Thrall <juliethrall10@gmail.com>
2019-06-11 13:17:47 -0700

    completed callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,49 +2,85 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
 
-  // Potential Solution:
+//Given this problem: 
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+//function firstItem(arr, cb) {
+// firstItem passes the first item of the given array to the callback function.
+//}
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+// Potential Solution:
 
-*/
+// Higher order function using "cb" as the call back
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
+
+// Function invocation 
+firstItem(items, function (first) {
+  console.log(first)
+});
+
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
+
 }
+
+getLength(items, function (length) {
+  console.log(length)
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, function (lastItem) {
+  console.log(lastItem)
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x + y;
+  return cb(result);
 }
+
+sumNums(1, 2, function (sum) {
+  console.log(sum);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x * y;
+  return cb(result);
 }
+
+multiplyNums(2, 3, function (result) {
+  console.log(result)
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let result = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].includes(item)) {
+      result = true;
+    }
+  }
+  return cb(result)
 }
+
+contains('yo-yo', items, function (boolean) {
+  console.log(boolean)
+})
+
+
 
 /* STRETCH PROBLEM */
 
