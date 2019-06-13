751d7fd642037e25bfb0d20581017d8d12f79b66
A.D.Faris <adfaris@gmail.com>
2019-06-12 23:30:29 -0400

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let name = 'A.D'
+let myName = () => {
+  console.log(`my name is ${name}`)
+}
+myName()
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
