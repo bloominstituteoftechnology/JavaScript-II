88862e38d1a267a0ab71619e226237afc2bcf20e
francistse23 <wtse.ucsd@gmail.com>
2019-06-14 11:18:30 -0700

    Completed callbacks.js and array-methods.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,12 +30,11 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  if (list[i] === item){
+  if (list.includes(item))){
     return cb(true);
   }
-  else{
-    return cb(false);
-  }
+
+  return cb(false);
 }
 
 /* STRETCH PROBLEM */
@@ -44,4 +43,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let duplicateFree = new Set(array);
+  cb(duplicateFree);
 }
