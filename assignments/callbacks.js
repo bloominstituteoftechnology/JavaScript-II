223e2ff94f9985211abee87b40eafe410266d7b4
A.D.Faris <adfaris@gmail.com>
2019-06-17 06:44:03 -0400

    CB, CL

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,12 +1,12 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array. 
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.
 // Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Gum']
 
-/* 
+/*
 
-  Given this problem: 
-  
+  Given this problem:
+
   function firstItem(arr, cb) {
     firstItem passes the first item of the given array to the callback function.
   }
@@ -18,7 +18,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  Function invocation 
+  Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
@@ -27,35 +27,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength (arr, cb) {
   // getLength passes the length of the array into the callback.
-  cb(arr.length)
+  return cb(arr.length)
 }
 
-function last(arr, cb) {
+getLength(items, function (arrayLength) {
+  console.log(arrayLength)
+})
+
+function last (arr, cb) {
   // last passes the last item of the array into the callback.
   cb(arr[arr.length - 1])
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  cb(x + y)
-}
+last(items, function (lastItem) {
+  console.log(lastItem)
+})
 
-function multiplyNums(x, y, cb) {
+function sumNums (x, y, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb (x + y)
+}
+sumNums(2, 4, function (sum) {
+  console.log(sum)
+})
+
+function multiplyNums (x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   cb(x * y)
 }
+multiplyNums(5, 6, function (result) {
+  console.log(result)
+})
 
-function contains(item, list, cb) {
+function contains (item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  
-
+  return cb(list.includes(item))
 }
+contains('Gum', items, function (itemExist) {
+  console.log(itemExist)
+})
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+function removeDuplicates (array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const uniqueSet = new Set(array)
+  return cb(uniqueSet)
 }
+removeDuplicates(items, function (uniqueArray) {
+  console.log(uniqueArray)
+})
--- assignments/closure.js
+++ assignments/closure.js
@@ -11,14 +11,41 @@ myName()
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let counter = 0
+  return function () {
+    return ++counter
+  }
+}
+
+const newCounter = counter()
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+  let counter = 0
+  let counterObject = {
+    increment: function () {
+      return ++counter
+    },
+    decrement: function () {
+      return --counter
+    }
+  }
+  return counterObject
+}
+const newCounteryFactory = counterFactory()
+
+console.log(newCounteryFactory.increment())
+
+console.log(newCounteryFactory.decrement())
+console.log(newCounteryFactory.increment())
+
+console.log(newCounteryFactory.decrement())
+console.log(newCounteryFactory.increment())
+console.log(newCounteryFactory.decrement())
+console.log(newCounteryFactory.increment())
