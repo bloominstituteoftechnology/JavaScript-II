dca5f54108f48fb555a83fd5285c0c6e558bea06
DasGMA <dasgrigoma@gmail.com>
2019-06-23 17:11:52 -0700

    Few Changes

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,29 +1,66 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 }
+
+firstItem(items, (firstItem) => {
+  console.log(firstItem);
+})
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, (length) => {
+  console.log(length);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
+
+last(items, (theLast) => {
+  console.log(theLast);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+sumNums(1, 2, (numSum) => {
+  console.log(numSum);
+})
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(2, 3, (multi) => {
+  console.log(multi);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      cb(true);
+    }
+  }
+  cb(false);
 }
+
+contains('Gum', items, (contains) => {
+  console.log(contains);
+})
 
 /* STRETCH PROBLEM */
 
@@ -31,4 +68,14 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const unique = [];
+  for (let i = 0; i < array.length; i++) {
+    contains(array[i], unique, isUnique => isUnique ? undefined : unique.push(array[i]));
+  }
+
+  cb(unique);
 }
+
+removeDuplicates(items, (duplicatesRemoved) => {
+  console.log(duplicatesRemoved);
+})
