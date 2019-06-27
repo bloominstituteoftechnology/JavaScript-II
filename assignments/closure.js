000718bcbf7190a76485fcf58d06ae04217f38db
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-06-26 20:30:23 -0400

    Finished with assignment -- callback stretch needed

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const lastName = "Leach";
+function greet() {
+  const firstName = "Jay";
+  return firstName + " " + lastName;
+}
 
+console.log(greet());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,14 +14,27 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function () {
+    count += 1;
+    return count;
+  }
 };
+const newCounter = counter();
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+let num = newCounter() - 1;
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+    return {
+    increment: () => num += 1,
+    decrement: () => num -= 1
+    }
+}
+
+const newCounterFactory = counterFactory();
+console.log(newCounterFactory.increment()); // Calls newCounter => increment
+console.log(newCounterFactory.decrement()); // Calls newCounter => decrement
