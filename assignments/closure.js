59e7a2d5842129df0fe3700ff566f6efa4186d30
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-06-27 19:48:58 -0400

    completed closures.js exercise

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function name(a) {
+  return function(j) {
+    console.log(`${a}\'s favorite language to program in is ${j}!!`);
+  }
+}
+const aaron = name('Aaron');
+
+aaron('JavaScript');
+
+const john = name('John');
+john('Python');
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
