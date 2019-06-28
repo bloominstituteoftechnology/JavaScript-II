1abe5b5b9f2ea419070c763954ef50814b451b15
david-pok <david.pok@gmail.com>
2019-06-28 01:49:55 -0700

    completes most except some stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,9 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function hi() {
+  let a = 4;
+  return a * a;
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,7 +11,15 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {count += 1; return count}
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
