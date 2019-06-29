e161b046e0642cff83d936091b1e8d83bd264d51
Mysti Dyse <mysti@Mystis-MacBook-Pro.local>
2019-06-28 18:47:20 -0700

    closure.js is complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,24 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+// Sorry if its not simple!
+function Postmates(name){
+  let welcome = 'welcome to Postmates!';
+  console.log(`Hi `+ name + ` ${welcome}`);
 
+  function theUsual(){
+    let usual = 'Bistek a la Mexicana';
+    let usualPlace = 'La Campana';
+    console.log(`Would you like your usual ${usual} from ${usualPlace}?`);
+
+    function Lucky(){
+      let discount = 'FREE'
+      console.log(`Guess what?! today is your lucky day! Your order is ${discount}!!`)
+    }
+    Lucky();
+  }
+  theUsual();
+}
+Postmates('Mysti');
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
