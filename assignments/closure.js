22e2a0c72fe11030c2ab0f95120694c8d3ab8c77
smv5047 <smv5047@gmail.com>
2019-06-30 20:57:48 -0400

    closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,19 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+//Starting Inventory of Books
+let bookInventory = 10;
+
+//function to subtract from inventory everytime a book is purchased
+function bookPurchased () {
+  return --bookInventory;
+};
+
+bookPurchased();
+bookPurchased();
+bookPurchased();
+bookPurchased();
+console.log(bookInventory);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
