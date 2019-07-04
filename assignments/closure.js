df7e1c868d4805deb5db269ac76f2784c8a190bf
Your name <a.m.verdine@gmail.com>
2019-07-04 12:19:08 -0400

    Initial commit for JS-II project: completed closure.js > added a counter > added an incrementing/decrementing object.

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,16 +21,61 @@ basicMath(10, 5);
 
 
 // ==== Challenge 2: Create a counter function ====
+
+// Return a function that when invoked increments and returns a counter variable.
+
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+let count = 0;
+  return function () {
+
+
+    return count++;
+  }
 };
+
+const newCardCount = counter();
+console.log(newCardCount());
+console.log(newCardCount());
+console.log(newCardCount());
+console.log(newCardCount());
+console.log(newCardCount());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+const counterFactory = () => {
+  let privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+    decrement: function() {
+      changeBy(-1);
+    },
+    value: function() {
+      return privateCounter;
+    }
+  }
 };
+
+let counter1 = counterFactory();
+let counter2 = counterFactory();
+console.log(counter1.value());
+
+counter1.increment();
+counter1.increment();
+console.log(counter1.value());
+
+counter1.decrement();
+console.log(counter1.value());
+console.log(counter2.value());
