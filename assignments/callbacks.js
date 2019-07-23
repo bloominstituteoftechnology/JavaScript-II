e1e91e71c517fa1c3d6234aad92efae5ba5b1417
Jordan <jordandoan@hotmail.com>
2019-07-23 08:27:11 -0700

    callbacks finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,25 +27,53 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
 
+// CB TEST - 4
+// getLength(items, function(lens){
+//   console.log(lens);
+// });
+
+
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  // last passes the last item of the array into the callback.\
+  return cb(arr[arr.length-1])
 }
+
+// CB TEST - Gum
+// last(items, function(last){
+//   console.log(last);
+// });
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y)
 }
+// CB TEST - NotebookPencil
+// sumNums(items[1],items[0], function(num){
+//   console.log(num);
+// });
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
+// CB TEST - 6
+// multiplyNums(2, 3,  function(mult){
+//   console.log(mult);
+// });
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item))
 }
-
+// CB TEST - true
+// contains('yo-yo', items, function(result){
+//   console.log(result);
+// });
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
