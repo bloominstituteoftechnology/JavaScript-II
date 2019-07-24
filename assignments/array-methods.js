443ef4bad4b05ecfe6f9fd9a9612cee0cd78e1e3
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-07-23 22:23:58 -0700

    complted callback and array

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,22 +58,25 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 let fullName = [];
     runners.forEach(runner =>
     fullName.push(runner.first_name + " " + runner.last_name)
-);
+)
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = 
+    runners.map(runner => runner.first_name.toUpperCase())
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = 
+        runners.filter(runner => runner.shirt_size === "L")
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = 
+        runners.reduce((total, runner) => (total === runner.donation))
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -28,24 +28,47 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, arrayItem => {
+  console.log("The length of the array is " + arrayItem);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
+last(items, arrayItem => {
+  console.log("The last array element is " + arrayItem);
+})
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+sumNums(1, 2, arrayItem => {
+  console.log("The sum of 1+2 is " + arrayItem);
+})
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+multiplyNums(2, 2, arrayItem => {
+  console.log("The product of 2*2 is " + arrayItem);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+contains("Pencil", items, arrayItem => {
+  console.log("Is 'Pencil' inside items? " + arrayItem);
+})
+
 
 /* STRETCH PROBLEM */
 
