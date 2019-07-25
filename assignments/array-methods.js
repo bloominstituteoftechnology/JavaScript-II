0266a71f0d3c54127e2a48a748679adb6ad952d7
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-07-24 22:20:56 -0700

    Update array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -76,7 +76,7 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = 
-        runners.reduce((total, runner) => (total === runner.donation))
+        runners.reduce((total, runner) => {total + runner.donation},0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
