bbc64084d59329d7b9c27b47a910f2b060fd99bb
Janet Azehko <Janet@projecterostribe.com>
2019-07-25 07:04:34 -0700

    working on final problem from assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const firstName = 'Slim'
+function rappingWithDrDre() {
+  const lastName = 'Shady'
+  return `Hi! My name is...what? My name is...who? My name is chicka chicka ${firstName} ${lastName}`
+}
+console.log(rappingWithDrDre());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -12,6 +18,18 @@ const counter = () => {
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const counter = () => {
+  let count = 0;
+  return () => {
+      count = count + 1;
+      return count;
+  }
+};
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
