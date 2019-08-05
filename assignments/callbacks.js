ae829ec5d1f902ee218e423c6e02ef2f1f30861e
Michael Agard <mdagard@gmail.com>
2019-08-05 19:58:26 -0400

    restructure calbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,107 +1,90 @@
-/* PEER REVIEW NOTE: I cannot for the life of me follow the logic on the solution
-for the stretch goal. I've placed it at the bottom commented out for your convenience. */
+// Create a higher order function and invoke the callback function to test your
+// work. You have been provided an example of a problem and a solution to see
+// how this works with our items array.  Study both the problem and the
+// solution to figure out the rest of the problems.
 
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Pad', 'Pad', 'Pikachu'];
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
-/* Problem 1
-firstItem passes the first item of the given array to the callback function. */
-
-function firstItem(arr, cb) {
-  console.log(cb(arr))
-}
-let first = function(items) {
-  return items[0];
-}
-firstItem(items, first)
-
-
-/* Problem #2
-getLength passes the length of the array into the callback. */
+/* 
+  //Given this problem: 
+  
+  function firstItem(arr, cb) {
+    // firstItem passes the first item of the given array to the callback function.
+  }
+  // Potential Solution:
+  // Higher order function using "cb" as the call back
+  function firstItem(arr, cb) {
+    return cb(arr[0]);
+  }
+  // Function invocation 
+  firstItem(items, function(first) {
+    console.log(first)
+  });
+*/
 
 function getLength(arr, cb) {
-  console.log(cb(arr));
-}
-let arrayLength = function(arr) {
-  return arr.length;
-}
-getLength(items, arrayLength);
+  // getLength passes the length of the array into the callback.
 
+  return cb(arr.length);
+}
 
-/* Problem #3
-last passes the last item of the array into the callback. */
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
-  console.log(cb(arr));
-}
-let lastItem = function(items) {
-  return items[items.length - 1];
-}
-last(items, lastItem);
+  // last passes the last item of the array into the callback.
 
+  const lastItem = arr[arr.length - 1];
+  cb(lastItem);
+}
 
-/* Problem #4
-sumNums adds two numbers (x, y) and passes the result to the callback. */
+last(items, function(last) {
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
-  console.log(cb(x, y));
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+
+  cb(x + y);
 }
-let sum = function(x, y) {
-  return x + y;
-}
-sumNums(3, 5, sum);
 
-
-
-/* Problem #5
-multiplyNums multiplies two numbers and passes the result to the callback. */
+sumNums(4, 2, function(result) {
+  console.log(result);
+})
 
 function multiplyNums(x, y, cb) {
-  console.log(cb(x, y));
+  // multiplyNums multiplies two numbers and passes the result to the callback.
+
+  cb(x * y);
 }
 
-let multiply = function (x, y) {
-  return x * y;
-}
-multiplyNums(3, 3, multiply);
-
-
-/* Problem #6
-contains checks if an item is present inside of the given array/list.
-Pass true to the callback if it is, otherwise pass false. */
+multiplyNums(4, 2, function(result) {
+  console.log(result);
+})
 
 function contains(item, list, cb) {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === item) {
-      return cb(true);
-    }
-  }
-  return cb(false);
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+
+  cb(list.includes(item));
 }
-contains('yo-yo', items, function(flag) {
-  console.log(flag); // should be false
-});
 
-contains('great beans', items, function(flag) {
-  console.log(flag); // should be true
-});
-
-
-/* STRETCH PROBLEM
-removeDuplicates() removes all duplicate values from the given array.
-Pass the duplicate free array to the callback function.
-Do not mutate the original array. */
+contains("Pikachu", items, function(result) {
+  console.log(result);
+})
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  const output = [];
-  for (i = 0; i < array.length; i++) {
-    if (!output.includes(array[i])) {output.push(array[i])};
-  }
-  return cb(output); //console.log(output);
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
+
+  cb(array.filter((item, index) => array.indexOf(item) === index))
+  
 }
 
-items.push('Notebook', 'yo-yo', 'Pencil');
-
-removeDuplicates(items, function(output) {console.log(output)})
+removeDuplicates(items, function(duplicateFreeArray) {
+  console.log(duplicateFreeArray)
+})
