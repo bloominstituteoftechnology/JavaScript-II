373a1611a1d38fbb894e3b0c5202c979201f943d
Michael Agard <mdagard@gmail.com>
2019-08-05 20:21:28 -0400

    restructure and complete stretch for closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,9 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+
 const closureContainer = () => {
-  variableOne = 'Closure';
-  variableTwo = 'Is Cool';
-  return closure = () => {return `${variableOne} ${variableTwo}`;}
+  const variableOne = 'This variable is inside of closureContainer().';
+  return closure = () => {return variableOne}
 }
 const closureVariable = closureContainer();
 console.log(closureVariable())
@@ -15,8 +15,11 @@ const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   return increment = () => {return ++zero}
 };
-const newCounter = counter();
+
 // Example usage: const newCounter = counter();
+
+const newCounter = counter();
+
 console.log(newCounter());
 console.log(newCounter());
 
@@ -24,7 +27,19 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  return {
+    increment: () => {return ++counter},
+    decrement: () => {return --counter}
+  }
 };
+const newCounterFactory = counterFactory();
+
+console.log(newCounterFactory.increment())
+console.log(newCounterFactory.increment())
+console.log(newCounterFactory.decrement())
+console.log(newCounterFactory.decrement())
