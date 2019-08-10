10a9d7f35a02d2316c9eadf5fa6515117441feb8
John Wells <molekewl2017@gmail.com>
2019-08-09 23:48:56 -0700

    some exercises complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
@@ -23,29 +23,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, (length) => {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr.slice(-1)[0])
 }
+last(items, (last) => {
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let total = x + y;
+  cb(total);
 }
+sumNums(5, 4, (total) => {
+  console.log(total);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x * y;
+  cb(result);
 }
+const MultiplyFeeder = (result) => {
+  console.log('The result is ' + result);
+}
+
+multiplyNums(5, 4, MultiplyFeeder);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
+  cb((list.includes(item)));
+  // if (list.includes(item)) {
+  //   cb(true)
+  // } 
+  // else
+  //   cb(false);
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+contains('yo-yo', items, (answer) => {
+  console.log(answer);
+});
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
