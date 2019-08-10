f4e314d318d7e3660c69d626679c01009cbd6bba
Angie0488 <43270346+Angie0488@users.noreply.github.com>
2019-08-10 14:23:11 -0400

    Completed closure.js and callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,7 +1,7 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+ 
 /* 
 
   //Given this problem: 
@@ -27,25 +27,51 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length)
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+    return cb(arr[3]);
+  }
+  last(items, function(lastItem) {
+    console.log(lastItem)
+  });
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+sumNums(1, 2, function(x, y) {
+  console.log(x + y)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+multiplyNums(3, 5, function(x, y) {
+  console.log(x * y)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+if(list.includes(item)) {
+  return cb(true);
+} else {
+  return cb(false);
+}
 }
 
+contains('Notebook', items, function(result) {
+  console.log(result)
+});
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const firstName = "Angie";
+function sayHello() {
+  const lastName = "Lawrence";
+  return lastName;
+}
+console.log(firstName);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
