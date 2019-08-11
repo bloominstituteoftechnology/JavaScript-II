12803dc6d0fa8d332f5ed7d2db5d4555ab725636
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-08-11 11:11:30 -0400

    Worked on callbacks, with the array example.  Was surprised that I understood it throughly and got it done on my first try! yay!

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,41 +1,47 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
-/* 
-
   //Given this problem: 
-  
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // Potential Solution:
-
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
   // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
 
-*/
-
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+ console.log(length)
+});
 
-function last(arr, cb) {
+
+function lastitem(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3]);
 }
+lastitem(items, function(last) {
+  console.log(last)
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+cb(function(results) {
+ console.log(results)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
