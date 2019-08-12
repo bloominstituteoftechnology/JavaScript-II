4d45756a0cf4981b9c4ae231967752eab5c74e3d
Angie0488 <43270346+Angie0488@users.noreply.github.com>
2019-08-11 21:46:01 -0400

    completed array-methods.js/part of stretch

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,27 +56,65 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(runner => {
+    fullName.push(`${runner.first_name} ${runner.last_name}`);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+    runners.map((runner, index) => {
+        allCaps[index] = runner.first_name.toUpperCase();
+    })
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(runner => {
+    if(runner.shirt_size === 'L') {
+        return runner;
+    }
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((totalValue, runner) => {
+    return totalValue + runner.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
-
+const myChildren = [
+    {
+        name: "Ryleigh",
+        age: 7,
+        favoriteColor: "Pink",
+        id: 1
+    },
+    {
+        name: "Everleigh",
+        age: 6,
+        favoriteColor: "Yellow",
+        id: 2
+    },
+    {
+        name: "Sophia",
+        age: 1,
+        favoriteColor: "Green",
+        id: 3
+    }
+]
 // Problem 1
+const childrensNamesAge = [];
+myChildren.forEach(myChildren => {
+    return childrensNamesAge.push(`My daughters name is ${myChildren.name}, and she is ${myChildren.age}.`);
+});
+console.log(childrensNamesAge);
 
 // Problem 2
 
