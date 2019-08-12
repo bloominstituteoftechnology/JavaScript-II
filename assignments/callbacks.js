a1fe8b806147024df84de3614ed8de38cf277b6b
Angie0488 <43270346+Angie0488@users.noreply.github.com>
2019-08-11 22:02:25 -0400

    completed challenge 2/stretch challenge for closure.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -78,4 +78,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -5,22 +5,23 @@ function sayHello() {
   const lastName = "Lawrence";
   return lastName;
 }
-console.log(firstName);
+//console.log(firstName);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return ++count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+ 
 };
