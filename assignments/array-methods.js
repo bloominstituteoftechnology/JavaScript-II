23419e677ee044cc90f68f8aa8077890ff791c17
Andy Barrows <andy.barrows@gmail.com>
2019-08-13 19:31:46 -0400

    second-commit-array-complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -65,23 +65,37 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+const caplock =(str) => {
+    return str.toUpperCase();
+    allCaps = fullName.map(caplock)
+    console.log(allcaps)
+}
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+const largeSized = (object) => {
+    return object.shirt_size === "L"
+  }
+  largeShirts = runners.filter(largeSized)
 console.log(largeShirts);
+
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce(total,runner) => totel + runner.donation, 0);
 console.log(ticketPriceTotal);
+
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+const emailList = [];
+runners.forEach(e => {emailList.push([e.first_name, e.email])});
 // Problem 2
-
+let smallShirts = runners.filter(function(lessThanMed){
+    return lessThanMed.shirt_size === "M" || lessThanMed.shirt_size === "S" || lessThanMed.shirt_size === "XS";
 // Problem 3
