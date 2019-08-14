6ebcedaf80dc8d9eefabaa4ad39e32ccdb7ce4b9
Carlos Turcios <turcios50@gmail.com>
2019-08-13 18:06:52 -0700

    finished JavaScript-II project

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const foo = 'bar';
+function returnFoo() {
+  const foo = 'pizza';
+  return foo;
+}
+function returnfoo2() {
+  return foo;
+}
+console.log(returnFoo());
+console.log(returnfoo2());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count = count + 1;
+    return count;
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
