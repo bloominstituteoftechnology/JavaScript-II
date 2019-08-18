820e29947648f94b75430f0cdc35cb5dbe70407e
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-18 12:57:53 +0200

    work on array challenge 1 wording

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,9 +56,9 @@ const runners = [
 ];
 
 // ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
-console.log(fullName);
+// The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called fullNames. This array contains just strings.
+let fullNames = [];
+console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
