d4d5a19625db75efda36367e4b14d9755ed2532a
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-08-20 18:52:40 -0400

    finished all assignments excluding stretch goals and arrays-methds.js challenge 3 and 4 fix

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,8 +3,11 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
-
+const num = 3;
+function add (num){
+  return num + 0;
+}
+console.log(add(num));
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
