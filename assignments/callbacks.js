0d0c7e9902cd902f429439a1d21b9021fc31d190
Daniel <danielkim.s@outlook.com>
2019-08-20 14:18:04 -0700

    Started on Callbacks Assignment

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,7 +41,11 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
+  return cb(arr.length)
+};
+getLength(items, function(arrLength){
+  console.log(arrLength)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
