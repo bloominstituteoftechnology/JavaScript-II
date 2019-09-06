6afcc434d1ccf495606372bc789024619b7acb65
CLYPR Technologies <cbanks@clypr.co>
2019-09-05 23:15:44 -0400

    start javascript2 assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -31,3 +31,6 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+//
+
