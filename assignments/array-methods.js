01d4ff693df6d6a7b78a1691624f46a013d0e273
Samuel Kraus <S_read@aol.com>
2019-09-07 13:48:19 -0500

    began foreach code

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,7 +58,23 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+
+
+for (let i = 0; i<runners.length; i++){
+  fullNames.push(runners[i].first_name + runners[i].last_name);
+  // console.log(fullNames);
+}
+
+
+  runners.forEach((name, i, array)=>{
+            console.log(name+name);
+            // console.log(array);
+            // console.log(runners);
+  });
+
+
 console.log(fullNames);
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
