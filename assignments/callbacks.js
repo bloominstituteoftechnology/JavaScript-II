0eac05ca4c6a2babf28e9dcdfe793ca8ea6a0b9c
CLYPR Technologies <cbanks@clypr.co>
2019-09-06 21:22:21 -0400

    completed all assignments

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,32 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+console.log(getLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.length-1);
 }
+console.log(last);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let z = x + y;
+  return cb(z);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let z = x * y;
+  return cb(z);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
