e6cec43516efe90fc8fc6ac5911e6a654ce24906
unknown <franzferdinan51@gmail.com>
2019-09-08 14:54:54 -0400

    i didnt learn any thing google did it all for me

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,64 +1,41 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
-
-  // GIVEN THIS PROBLEM:
-
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // SOLUTION:
-
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  // NOTES ON THE SOLUTION:
-
-  // firstItem is a higher order function.
-  // It expects a callback (referred to as `cb`) as its second argument.
-  // To test our solution, we can use the given `items` array and a variety of callbacks.
-  // Note how callbacks can be declared separately, or inlined.
-
-  // TEST 1 (inlined callback):
-
-  const test1 = firstItem(items, item => `I love my ${item}!`);
-  console.log(test1); // "I love my Pencil!"
-
-  // TEST 2 (declaring callback before hand):
-
-  function logExorbitantPrice(article) {
-    return `this ${article} is worth a million dollars!`;
-  };
-
-  const test2 = firstItem(items, logExorbitantPrice);
-  console.log(test2); // "this Pencil is worth a million dollars!"
-*/
-
+function firstItem(arr, cb) {
+  // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+let z = x+y;
+cb(z);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+let z = x * y;
+cb(z);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+cb(list.includes(item))
+
 }
+
+contains('Pencil', items, console.log)
+
 
 /* STRETCH PROBLEM */
 
