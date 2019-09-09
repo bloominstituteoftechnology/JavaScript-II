ed1daa4ba4551ea18d96c4159e44cf1a0ee56cef
brianetaveras <taveras574@gmail.com>
2019-09-09 19:38:10 -0400

    Finished challenge 2 of closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,6 +15,7 @@ console.log(name)
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
+let count = 0
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
@@ -22,7 +23,16 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+ const counter = ()=>{
+    count++
+    return count
+ }
+ return counter()
 };
+
+console.log(counterMaker()) // 1
+console.log(counterMaker()) // 2
+console.log(counterMaker()) // 3
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
