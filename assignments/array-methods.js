43c60ed34d68a3b97fffc2e3a5c3316d5faf29ac
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-09-13 09:07:05 -0700

    Finished callbackx, arrays, closures

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,22 +57,42 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
-let fullNames = [];
-console.log(fullNames);
+// let fullNames = [];
+// console.log(fullNames);
 
+runners.forEach((fullNames)=> {
+  console.log(`${fullNames.first_name} ${fullNames.last_name}`)
+})
+console.log(fullNames);
+// -------------------------------------------------------------
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
-console.log(firstNamesAllCaps);
+
+runners.map((firstNamesAllCaps)=>{
+  console.log(`${firstNamesAllCaps.first_name.toUpperCase()}`)
+  });
+
+
+// -------------------------------------------------------------
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
-console.log(runnersLargeSizeShirt);
+
+const runnersLargeSizeShirt = runners.filter((item) =>{ 
+  return item.shirt_size === "L"
+})
+console.log(runnersLargeSizeShirt)
+// -------------------------------------------------------------
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+// let ticketPriceTotal = 0;
+// console.log(ticketPriceTotal);
+
+const ticketPriceTotal= runners.reduce((currentTotal, item) => { 
+  return item.donation + currentTotal
+}, 0)
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
