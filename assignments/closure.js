43ea02d45dcaee66b031df8ed7ee803a8192c169
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-09-17 16:13:05 -0500

    callback.js and array-methods complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,9 +4,7 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
-
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
