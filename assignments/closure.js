81ef30ec18cbf77f819c10e09263542fb94a53b8
miriam-sullivan <maljayd42@gmail.com>
2019-09-17 16:00:30 -0500

    Finished bare bones stuff

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,7 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
-
+function showName() {
+  var name = "Mylastname";
+  function Name() {
+      alert (Sullivan); 
+  }
+  Name();    
+}
+showName();
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
