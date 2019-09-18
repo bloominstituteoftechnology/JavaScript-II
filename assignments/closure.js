d8ce6ae84a362adb7dc290d6d8d25b3e4c68f641
Blaque <rikeiwilliams@gmail.com>
2019-09-17 23:41:56 -0400

    finished closures except 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,31 +3,61 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
-
+const num2 = 7;
+function closure() {
+  const num1 = 4;
+  return num1 + num2;
+}
+console.log(closure());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-const counterMaker = () => {
+const counterMaker = (limit) => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  let count = 0;
+
+  return function() {
+    count = count + 1;
+    return count;
+  }
+
+
+
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
+const myCounter = counterMaker();
+console.log(myCounter(2));
+console.log(myCounter(2));
+console.log(myCounter(2));
+console.log(myCounter(2));
+
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+
+
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  const obj {
+    inrcrement: function() {
+      return num++;
+    }
+    decrement: function() {
+      return num --;
+    }
+  }
+
 };
