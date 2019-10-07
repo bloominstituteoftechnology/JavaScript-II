170ff17f54850f93244df17889d883be6ab67704
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-10-07 14:40:29 -0500

    after finishing closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,19 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const dogs = () => {
+  let dogNumber = 0;
+  return function() {
+    dogNumber = dogNumber + 1;
+    return dogNumber;
+  }
+};
+
+const moreDogs = dogs();
+console.log(moreDogs());
+console.log(moreDogs());
+console.log(moreDogs());
+console.log(moreDogs());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
