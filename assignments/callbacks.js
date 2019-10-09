902362f7e1dd6714aff991cf9191ead6aae2ee50
kevin <akolade4real@gmail.com>
2019-10-08 22:12:59 -0500

    adegbola-aiyewumi MVP

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,8 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works with our items array. 
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum']
 
 /* 
 
@@ -13,7 +15,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // SOLUTION:
 
   function firstItem(arr, cb) {
-    return cb(arr[0]);
+    return cb(arr[0])
   }
 
   // NOTES ON THE SOLUTION:
@@ -25,45 +27,59 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // TEST 1 (inlined callback):
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
+  const test1 = firstItem(items, item => `I love my ${item}!`)
   console.log(test1); // "I love my Pencil!"
 
   // TEST 2 (declaring callback before hand):
 
   function logExorbitantPrice(article) {
-    return `this ${article} is worth a million dollars!`;
-  };
+    return `this ${article} is worth a million dollars!`
+  }
 
-  const test2 = firstItem(items, logExorbitantPrice);
+  const test2 = firstItem(items, logExorbitantPrice)
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    // getLength passes the length of the array into the callback.
+    return cb(arr.length)
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    // last passes the last item of the array into the callback.
+    return cb(arr[arr.length - 1])
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    let sumTwoNums = x + y
+    return cb(sumTwoNums)
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    // multiplyNums multiplies two numbers and passes the result to the callback.
+    let multiplyTwoNums = x * y
+    return cb(multiplyTwoNums)
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].contains(item)) {
+            isContain = true
+        } else {
+            isContain = false
+        }
+    }
+    return cb(isContain)
 }
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+    // removeDuplicates removes all duplicate values from the given array.
+    // Pass the duplicate free array to the callback function.
+    // Do not mutate the original array.
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -3,31 +3,70 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+const name = 'Kevin'
 
+function returnName() {
+    return name
+}
+returnName()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
-};
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+    // IMPLEMENTATION OF counterMaker:
+    // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+    // 2- Declare a function `counter`. It should increment and return `count`.
+    //      NOTE: This `counter` function, being nested inside `counterMaker`,
+    //      "closes over" the `count` variable. It can "see" it in the parent scope!
+    // 3- Return the `counter` function.
+
+    let counterValue = 0
+    return function() {
+        counterValue = counterValue + 1
+        return counterValue
+    }
+}
+const myCounter = counterMaker()
+console.log(myCounter())
+    // Example usage: const myCounter = counterMaker()
+    // myCounter() // 1
+    // myCounter() // 2
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+const limitParameter = limit => {
+    let counter = 0
+    let maximumCount = counter
+
+    return function counterMaker() {
+        if (counter > maximumCount) {
+            counter = 1
+            return counter
+        } else {
+            return counter++
+        }
+    }
+}
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+    // Return an object that has two methods called `increment` and `decrement`.
+    // `increment` should increment a counter variable in closure scope and return it.
+    // `decrement` should decrement the counter variable and return it.
+
+    let counter = 0
+    return {
+        increment() {
+            return counter++
+        },
+        decrement() {
+            return counter--
+        }
+    }
+}
+
+const objectCounter = counterFactory()
+console.log(objectCounter.increment)
+console.log(objectCounter.decrement)
