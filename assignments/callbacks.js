ca6e87100cccb6bd3883ea2486d2e6c4d4af94e7
toddmurphy <toddmurphy93@gmail.com>
2019-10-15 20:24:58 -0300

    completed callbacks challenge 2

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -50,26 +50,40 @@ function displayLength(name) {
 }
 displayLength('Todd')
 
-// //function last(arr, cb) {
-//     // last passes the last item of the array into the callback.
-// }
+function last(items, cb) {
+    // last passes the last item of the array into the callback.
+    return cb(items[items.length - 1])
+}
+console.log(last(items, lastItem))
 
-// //function sumNums(x, y, cb) {
-//     // sumNums adds two numbers (x, y) and passes the result to the callback.
-// }
+function lastItem(placeHolder) {
+    //placehoder is pulling from the cb(...) i dont' understand how cb gets to placeholder
+    return `The last item in the items array is ${placeHolder}`
+}
+lastItem()
 
-// //function multiplyNums(x, y, cb) {
+function sumNums(x, y, cb) {
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x + y)
+}
+sumNums(num1, num2, addSum)
+
+function addSum(newPlaceHolder) {
+    return ` text... ${newPlaceHolder}`
+}
+addSum()
+// function multiplyNums(x, y, cb) {
 //     // multiplyNums multiplies two numbers and passes the result to the callback.
 // }
 
-// //function contains(item, list, cb) {
+// function contains(item, list, cb) {
 //     // contains checks if an item is present inside of the given array/list.
 //     // Pass true to the callback if it is, otherwise pass false.
 // }
 
 // /* STRETCH PROBLEM */
 
-// //function removeDuplicates(array, cb) {
+// function removeDuplicates(array, cb) {
 //     // removeDuplicates removes all duplicate values from the given array.
 //     // Pass the duplicate free array to the callback function.
 //     // Do not mutate the original array.
