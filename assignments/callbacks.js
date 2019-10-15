d3ba26e50a236fd0a27e8c943004692366be0841
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-10-15 13:19:53 -0700

    finished all problems on callbacks, working on stretch problem

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -93,17 +93,59 @@ const sumNums2 = (x, y, cb) => {
 }
 
 sumNums2(420, 666, sumOfTwo => {
-  console.log(`This is the sum of any two numbers: ${sumOfTwo}.`);
+  console.log(`This is the sum of the chosen two numbers: ${sumOfTwo}.`);
 });
 
+
+//ES5
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x, y);
 }
 
+multiplyNums(666, 420, function(x, y) {
+  console.log(x * y);
+});
+
+//ES6
+const multiplyNums2 = (x, y, cb) => {
+  cb(x * y);
+}
+
+multiplyNums2(420, 666, multipleOfTwo => {
+  console.log(`This is the multiple of the chosen two numbers: ${multipleOfTwo}.`);
+});
+
+
+//first way to do it
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(`Yes, this list contains your item - ${item}.`);
+  } else {
+    return cb(`No, this list does not contain your item - ${item}.`);
+  }
 }
+
+contains('yo-yo', items, function(check) {
+  console.log(check);
+})
+
+//second way to do it
+const contains2 = (arr, str, cb) => {
+  const inArray = () => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === str) {
+        return `This list contains ${str}.`;
+      }
+    } return `This list does not contain ${str}.`;
+  };
+  cb(inArray());
+};
+contains2(items, 'yo-yo', check => {
+  console.log(check);
+});
 
 /* STRETCH PROBLEM */
 
