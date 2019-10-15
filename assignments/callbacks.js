e4d64782275ba713536a4785e7089f90237bb7fc
Braden <fish2355@hotmail.com>
2019-10-15 14:34:38 -0600

    finished closure and callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,34 +1,34 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
-  // GIVEN THIS PROBLEM:
+  GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
+    firstItem passes the first item of the given array to the callback function.
   }
 
-  // SOLUTION:
+  SOLUTION:
 
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
 
-  // NOTES ON THE SOLUTION:
+  NOTES ON THE SOLUTION:
 
-  // firstItem is a higher order function.
-  // It expects a callback (referred to as `cb`) as its second argument.
-  // To test our solution, we can use the given `items` array and a variety of callbacks.
-  // Note how callbacks can be declared separately, or inlined.
+  firstItem is a higher order function.
+  It expects a callback (referred to as `cb`) as its second argument.
+  To test our solution, we can use the given `items` array and a variety of callbacks.
+  Note how callbacks can be declared separately, or inlined.
 
-  // TEST 1 (inlined callback):
+  TEST 1 (inlined callback):
 
   const test1 = firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
 
-  // TEST 2 (declaring callback before hand):
+  TEST 2 (declaring callback before hand):
 
   function logExorbitantPrice(article) {
     return `this ${article} is worth a million dollars!`;
@@ -38,27 +38,74 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+cbLength = arr => arr.length;
+
+//! console.log(getLength(items, cbLength));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr);
 }
+cbLast = arr => arr[arr.length - 1];
+
+//! console.log(last(items, cbLast));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+cbSumNums = (x, y) => x + y;
+
+//! console.log(sumNums(2, 4, cbSumNums));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+cdMultiplyNums = (x, y) => x * y;
+
+//! console.log(multiplyNums(3, 6, cdMultiplyNums));
+
+// let items = ["Pencil", "Notebook", "yo-yo", "Gum"];
+/*
+function contains(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  list.forEach(function(currentValue) {
+    if (item == currentValue) {
+      return cb(true);
+    } else {
+      cb(false);
+    }
+  });
+  //* return item == list[i] ? cb(true) : cb(false);
+}
+//! let containsCb = argumentPassed => console.log(argumentPassed);
+
+//! contains("yo-yo", items, containsCb);
+*/
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    //* return item == list[i] ? cb(true) : cb(false);
+    if (item == list[i]) {
+      return cb(true);
+    }
+  }
+  return cb(false);
 }
+let containsCb = argumentPassed => console.log(argumentPassed);
+
+contains("yo-yo", items, containsCb);
 
 /* STRETCH PROBLEM */
 
@@ -67,3 +114,5 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+// const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
--- assignments/closure.js
+++ assignments/closure.js
@@ -3,10 +3,15 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
+function gerald(param1, param2) {
+  let result = function() {
+    console.log(`${param1} is not ${param2}`);
+  };
+  result(param1, param2);
+}
+gerald("Jackie", "George");
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
