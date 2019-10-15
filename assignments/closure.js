1a38194684b537955c3c39e21e78126fc4e5f0ca
Jor Ming Poon <jormpoon@Jors-MBP.home>
2019-10-15 19:47:33 -0400

    strech goal

--- assignments/closure.js
+++ assignments/closure.js
@@ -52,4 +52,28 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => ( ++count ),
+    decrement: () => ( --count ),
+  }
 };
+
+const counterNew = counterFactory();
+
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.increment())
+console.log(counterNew.decrement())
+
+
