5012c09344226922f359ebac43739b5a6c8cb116
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-10-15 12:55:41 -0700

    callback.js mvp complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.legth - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let mult = x * y;
+  cb(mult);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
