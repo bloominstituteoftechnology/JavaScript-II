5ae2edd45c7144a4bc5dbf4aee75e89a3e1cac61
dueka <ekanemd@live.com>
2019-10-16 08:15:12 +0100

    question 3 -5

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -55,15 +55,29 @@ last(items, function(lastItem) {
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+sumNums(2, 4, function(x, y) {
+  console.log(x + y);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+multiplyNums(2, 7, function(x, y) {
+  console.log(x * y);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
