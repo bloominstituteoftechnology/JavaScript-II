719e256f1a1c28914b955db6f5f902299f6e3c8c
Trent Norris <trent.norris018@gmail.com>
2019-12-04 14:50:54 -0800

    closure challenge 2 completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -19,11 +19,19 @@ returnDog();
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  let count = 0;
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+  return function counter() {
+    count++;
+    return count;
+  }
 };
+
+const myCounter = counterMaker();
+console.log(myCounter);
+
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
