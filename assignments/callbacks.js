054b27c9236cb1765355aa703d2100e4a9d5ca99
Trent Norris <trent.norris018@gmail.com>
2019-12-05 02:17:39 -0800

    first callback problem solved

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work.
+ //You have been provided an example of a problem and a solution to see how this works with our items array.  
+ //Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -38,10 +40,11 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return arr.length;
 }
+console.log(getLength(items));
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
